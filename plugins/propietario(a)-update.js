865e7588f0ac3d159af27510329f47668452a3cd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-01 13:54:11 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/propietario(a)-update.js
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+try {  
+if (global.conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+conn.reply(m.chat, stdout.toString(), m)}
+} catch {
+var update = execSync('git remote set-url origin https://github.com/GataNina-Li/GataBot-MDLite.git && git pull')
+await m.reply(update.toString())
+}}
+handler.command = /^update|actualizar$/i
+handler.rowner = true
+export default handler 
