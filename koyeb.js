316d48a97127916acb5cee0e5a8079822d0395a6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-01 13:13:35 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- koyeb.js
+++ koyeb.js
@@ -2,15 +2,13 @@ FROM fedora:37
 
 RUN sudo dnf -y update &&\
     sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
-    sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
+    sudo dnf install -y git ffmpeg ImageMagick nodejs libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/GataNina-Li/GataBot-MD
+RUN git clone https://github.com/GataNina-Li/GataBotLite-MD
 
-WORKDIR /root/GataBot-MD
+WORKDIR /root/GataBotLite-MD
 
-COPY ./root/GataBot-MD
-
-RUN yarn install
+COPY ./root/GataBotLite-MD
 
 CMD ["node", "index.js"]
