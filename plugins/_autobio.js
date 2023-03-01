b487466209e0096fe07210b2f4a359b17d5bb9f3
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-03-01 15:59:50 -0300

    Add files via upload

---
+++ plugins/_autobio.js
@@ -0,0 +1,24 @@
+let handler = m => m
+handler.all = async function (m) {
+let setting = global.db.data.settings[this.user.jid]
+	
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) { process.send('uptime')
+_muptime = await new Promise(resolve => { process.once('message', resolve) 
+setTimeout(resolve, 7000) }) * 1000}
+
+let uptime = clockString(_uptime)
+let bio = `${global.packname} ║ ✅ ${uptime} ⌛ ║ ⒼⒷ 𓃠 #estado #menu #jadibot #grupos #owner 💻`
+await this.updateProfileStatus(bio).catch(_ => _)
+setting.status = new Date() * 1
+}
+export default handler
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('') 
+} 
