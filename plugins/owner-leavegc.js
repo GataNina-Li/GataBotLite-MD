1b6bafda45a15da339fd525a6283ea5b3fe34534
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-01 13:19:50 -0500

    Delete plugins directory

--- plugins/owner-leavegc.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-await conn.reply(id, '*GATABOT ABANDONA EL GRUPO FUE UN GUSTO ESTA AQUI HASTA PRONTO 👋*') 
-await conn.groupLeave(id)}
-handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
-handler.group = true
-handler.rowner = true
-export default handler
