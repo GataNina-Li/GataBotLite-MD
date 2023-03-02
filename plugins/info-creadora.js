97ab1c1e5f6a08aeb3926ef3f6c0014fd623c6c3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-02 15:49:26 -0500

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -1,4 +1,4 @@
-let { MessageType } = (await import('@adiwajshing/baileys')).default
+/*let { MessageType } = (await import('@adiwajshing/baileys')).default
 
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 try {
@@ -73,9 +73,9 @@ return await conn.sendMessage(m.chat, listMessage, {quoted: m})
 //}}
 
 handler.command = /^(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
-export default handler
+export default handler*/
 
-/*
+
 let { MessageType } = (await import('@adiwajshing/baileys')).default
 
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
@@ -86,14 +86,11 @@ let name = await conn.getName(m.sender)
 let type = (args[0] || '').toLowerCase()
 let _type = (args[0] || '').toLowerCase()
 
-//------- Nombre
-  let nowner = `${wm.split`@`[0]}@s.whatsapp.net`
-  let teksnomor = `
-• @${wm.split`@`[0]} •
-------- ${wm} -------
-`
 
-//------------ BIO
+let nowner = `${wm.split`@`[0]}@s.whatsapp.net`
+let teksnomor = `
+• @${wm.split`@`[0]} •
+------- ${wm} -------`
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => imagen1[1]) 
 let teksbio = `
 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
@@ -119,47 +116,38 @@ let teksbio = `
 *Sr. Pablo* - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼
 *Wa.me/51993042301*
 `
-  let teks = ' '
 const sections = [
-   {
-	title: `PROPIETARIO/OWNER`,
-	rows: [
-	    {title: "📱 • NOMBRE", rowId: ".owner nomor"},
-	{title: "🙌 • NUMERO", rowId: ".owner bio"},
-	{title: "🌐 • CUENTAS OFICIALES", rowId: ".cuentasgb"},
-	{title: "😸 • GRUPOS", rowId: ".grupos"},
-	{title: "🌎 • SCRIPT", rowId: ".sc"},
-	]
-    },{
-	title: `–––––––·• APOYA AL BOT –––––––·•`,
-	rows: [
-	    {title: "💹 • DONAS", rowId: ".paypal"},
-	{title: "🤖 • INSTALARBOT", rowId: ".instalarbot"},
-	{title: "🌟 • PREMIUM", rowId: ".pasepremium"},
-	]
-  },
+{ title: '🌟 I N F O',
+rows: [
+{title: "⊜ NOMBRE", rowId: usedPrefix + command + "nombre"},
+{title: "⊜ NUMERO", rowId: usedPrefix + command + "numero"},
+{title: "⊜ CUENTAS", rowId: usedPrefix + "cuentasgb"},
+{title: "⊜ GRUPOS", rowId: usedPrefix + "grupos"},
+]},
+{ title: '✨ A P O Y O',
+rows: [
+{title: "⊜ DONAS", rowId: usedPrefix + "paypal"},
+{title: "⊜ INSTALARBOT", rowId: usedPrefix + "instalarbot"},
+{title: "⊜ PREMIUM", rowId: usedPrefix + "pase premium"},
+]},
 ]
 
 const listMessage = {
-  text: teks,
-  footer: null,
-  title: `╭━━━✦ *OWNER ✦━━━━⬣
-┃დ HOLA 👋 ${name}
-┃≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋≋
-┃${wm}
-╰━━━━━✦ *${vs}* ✦━━━━⬣`,
-  buttonText: "HAGA CLICK AQUI",
-  sections
-}
+text: wm,
+footer: null,
+title: `🌼 *SELECCIONE QUE QUIERE SABER*
+⎔ ${taguser} 👋`,
+buttonText: "🌹 SELECCIONAR AQUÍ 🌹",
+sections }
 
   try {
     if (/(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)/i.test(command)) {
       const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
         switch (type) {
-          case 'nomor':
+          case 'nombre':
           conn.reply(m.chat, "Nombre del bot : GataBot-MD 🐈", m, { contextInfo: { mentionedJid: [nowner] }})
             break
-            case 'bio':
+            case 'numero':
           conn.sendHydrated(m.chat, teksbio, wm, ppown, "https://www.instagram.com/gata_dios", "Instagram",null, [null, null], m)
             break
           default:
@@ -182,8 +170,6 @@ const listMessage = {
   }
 }
 
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
 handler.command = /^(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
 
-export default handler*/
+export default handler
