07bf92a19d71819efb85fd21323048fb7bf08b61
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-02 16:12:17 -0500

    Update _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,7 +1,7 @@
 function handler(m) {
 let catalogo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: wm, orderTitle: 'Bang', thumbnail: imagen1, sellerJid: '0@s.whatsapp.net'}}}
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
+this.sendContact(m.chat, data.map(([id, name]) => [id, name]), catalogo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
 }
 
 handler.help = ['creador']
