1718ae6816f80641881733483107f3398ea31e3d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-02 17:01:56 -0500

    Update _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,13 +1,8 @@
 function handler(m) {
-let catalogo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: wm, orderTitle: 'Bang', thumbnail: imagen1, sellerJid: '0@s.whatsapp.net'}}}
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-this.sendContact(m.chat, data.map(([id, name]) => [id, name]), catalogo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
+this.sendContact(m.chat, data.map(([id, name]) => [id, name]), fkontak, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
 }
 
-handler.help = ['creador']
-handler.tags = ['info']
-
 handler.command = ['prueba04'] 
-
-
 export default handler
