b02ed6ab408c4a83d52b2b937b5bba2e4c34fc40
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-02 16:11:27 -0500

    Create _prueba.js

---
+++ plugins/_prueba.js
@@ -0,0 +1,13 @@
+function handler(m) {
+let catalogo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: wm, orderTitle: 'Bang', thumbnail: imagen1, sellerJid: '0@s.whatsapp.net'}}}
+const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
+}
+
+handler.help = ['creador']
+handler.tags = ['info']
+
+handler.command = ['prueba04'] 
+
+
+export default handler
