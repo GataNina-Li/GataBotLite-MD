8145ad72c047f8de9ef2f0fc0cacd0e2ca775d5a
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-03-03 09:42:23 -0300

    Add files via upload

---
+++ plugins/info-script.js
@@ -0,0 +1,15 @@
+import { promises } from 'fs'
+import { join } from 'path'
+
+let handler = async function (m, { conn, __dirname }) {
+let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+  
+m.reply(` ${_package.homepage}`.trim())
+    
+}
+
+handler.help = ['script']
+handler.tags = ['info']
+handler.command = ['sc', 'git', 'script'] 
+
+export default handler
