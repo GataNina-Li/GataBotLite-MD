d09f6c26d818495e33475abc08dfef708890c72f
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-03-03 16:35:01 -0300

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -39,6 +39,7 @@ const cpu = cpus.reduce((last, cpu, _, { length }) => {
 const { restrict } = global.db.data.settings[conn.user.jid] || {}
 const { autoread } = global.opts
 let pp = './media/menus/Menu1.jpg'
+let vn = './media/infobot.mp3'
 let old = performance.now()
   //await m.reply('_Realizando test_')
   let neww = performance.now()
