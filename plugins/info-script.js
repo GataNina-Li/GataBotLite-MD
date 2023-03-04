fb9567ae90ce902eb02355fbf4c343575c9bcbc3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 18:40:11 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,14 +2,13 @@ import { promises } from 'fs'
 import { join } from 'path'
 
 let handler = async function (m, { conn, __dirname }) {
+try{
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-  
 m.reply(` ${_package.homepage}`.trim())
-    
-}
+}} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}
 
-handler.help = ['script']
-handler.tags = ['info']
-handler.command = ['sc', 'git', 'script'] 
-
+handler.command = ['codigo', 'código', 'sc', 'git', 'script'] 
 export default handler
