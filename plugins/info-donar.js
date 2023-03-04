6b7f3686311f3a49956adfcd73cf2c877786eb7c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 14:09:40 -0500

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,4 +1,3 @@
-import fs from 'fs'
 let handler = async (m, { conn, command }) => {
 let media = './media/menus/Menu4paypal.jpg'  
 let str = `
@@ -13,7 +12,7 @@ let templateButtons = [
 {index: 2, urlButton: {displayText: '💚 🅘🅝🅢🅣🅐🅖🅡🅐🅜', url: ig}},
 {index: 3, quickReplyButton: {displayText: 'INFOBOT', id: '#infobot'}}
 ]
-await conn.sendMessage(m.chat, { video: { url: fs.readFileSync(media) }, gifPlayback: true, gifAttribution: ~~(Math.random() * 2), caption: '*◜🌹❤️ DONACIÓN VOLUNTARIA❤️🌹◞*', footer: str, templateButtons }, { quoted: m})
+await conn.sendMessage(m.chat, { video: { url: img21 }, gifPlayback: true, gifAttribution: ~~(Math.random() * 2), caption: '*◜🌹❤️ DONACIÓN VOLUNTARIA❤️🌹◞*', footer: str, templateButtons }, { quoted: m})
 }
 handler.command = /^dona(te|si)|donar|apoyar|paypal|donating$/i 
 export default handler
