b550a97b74065521e36106534a82d5e3822994c7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 16:52:33 -0500

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -14,12 +14,13 @@ let templateButtons = [
 ]
 await conn.sendMessage(m.chat, { image: { url: img21 }, gifPlayback: false, gifAttribution: ~~(Math.random() * 2), caption: '*◜🌹❤️ DONACIÓN VOLUNTARIA ❤️🌹◞*', footer: str, templateButtons }, { quoted: m})
 
-const botones = [ {index: 1, urlButton: {displayText: `Copiar 📍`, url: `https://www.whatsapp.com/otp/copy/${paypal}`}},]
-await conn.sendPayment(m.chat, '99999999', { text: md, templateButtons: botones, footer: author })
-//await conn.sendMessage(m.chat, { text: `
-//*╭──────────────╮*
-//│☘️ • copia?
-//*╰──────────────╯*`, templateButtons: botones, footer: author })
+const botones = [ 
+{index: 1, urlButton: {displayText: '𝗣𝗔𝗬𝗣𝗔𝗟', url: 'https://www.whatsapp.com/otp/copy/' + paypal}},
+{index: 2, urlButton: {displayText: '𝗚𝗜𝗧𝗛𝗨𝗕', url: 'https://www.whatsapp.com/otp/copy/' + md}},
+{index: 3, urlButton: {displayText: packname, url: 'https://www.whatsapp.com/otp/copy/' + `https://wa.me/${global.conn.user.jid.split`@`[0]}`}},
+]
+//await conn.sendPayment(m.chat, '99999999', { text: md, templateButtons: botones, footer: author })
+await conn.sendMessage(m.chat, { text: '🍄 *_También puedes compartir lo siguiente como Muestra de Apoyo_*', templateButtons: botones, footer: wm })
 }
 handler.command = /^dona(te|si)|donar|apoyar|paypal|donating$/i 
 export default handler
