28b0bf3f454c66cee0fe9d57f7db9b072cf4fa4f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 17:10:30 -0500

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -18,9 +18,10 @@ await conn.sendMessage(m.chat, { image: { url: img21 }, gifPlayback: false, gifA
 //{index: 3, urlButton: {displayText: packname, url: 'https://www.whatsapp.com/otp/copy/' + `https://wa.me/${global.conn.user.jid.split`@`[0]}`}},
 //]
 //await conn.sendPayment(m.chat, '99999999', { text: md, templateButtons: botones, footer: author })
-await conn.sendMessage(userPri, { text: wm, templateButtons: [{index: 1, urlButton: {displayText: '𝗣𝗔𝗬𝗣𝗔𝗟', url: 'https://www.whatsapp.com/otp/copy/' + paypal}},], footer: '🍄 También puedes compartir lo siguiente como Muestra de Apoyo' })
-await conn.sendMessage(userPri, { text: '', templateButtons: [{index: 2, urlButton: {displayText: '𝗚𝗜𝗧𝗛𝗨𝗕', url: 'https://www.whatsapp.com/otp/copy/' + md}},], footer: '' })
-await conn.sendMessage(userPri, { text: '', templateButtons: [{index: 3, urlButton: {displayText: packname, url: 'https://www.whatsapp.com/otp/copy/' + `https://wa.me/${global.conn.user.jid.split`@`[0]}`}},], footer: '' })
+//await conn.sendMessage(userPri, { text: wm, templateButtons: [{index: 1, urlButton: {displayText: '𝗣𝗔𝗬𝗣𝗔𝗟', url: 'https://www.whatsapp.com/otp/copy/' + paypal}},], footer: '🍄 También puedes compartir lo siguiente como Muestra de Apoyo' })
+//await conn.sendMessage(userPri, { text: '', templateButtons: [{index: 2, urlButton: {displayText: '𝗚𝗜𝗧𝗛𝗨𝗕', url: 'https://www.whatsapp.com/otp/copy/' + md}},], footer: '' })
+//await conn.sendMessage(userPri, { text: '', templateButtons: [{index: 3, urlButton: {displayText: packname, url: 'https://www.whatsapp.com/otp/copy/' + `https://wa.me/${global.conn.user.jid.split`@`[0]}`}},], footer: '' })
+conn.sendMessage(userPri, { text: `Apoyar`, mentions: [m.sender], }, { quoted: m })
 }
 handler.command = /^dona(te|si)|donar|apoyar|paypal|donating$/i 
 export default handler
