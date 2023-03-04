6ab5927ffffb2dcbdb304293f4951e4f8d0b9bb7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 13:38:45 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -13,9 +13,9 @@ let str = `
 ◎ *FACEBOOK*
 *${fb}*
 
-❥ *_Siguenos en las Cuentas Oficiales para mantenerte al tanto de_ ${packname}*`.trim()
+${lenguajeGB.smsOfc2()}`.trim() 
   
-await conn.sendButton(m.chat, '🌸 *◜CUENTAS OFICIALES◞* 🌸', str, media, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], m)}
+await conn.sendButton(m.chat, lenguajeGB.smsOfc1(), str, media, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], m)}
 
 handler.command = /^cuenta|cuentas|cuentasoficiales|gataig|cuentasgb|cuentagb|accounts|gataaccounts|account|iggata|cuentasdegata|cuentasdegatabot|cuentagatabot|cuentasgatabot$/i
 export default handler
