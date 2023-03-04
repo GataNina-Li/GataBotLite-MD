7723f6839324d7452b72f8f4900f9b56b9105d30
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 13:32:50 -0500

    Update info-cuentas-oficiales.js

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -1,5 +1,5 @@
-let media = './media/menus/Menu2.jpg'
 let handler = async (m, { conn, usedPrefix, command }) => {
+let media = './media/menus/Menu2.jpg'
 let str = `
 ◎ *GITHUB*
 *${md}*
@@ -13,9 +13,9 @@ let str = `
 ◎ *FACEBOOK*
 *${fb}*
 
-❥ *_Siguenos en las Cuentas Oficiales para mantenerte al tanto de ${packname}*_`.trim()
+❥ *_Siguenos en las Cuentas Oficiales para mantenerte al tanto de_ ${packname}*`.trim()
   
-await conn.sendButton(m.chat, '🌸 *◜CUENTAS OFICIALES◞* 🌸', str, null, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], m)}
+await conn.sendButton(m.chat, '🌸 *◜CUENTAS OFICIALES◞* 🌸', str, media, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], m)}
 
 handler.command = /^cuenta|cuentas|cuentasoficiales|gataig|cuentasgb|cuentagb|accounts|gataaccounts|account|iggata|cuentasdegata|cuentasdegatabot|cuentagatabot|cuentasgatabot$/i
 export default handler
