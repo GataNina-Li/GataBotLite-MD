ec48e022895c20ad6fab5808bbc5f306480b5bac
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-03-04 16:35:09 -0600

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,15 +1,34 @@
-import { promises } from 'fs'
-import { join } from 'path'
-
-let handler = async function (m, { conn, __dirname }) {
-let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-  
-m.reply(` ${_package.homepage}`.trim())
-    
-}
-
+let handler = async (m, { conn, usedPrefix }) => {
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let texto1 = `*https://github.com/GataNina-Li*`
+let buttonMessage= {
+'document': { url: `https://github.com/GataNina-Li/GataBotLite-MD` },
+'mimetype': `application/${document}`,
+'fileName': `GataBotLite - MD 🐈💖`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': script,
+'mediaType': 2,
+'previewType': 'pdf',
+'title': ``,
+'body': global.author,
+'thumbnail': global.img,
+'sourceUrl': 'https//wa.me/50499698072' }},
+'caption': texto1,
+'footer': '\n𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻',
+'buttons':[
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: 'Menú 📒'}, type: 1}, 
+{buttonId: `${usedPrefix}donar`, buttonText: {displayText: 'Donar 📌'}, type: 1}],
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
+handler.tags =['info']
 handler.help = ['script']
-handler.tags = ['info']
-handler.command = ['sc', 'git', 'script'] 
-
+handler.command = ['sc', 'script']
+handler.register = true
 export default handler
+
