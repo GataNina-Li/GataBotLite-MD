e762b9306ce69f64aee49c5528bd8f3221393926
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 14:22:17 -0500

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,18 +1,18 @@
-let handler = async (m, { conn, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
 let media = './media/menus/Menu4paypal.jpg'  
 let str = `
-🌼 *_Se acepta apoyo voluntario por PayPal, si deseas apoyar de otra forma puedes donar un Número en Instagram para que el repositorio siga vigente, Gracias!!_
+🌼 Se acepta apoyo voluntario por PayPal, si deseas apoyar de otra forma puedes donar un Número en Instagram para que el repositorio siga vigente, Gracias!!
 
 ◎ *PAYPAL*
 *_https://paypal.me/OficialGD_*`.trim()
   
 //await conn.sendButton(m.chat, lenguajeGB.smsOfc1(), str, media, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], m)
 let templateButtons = [ 
-{index: 1, urlButton: {displayText: '❤️ 🅖🅘🅣🅗🅤🅑', url: md}},
-{index: 2, urlButton: {displayText: '💚 🅘🅝🅢🅣🅐🅖🅡🅐🅜', url: ig}},
-{index: 3, quickReplyButton: {displayText: 'INFOBOT', id: '#infobot'}}
+{index: 1, urlButton: {displayText: '💙 ＰＡＹＰＡＬ', url: paypal}},
+{index: 2, urlButton: {displayText: '🧡 ＩＮＳＴＡＧＲＡＭ', url: ig}},
+{index: 3, quickReplyButton: {displayText: lenguajeGB.smsConMenu(), id: `${usedPrefix}menu`}}
 ]
-await conn.sendMessage(m.chat, { location: { url: img21 }, gifPlayback: false, gifAttribution: ~~(Math.random() * 2), caption: '*◜🌹❤️ DONACIÓN VOLUNTARIA❤️🌹◞*', footer: str, templateButtons }, { quoted: m})
+await conn.sendMessage(m.chat, { image: { url: img21 }, gifPlayback: false, gifAttribution: ~~(Math.random() * 2), caption: '*◜🌹❤️ DONACIÓN VOLUNTARIA❤️🌹◞*', footer: str, templateButtons }, { quoted: m})
 }
 handler.command = /^dona(te|si)|donar|apoyar|paypal|donating$/i 
 export default handler
