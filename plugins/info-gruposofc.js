41498b78ce1763c2aa408831c5dddf34a7f4de29
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-05 18:10:59 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -5,52 +5,28 @@ let handler = async (m, { conn, usedPrefix , command }) => {
 
 let str = `
 *◎⇢ ◜🌺 GRUPOS OFICIALES 🌺◞*
-⭔
+⭔ ${grupo[0]}
 
-⭔
+⭔ ${grupo[1]}
 
-⭔
+⭔ ${grupo[2]}
 
-⭔
+⭔ ${grupo[3]}
 
-⭔
+⭔ ${grupo[4]}
 
-⭔
+⭔ ${grupo[5]}
 
 
+*◎⇢ ◜🌸 COLABORACIÓN 🌸◞*
+⭔ ${grupo[6]}
 
-◎⇢ ${data1.subject}
-┆ ⭔ *id* ${data1.id}
-┆ ⭔ *creado* ${data1.creation}
-┆ ⭔ *owner* ${data1.owner}
-◎⇢ ${grupo[0]}
+⭔ ${grupo[7]}
 
-◎⇢ ${data2.subject}
-┆ ⭔ *id* ${data2.id}
-┆ ⭔ *creado* ${data2.creation}
-┆ ⭔ *owner* ${data2.owner}
-◎⇢ ${grupo[1]}
-
-`.trim()*/
+⭔ ${grupo[8]}`.trim()
   
 await conn.sendButton(m.chat, wm, str, img, [[lenguajeGB.smsConMenu(), usedPrefix  + 'menu']], m)
 }
 
 handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupsgb$/i
 export default handler
-
-const extractGroupMetadata = (result) => {
-	const group = baileys.getBinaryNodeChild(result, 'group')
-	const descChild = baileys.getBinaryNodeChild(group, 'description')
-	let desc
-	if (descChild) desc = baileys.getBinaryNodeChild(descChild, 'body')?.content
-	const metadata = {
-		id: group.attrs.id.includes('@') ? group.attrs.id : baileys.jidEncode(group.attrs.id, 'g.us'),
-		subject: group.attrs.subject,
-		creation: new Date(+group.attrs.creation * 1000).toLocaleString('id', { timeZone: 'Asia/Jakarta' }),
-		owner: group.attrs.creator ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.creator).split('@')[0] : undefined,
-		desc
-	}
-	return metadata
-}
-
