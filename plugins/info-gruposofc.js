2a20f65e4f38604bbc729fdd73040d91b7433acc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 20:22:13 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,6 +1,7 @@
 import * as baileys from '@adiwajshing/baileys'
 
 const grupos = [nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt2, nnnttt3, nnnttt4]
+const nombre = [nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt2, nnnttt3, nnnttt4]
 //const grupo = nna
 let handler = async (m, { conn, command }) => { 	
 
@@ -14,12 +15,43 @@ let handler = async (m, { conn, command }) => {
 //let data = extractGroupMetadata(res)
 //})
 	
-for (let i = 0; i < grupos.length; i++) {
-  const [, code] = grupos[i].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
-  const res = conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code } }] })
-  const data = extractGroupMetadata(res)
-  console.log(`Metadata for ${grupos[i]}: `, data)
+//for (let i = 0; i < grupos.length; i++) {
+//  const [, code] = grupos[i].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
+//  const res = conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code } }] })
+//  const data = extractGroupMetadata(res)
+//  console.log(`Metadata for ${grupos[i]}: `, data)
+//}
+
+const extractGroupMetadata = (result) => {
+    const group = baileys.getBinaryNodeChild(result, 'group');
+    const descChild = baileys.getBinaryNodeChild(group, 'description');
+    let desc;
+    if (descChild) desc = baileys.getBinaryNodeChild(descChild, 'body')?.content;
+    const metadata = {
+      id: group.attrs.id.includes('@') ? group.attrs.id : baileys.jidEncode(group.attrs.id, 'g.us'),
+      subject: group.attrs.subject,
+      creation: new Date(+group.attrs.creation * 1000).toLocaleString('id', { timeZone: 'Asia/Jakarta' }),
+      owner: group.attrs.creator ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.creator).split('@')[0] : undefined,
+      desc
+    };
+    return metadata;
+  };
+
+  let metadataObjects = {};
+  
+  for (let i = 0; i < grupos.length; i++) {
+    let [, code] = grupos[i].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || [];
+    let res = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code } }] });
+    let metadata = extractGroupMetadata(res[0]);
+    metadataObjects[nombre[i]] = metadata;
+  }
+
+  console.log(metadataObjects);
 }
+
+getGroupMetadata();
+	
+
 
 let str = `
 
@@ -72,18 +104,3 @@ conn.sendHydrated(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄
 
 handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupsgb$/i
 export default handler
-
-const extractGroupMetadata = (result) => {
-	const group = baileys.getBinaryNodeChild(result, 'group')
-	const descChild = baileys.getBinaryNodeChild(group, 'description')
-	let desc
-	if (descChild) desc = baileys.getBinaryNodeChild(descChild, 'body')?.content
-	const metadata = {
-		id: group.attrs.id.includes('@') ? group.attrs.id : baileys.jidEncode(group.attrs.id, 'g.us'),
-		subject: group.attrs.subject,
-		creation: new Date(+group.attrs.creation * 1000).toLocaleString('id', { timeZone: 'Asia/Jakarta' }),
-		owner: group.attrs.creator ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.creator).split('@')[0] : undefined,
-		desc
-	}
-	return metadata
-}
