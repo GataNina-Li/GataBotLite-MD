bb82bf73780a5577eaf849e73863235862411a4b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-05 17:52:10 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,27 +1,37 @@
 import * as baileys from '@adiwajshing/baileys'
-
 const grupo = [nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt2, nnnttt3, nnnttt4]
+
 let handler = async (m, { conn, usedPrefix , command }) => { 
-let enlace = ''	
+let enlace1 = ''	
+let enlace2 = ''	
+let enlace3 = ''	
 	
 async function grupo1() {	
 let [_1, code] = grupo[0].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
 let res1 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code } }] })
 let data1 = extractGroupMetadata(res1)
-enlace = data1.subject
+enlace1 = data1.subject
+}
+	
+async function grupo2() {	
+let [_1, code] = grupo[1].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
+let res1 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code } }] })
+let data1 = extractGroupMetadata(res1)
+enlace2 = data1.subject
+}
+	
+async function grupo3() {	
+let [_1, code] = grupo[2].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
+let res1 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code } }] })
+let data1 = extractGroupMetadata(res1)
+enlace3 = data1.subject
 }
 
-//let [_1, code_] = grupo[0].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
-//let [_2, code__] = grupo[0].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
-
-//let res1 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code_ } }] })
-//let res2 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code__ } }] })
-
-//let data1 = extractGroupMetadata(res1)
-//let data2 = extractGroupMetadata(res2)
 
 let str = `
-${enlace}
+${enlace1}
+${enlace2}
+${enlace3}
 `.trim()
 
 /*let str = `
