7064468690f8e15c68aea1e8b05740203d738bda
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-04 23:00:07 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,9 +4,9 @@ const grupo = [nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt2, nnnttt3, nnnttt4]
 let handler = async (m, { conn, usedPrefix , command }) => { 	
 
 let [, code1] = grupo[0].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
-let [, code2] = grupo[1].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
-let [, code3] = grupo[2].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
-let [, code4] = grupo[3].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
+//let [, code2] = grupo[1].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
+//let [, code3] = grupo[2].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
+//let [, code4] = grupo[3].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
 //let [, code5] = grupo[4].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
 //let [, code6] = grupo[5].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
 //let [, code7] = grupo[6].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
@@ -14,9 +14,9 @@ let [, code4] = grupo[3].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{2
 //let [, code9] = grupo[8].match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
 
 let res1 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code1 } }] })
-let res2 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code2 } }] })
-let res3 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code3 } }] })
-let res4 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code4 } }] })
+//let res2 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code2 } }] })
+//let res3 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code3 } }] })
+//let res4 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code4 } }] })
 //let res5 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code5 } }] })
 //let res6 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code6 } }] })
 //let res7 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code7 } }] })
@@ -24,9 +24,9 @@ let res4 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to
 //let res9 = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code9 } }] })
 
 let data1 = extractGroupMetadata(res1)
-let data2 = extractGroupMetadata(res2)
-let data3 = extractGroupMetadata(res3)
-let data4 = extractGroupMetadata(res4)
+//let data2 = extractGroupMetadata(res2)
+//let data3 = extractGroupMetadata(res3)
+//let data4 = extractGroupMetadata(res4)
 //let data5 = extractGroupMetadata(res5)
 //let data6 = extractGroupMetadata(res6)
 //let data7 = extractGroupMetadata(res7)
@@ -42,26 +42,9 @@ let str = `
 ┆ ⭔ *owner* ${data1.owner}
 ◎⇢ ${grupo[0]}
 
-◎⇢ ${data2.subject}
-┆ ⭔ *id* ${data2.id}
-┆ ⭔ *creado* ${data2.creation}
-┆ ⭔ *owner* ${data2.owner}
-◎⇢ ${grupo[1]}
-
-◎⇢ ${data3.subject}
-┆ ⭔ *id* ${data3.id}
-┆ ⭔ *creado* ${data3.creation}
-┆ ⭔ *owner* ${data3.owner}
-◎⇢ ${grupo[2]}
-
-◎⇢ ${data4.subject}
-┆ ⭔ *id* ${data4.id}
-┆ ⭔ *creado* ${data4.creation}
-┆ ⭔ *owner* ${data4.owner}
-◎⇢ ${grupo[3]}
 `.trim()
   
-await conn.sendButton(m.chat, wm, text, img, [[lenguajeGB.smsConMenu(), usedPrefix  + 'menu']], m)
+await conn.sendButton(m.chat, wm, str, img, [[lenguajeGB.smsConMenu(), usedPrefix  + 'menu']], m)
 }
 
 handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupsgb$/i
