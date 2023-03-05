29726bcc2dd1a82068246e3b69bcfbffd65502ad
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-05 18:58:03 -0500

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let handler = async function (m, { conn, __dirname }) {
 try{
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-await conn.reply(m.chat, `*_${_package.homepage}_*`, m, { contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: lenguajeGB.smsCreApoyo(), previewType: 1, thumbnail: await(await fetch(gataMenu.getRandom())), sourceUrl: md}}})
+await conn.reply(m.chat, `*_${_package.homepage}_*`, m, { contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: lenguajeGB.smsCreApoyo(), previewType: 1, thumbnail: gataMenu.getRandom(), sourceUrl: md}}})
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
