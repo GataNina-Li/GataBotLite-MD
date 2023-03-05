bea0bcb9ef492d0833d0c17bd6138425a8378623
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-05 18:32:08 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,4 +1,3 @@
-import * as baileys from '@adiwajshing/baileys'
 const grupo = [nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt2, nnnttt3, nnnttt4, nnnttt5]
 
 let handler = async (m, { conn, usedPrefix , command }) => { 
