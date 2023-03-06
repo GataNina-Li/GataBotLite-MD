cf3dfc6e94acc180dbe03639e287f707b7f8c007
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-06 15:39:19 -0500

    Update info-grupos-lista.js

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -1,16 +1,18 @@
 let handler = async (m, { conn, isOwner }) => {
-	let groups = Object.values(await conn.groupFetchAllParticipating()),
-		txt = ` ${gt} _ESTÁ EN ESTOS GRUPOS_\n_IS IN THESE GROUPS:_\n\n*✦ Total de Grupos:* ${groups.length}\n\n`
-	for (let i = 0; i < groups.length; i++) {
-		txt += `*✦ Grupo:* ${groups[i].subject}\n`
-			+ `*✦ ID:* ${groups[i].id}\n`
-			+ `${isOwner ? `*✦ Participantes:* ${groups[i].participants.length}\n` : ''}`
-			+ `${isOwner ? `*✦ Bot Admin:* ${!!groups[i].participants.find(v => v.id == conn.user.jid).admin}\n` : ''}┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n`
-	}
-	m.reply(txt.trim())
+let groups = Object.values(await conn.groupFetchAllParticipating()),
+txt = `${packname} _ESTÁ EN ESTOS GRUPOS:_
+
+*⭔ Total de Grupos:* ${groups.length}\n\n`
+
+for (let i = 0; i < groups.length; i++) {
+txt += `
+*⋄ Grupo:* ${groups[i].subject}
+*⋄ ID:* ${groups[i].id}
+${isOwner ? `*⋄ Participantes:* ${groups[i].participants.length}\n` : ''}
+${isOwner ? `*⋄ Bot Admin:* ${!!groups[i].participants.find(v => v.id == conn.user.jid).admin == true ? '✅' : '❌'}\n` : ''}┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n`
 }
-handler.help = ['groups', 'grouplist']
-handler.tags = ['info']
+m.reply(txt.trim())
+}
 handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos|listadegrupos|grupolista|listagrupo)$/i
 
 export default handler
