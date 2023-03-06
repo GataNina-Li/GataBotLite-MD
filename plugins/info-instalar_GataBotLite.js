246daf92670a62d594d39c2ca1b422fa11393278
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-06 14:31:36 -0500

    Update info-instalar_GataBotLite.js

--- plugins/info-instalar_GataBotLite.js
+++ plugins/info-instalar_GataBotLite.js
@@ -86,7 +86,6 @@ _◊ Cree su cuenta en Heroku, si ya tiene cuenta use el segundo Botón._`
 
 let buttons3 = [ 
 {index: 1, urlButton: {displayText: 'G I T H U B', url: md}},
-{index: 2, urlButton: {displayText: 'C R E A R  C U E N T A', url: 'https://id.heroku.com/login'}},
 {index: 3, urlButton: {displayText: 'H E R O K U', url: 'https://heroku.com/deploy?template=https://github.com/GataNina-Li/GataBotMDLite-Heroku'}},
 {index: 4, urlButton: {displayText: 'B U I L D P A C K', url: 'https://www.whatsapp.com/otp/copy/' + codigo2}},
 {index: 5, quickReplyButton: {displayText: lenguajeGB.smsConMenu(), id: `${usedPrefix}menu`}},
