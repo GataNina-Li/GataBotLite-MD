f01c9ef9c5a586732251f583bacc0ebfed4b6202
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-05 19:37:58 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 import fs from 'fs'
 
 let handler = async function (m, { conn, __dirname, usedPrefix, command }) {
-try{
+try{ 
 let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCreApoyo(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(gataMenu.getRandom())).buffer() }}}
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}  
 await conn.sendButton(m.chat, `*_${_package.homepage}_*`, lenguajeGB.smsPrivadoDonar(), img5, [[lenguajeGB.smsCreInfoBot(), usedPrefix  + `${lenguajeGB.lenguaje() == 'es' ? 'creadora' : 'owner'}`]], m, frep)  
