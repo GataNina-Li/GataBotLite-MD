5e6f3ee5d811ad64856f64ef2b9bd398e61bb0b0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-06 16:17:45 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -9,17 +9,17 @@ let uptime = clockString(_uptime)
 let totalreg = Object.keys(global.db.data.users).length
 let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
 
-await conn.sendButton(m.chat, wm, `
+await conn.sendButton(m.chat, wm.trim(), `
 *╭𝄗𝄗✦ --ESTADO-- ✦𝄗𝄗⬣*
  ⎸ *⍟ Versión *
  ⎸ ❉ ➺ ${vs}
  ⎸ ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
- ⎸ ❉ *Usuario(s)* ➺ ${Object.keys(global.db.data.users).length} 
- ⎸ ❉ *Registrados(as)* ➺ ${rtotalreg}/${totalreg}
- ⎸ ❉ *Chat(s) Prohibido(s)* ➺ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
- ⎸ ❉ *Usuario(s) Prohibido(s)* ➺ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
+ ⎸ ❉ *Usuarios/as* ➺                *_${Object.keys(global.db.data.users).length}_*
+ ⎸ ❉ *Registrados/as* ➺             *_${rtotalreg}/${totalreg}_*
+ ⎸ ❉ *Chat/s Prohibido/s* ➺         *_${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}_* 
+ ⎸ ❉ *Usuarios/as Prohibidos/as)* ➺ *_${Object.entries(global.db.data.users).filter(user => user[1].banned).length}_* 
  ⎸ ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
- ⎸ ❉ *Activa durante:*    *_${uptime}_*
+ ⎸ ❉ *Activa durante* ➺            *_${uptime}_*
 *╰𝄗𝄗𝄗𝄗𝄗𝄗𝄗⬣*`, gataImg.getRandom(), [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], m)
 }
 handler.command = /^(estado|status|estate|state|stado|stats|botstat(us)?)$/i
