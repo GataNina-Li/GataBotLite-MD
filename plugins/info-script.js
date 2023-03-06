7ca5f8a1157272f31d68709612f3f157af194a5c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-05 19:32:20 -0500

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -6,7 +6,7 @@ let handler = async function (m, { conn, __dirname, usedPrefix, command }) {
 try{
 let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCreApoyo(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(gataMenu.getRandom())).buffer() }}}
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}  
-await conn.sendButton(m.chat, `*_${_package.homepage}_*`, lenguajeGB.smsPrivadoDonar(), img5, [[lenguajeGB.smsCreInfoBot(), usedPrefix  + `${lenguajeGB.lenguaje() == 'es' ? 'creadora' : 'owner'}`]], m)  
+await conn.sendButton(m.chat, `*_${_package.homepage}_*`, lenguajeGB.smsPrivadoDonar(), img5, [[lenguajeGB.smsCreInfoBot(), usedPrefix  + `${lenguajeGB.lenguaje() == 'es' ? 'creadora' : 'owner'}`]], m, frep)  
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
