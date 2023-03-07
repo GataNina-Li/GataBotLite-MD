4bd741048c8db48733ebfb758b73effb686bddc9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-07 16:24:23 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -53,5 +53,4 @@ function clockString(ms) {
 let h = Math.floor(ms / 3600000)
 let m = Math.floor(ms / 60000) % 60
 let s = Math.floor(ms / 1000) % 60
-console.log({ms,h,m,s})
 return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
