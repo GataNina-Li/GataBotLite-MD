5d584add02651e6ad3682e8be5c5275fe0594044
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-03-07 19:34:43 -0300

    Add files via upload

---
+++ plugins/fun-formarpareja.js
@@ -0,0 +1,16 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)}, 𝙔𝙖 𝙚𝙨 𝙝𝙤𝙧𝙖 𝙙𝙚 𝙦𝙪𝙚 𝙩𝙚 💍 𝘾𝙖𝙨𝙚𝙨 𝙘𝙤𝙣 ${toM(b)}, 𝙇𝙞𝙣𝙙𝙖 𝙋𝙖𝙧𝙚𝙟𝙖 😉💓*`, null, {
+mentions: [a, b]
+  
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['formarpareja','formarparejas']
+handler.group = true
+export default handler
