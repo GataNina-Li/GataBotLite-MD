c66ef3557c3174ee36f53fc6c06bf324ae9dad58
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-08 14:33:24 -0500

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -52,32 +52,45 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     const results = response.data.results;
     const primerResultado = results[0];
     
-    let resultadoEnBruto = '\n';
+    let resultadoEnBruto = '';
     for (let prop in primerResultado.header) {
-      resultadoEnBruto += `${prop}: ${primerResultado.header[prop]}\n`;
+      resultadoEnBruto += `${prop}\n${primerResultado.header[prop]}\n`;
     }
     resultadoEnBruto += '\n';
     for (let prop in primerResultado.data) {
-      resultadoEnBruto += `${prop}: ${primerResultado.data[prop]}\n`;
+      resultadoEnBruto += `${prop}\n${primerResultado.data[prop]}\n`;
     }
     
     let enlace = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: ig, mediaType: 'VIDEO', description: '', title: wm, body: '😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(primerResultado.header.thumbnail)).buffer(), sourceUrl: ig }}}
-    await conn.sendButton(m.chat, wm, `
-      Número de resultados: ${results.length}
-      Resultados encontrados? ${Boolean(results)}
+await conn.sendButton(m.chat, wm, `
+Número de resultados: ${results.length}
+Resultados encontrados? ${Boolean(results)}
 
-      Límites:
-      Solicitudes restantes (corto plazo): ${response.data.short_remaining} 
-      Solicitudes restantes (largo plazo): ${response.data.long_remaining}
+LÍMITES 
+Solicitudes restantes (corto plazo) 
+${results.short_remaining} 
 
-      Resultado:
-      URL de la miniatura: ${primerResultado.header.thumbnail}
-      Puntuación de similitud: ${primerResultado.header.similarity}
-      Título: ${primerResultado.data.title}
-      URLs: ${primerResultado.data.ext_urls}
-      Autor: ${primerResultado.data.member_name}
-      Resultado en bruto:
-      ${resultadoEnBruto}
+Solicitudes restantes (largo plazo)
+${results.long_remaining}
+
+RESULTADO
+URL de la miniatura
+${primerResultado.header.thumbnail}
+
+Puntuación de similitud
+${primerResultado.header.similarity}
+
+Título
+${primerResultado.data.title}
+
+URLs
+${primerResultado.data.ext_urls}
+
+Autor
+${primerResultado.data.member_name}
+
+RESULTADO EN BRUTO
+${resultadoEnBruto}
     `.trim(), image_url, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, enlace)
   } catch (error) {
     console.log(error);
