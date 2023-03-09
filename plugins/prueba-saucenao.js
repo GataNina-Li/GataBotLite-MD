b07c3c08da9ab962eefbe7d8ca047d92e45c64e9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-08 21:25:34 -0500

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -12,13 +12,13 @@ const image_url = 'https://i.imgur.com/oZjCxGo.jpg'
 try {
 //let regex = /\.(jpg|jpeg|png)$/i
 
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) return m.reply('No se puede')
-let media = await q.download()
-let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+//let q = m.quoted ? m.quoted : m
+//let mime = (q.msg || q).mimetype || ''
+//if (!mime) return m.reply('No se puede')
+//let media = await q.download()
+//let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 //while (!link) {
-let link = await (isTele ? uploadImage : uploadFile)(media)
+//let link = await (isTele ? uploadImage : uploadFile)(media)
 //}
 
 //if (!text) return m.reply('INGRESE EL ENLACE DE UNA IMAGEN QUE TERMINE EN jpg, jpeg o png')
@@ -132,7 +132,7 @@ await conn.sendButton(m.chat, `*Número de resultados:* ${results.length}
 *Autor*
 • ${primerResultado.data.member_name === undefined ? 'No encontrado' : primerResultado.data.member_name}\n`,  `*◎ R E S U L T A D O  E N  B R U T O*
 
-${resultadoEnBruto}`.trim(), link, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, frep)
+${resultadoEnBruto}`.trim(), 'https://telegra.ph/file/405daebd4bc0d69e5d165.mp4', [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, frep)
 } catch (error) {
 console.log(error);
 }}
