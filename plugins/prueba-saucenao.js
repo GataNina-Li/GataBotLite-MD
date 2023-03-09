74656a110a352b90ef5b8dbc8561f94518cde42d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-08 23:36:20 -0500

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -23,9 +23,10 @@ url = await uploadImage(media)
     
 } else if (m.quoted && /image\/webp/.test(mime)) {
 let media = await q.download()
-let img = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
+let mediaUint8Array = new TextEncoder().encode(media)
+let img = await webp2png(mediaUint8Array).catch(_ => null) || Buffer.alloc(0)
 url = await uploadImage(img)
-    
+   
 } else {
 return m.reply('Ingrese un enlace o responda al mensaje con una imagen en formato PNG o JPG o JPEG.');
 }
