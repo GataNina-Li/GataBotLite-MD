2eb48cbbe458859b6d2b413fc8f4fe78779f9be2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-08 22:33:09 -0500

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -7,25 +7,26 @@ import uploadImage from '../lib/uploadImage.js'
 
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
 const api_key = '45e67c4cbc3d784261ffc83806b5a1d7e3bd09ae'
-const image_url = 'https://i.imgur.com/oZjCxGo.jpg'
 
 try {
-//let regex = /\.(jpg|jpeg|png)$/i
+let url
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
 
-//let q = m.quoted ? m.quoted : m
-//let mime = (q.msg || q).mimetype || ''
-//if (!mime) return m.reply('No se puede')
-//let media = await q.download()
-//let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-//let link = await (isTele ? uploadImage : uploadFile)(media)
+if (text) {
+url = text
+    
+} else if (m.quoted && /image\/(png|jpe?g)/.test(mime)) {
+let media = await q.download()
+url = await uploadImage(media)
+    
+} else {
+return m.reply('Ingrese un enlace o responda al mensaje con una imagen en formato PNG o JPG o JPEG.');
+}
 
-
-//if (!text) return m.reply('INGRESE EL ENLACE DE UNA IMAGEN QUE TERMINE EN jpg, jpeg o png')
-//if (!regex.test(text)) return m.reply('SOLO SE PERMITE ENLACE DE IMAGEN QUE TERMINE EN jpg, jpeg o png')   
-
-const response = await axios.get(`https://saucenao.com/search.php?db=999&output_type=2&testmode=1&numres=6&api_key=${api_key}&url=${encodeURIComponent('https://telegra.ph/file/114d82ab377ccc568076a.jpg')}`)
+const response = await axios.get(`https://saucenao.com/search.php?db=999&output_type=2&testmode=1&numres=6&api_key=${api_key}&url=${encodeURIComponent(url)}`)
 const results = response.data.results;
-const primerResultado = results[0];
+const primerResultado = results[0]
     
 let resultadoEnBruto = ''
 for (let prop in primerResultado.header) {
@@ -94,13 +95,7 @@ propName = prop
 }
 resultadoEnBruto += `*${propName}*\n${primerResultado.data[prop]}\n\n`}
     
-//let enlace = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: md, mediaType: 'IMAGE', description: '', title: wm, body: '😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(img3)).buffer(), sourceUrl: md }}}
-//let frep
-//while (!primerResultado.header.thumbnail) {
 let frep = { contextInfo: { externalAdReply: {title: wm, body: author, sourceUrl: md, thumbnail: await(await fetch(primerResultado.header.thumbnail)).buffer() }}}
-//}
-
-
 await m.reply('*ESPERE UN MOMENTO...*')
 await conn.sendButton(m.chat, `*Número de resultados:* ${results.length}
 *Resultados encontrados:* ${Boolean(results) === true ? 'Si' : 'No'}
@@ -131,10 +126,9 @@ await conn.sendButton(m.chat, `*Número de resultados:* ${results.length}
 *Autor*
 • ${primerResultado.data.member_name === undefined ? 'No encontrado' : primerResultado.data.member_name}\n`,  `*◎ R E S U L T A D O  E N  B R U T O*
 
-${resultadoEnBruto}`.trim(), img3, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, frep)
+${resultadoEnBruto}`.trim(), url, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, frep)
 } catch (error) {
-console.log(error);
-}
+console.log(error)}
 }
 
 handler.command = /^sauce$/i
