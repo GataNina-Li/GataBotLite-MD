ac165919edaf530667b6818d8362f36b5f9bf7f9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-10 16:15:23 -0500

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -24,15 +24,13 @@ ${timestamp}
 ${views}
 
 *◎ URL*
-${url}
-
-${wm}`.trim()
+${url}`.trim()
 		
 let templateButtons = [ 
 {index: 1, urlButton: {displayText: 'VER EN YOUTUBE', url: url}},
-{index: 3, quickReplyButton: {displayText: 'AUDIO', id: `${usedPrefix}yta ${url}`}},
+{index: 2, quickReplyButton: {displayText: 'AUDIO', id: `${usedPrefix}yta ${url}`}},
 {index: 3, quickReplyButton: {displayText: 'VIDEO', id: `${usedPrefix}ytv ${url}`}},
-{index: 3, quickReplyButton: {displayText: lenguajeGB.smsConMenu(), id: `${usedPrefix}menu`}},
+{index: 4, quickReplyButton: {displayText: lenguajeGB.smsConMenu(), id: `${usedPrefix}menu`}},
 ]
 await conn.sendMessage(m.chat, { image: { url: thumbnail }, gifPlayback: false, gifAttribution: ~~(Math.random() * 2), caption: video, footer: wm, templateButtons }, { quoted: m})
 }
