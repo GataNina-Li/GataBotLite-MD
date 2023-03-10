dc8292c34b6b93f58a8ec0a0387b1c7541e7c621
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-10 11:18:16 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,6 +1,7 @@
 const grupo = [nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt2, nnnttt3, nnnttt4, nnnttt5]
 
 let handler = async (m, { conn, usedPrefix , command }) => { 
+try{
 let str = `
 ${lenguajeGB.smsGrupoOfc1()}
 _⭔ ${grupo[0]}_
@@ -25,7 +26,10 @@ _⭔ ${grupo[8]}_
 
 _⭔ ${grupo[9]}_`.trim()
 await conn.sendButton(m.chat, wm, str, gataMenu.getRandom(), [[lenguajeGB.smsConMenu(), usedPrefix  + 'menu']], m)
-}
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
 
 handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupsgb$/i
 export default handler
