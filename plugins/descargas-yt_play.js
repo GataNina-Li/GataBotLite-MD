725ced85adf8181fbaaeac266ed535a793202e1e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-10 16:32:10 -0500

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -6,7 +6,7 @@ let vid = (await yts(text)).all[0]
 if (!vid) throw `${lenguajeGB['smsAvisoFG']()}𝙉𝙊 𝙎𝙀 𝙋𝙐𝘿𝙊 𝙀𝙉𝘾𝙊𝙉𝙏𝙍𝘼𝙍 𝙀𝙇 𝘼𝙐𝘿𝙄𝙊/𝙑𝙄𝘿𝙀𝙊. 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘾𝙊𝙉 𝙊𝙏𝙍𝙊 𝙉𝙊𝙈𝘽𝙍𝙀 𝙊 𝙏𝙄𝙏𝙐𝙇𝙊\n\n𝙏𝙃𝙀 𝘼𝙐𝘿𝙄𝙊/𝙑𝙄𝘿𝙀𝙊 𝘾𝙊𝙐𝙇𝘿 𝙉𝙊𝙏 𝘽𝙀 𝙁𝙊𝙐𝙉𝘿. 𝙏𝙍𝙔 𝘼𝙉𝙊𝙏𝙃𝙀𝙍 𝙉𝘼𝙈𝙀 𝙊𝙍 𝙏𝙄𝙏𝙇𝙀`
 let { title, description, url, thumbnail, videoId, timestamp, views, published } = vid
 
-let video = `*◜Y O U T U B E◞*
+let video = `*◜⋯ ⋯ ⋯Y O U T U B E ⋯ ⋯ ⋯◞*
 
 *◎ TÍTULO*
 ${title}
@@ -28,8 +28,8 @@ ${url}`.trim()
 		
 let templateButtons = [ 
 {index: 1, urlButton: {displayText: 'VER EN YOUTUBE', url: url}},
-{index: 2, quickReplyButton: {displayText: 'AUDIO', id: `${usedPrefix}yta ${url}`}},
-{index: 3, quickReplyButton: {displayText: 'VIDEO', id: `${usedPrefix}ytv ${url}`}},
+{index: 2, quickReplyButton: {displayText: '▢ AUDIO', id: `${usedPrefix}yta ${url}`}},
+{index: 3, quickReplyButton: {displayText: '▢ VIDEO', id: `${usedPrefix}ytv ${url}`}},
 {index: 4, quickReplyButton: {displayText: lenguajeGB.smsConMenu(), id: `${usedPrefix}menu`}},
 ]
 await conn.sendMessage(m.chat, { image: { url: thumbnail }, gifPlayback: false, gifAttribution: ~~(Math.random() * 2), caption: video, footer: wm, templateButtons }, { quoted: m})
