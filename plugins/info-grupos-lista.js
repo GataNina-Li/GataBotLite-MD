9ea94e450f6add911546d0456740f7a6ee9a6469
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-10 11:17:55 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -1,4 +1,5 @@
 let handler = async (m, { conn, isOwner }) => {
+try{
 let groups = Object.values(await conn.groupFetchAllParticipating()),
 txt = `${packname} ${lenguajeGB.smsLisA()}
 
@@ -12,7 +13,10 @@ ${isOwner ? `${lenguajeGB.smsLisE()} ${groups[i].participants.length}` : ''}
 ${isOwner ? `${lenguajeGB.smsLisF()} ${!!groups[i].participants.find(v => v.id == conn.user.jid).admin == true ? '✅' : '❌'}\n` : ''}`
 }
 m.reply(txt.trim())
-}
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
 handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos|listadegrupos|grupolista|listagrupo)$/i
 
 export default handler
