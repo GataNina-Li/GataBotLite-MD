4d6634a89cb0ed79dff2a7ac8f586c45d14a7a46
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-12 23:31:36 -0500

    Update info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -42,12 +42,12 @@ for (let i = 0; i < contacts.length; i++) {
   let number = String(contact[0])
   let name = await conn.getName(number+'@s.whatsapp.net') 
   
-let contacts = [
+let lista = [
   [number1, await conn.getName(number1+'@s.whatsapp.net'), '💖 Creadora', 'Solo temas de GataBot', 'centergatabot@gmail.com', '🇪🇨 Ecuador', '🎁 https://github.com/GataNina-Li', '🐱 GataNina-Li'],
   [conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), '🐈 Bot de WhatsApp', '📵 No hacer Spam por favor', 'gatabot@gmail.com', '🇪🇨 Ecuador', '🎁 ' + md, packname]
 ]
 
-await conn.sendContactArray(m.chat, contacts, null, { quoted: m })
+await conn.sendContactArray(m.chat, lista, null, { quoted: m })
 }
 
 await m.reply(`Hola @${m.sender.split(`@`)[0]} Contactos disponibles`)
