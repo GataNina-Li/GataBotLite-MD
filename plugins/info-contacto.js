3d8f4652299cb85908f8508047f546f4cbea3259
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-12 22:48:15 -0500

    Update info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -20,7 +20,7 @@ let name = await conn.getName(who)
 //], fkontak)
 //await m.reply(`ʜᴇʟʟᴏ @${m.sender.split(`@`)[0]} Contactos disponibles`)
 
-let contacts = global.owner.filter(c => c[2] === true)
+/*let contacts = global.owner.filter(c => c[2] === true)
 let numero = contacts.map(c => c[0])
 let nombres = []
 
@@ -32,9 +32,38 @@ for (let n of numero) {
 const sentMsg = await conn.sendContactArray(m.chat, [
 [`${numero[0]}`, `${nombres[0].notify}`, `💖 Creadora `, `Solo temas de GataBot`, `centergatabot@gmail.com`, `🇪🇨 Ecuador`, '🎁 https://github.com/GataNina-Li', `🐱 GataNina-Li`],
 [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🐈 Bot de WhatsApp`, `📵 No hacer Spam por favor`, `gatabot@gmail.com`, `🇪🇨 Ecuador`, '🎁 ' + md, packname]
-], fkontak)
+], fkontak)*/
 
-  await m.reply(`Hola @${m.sender.split(`@`)[0]} Contactos disponibles`)
+let contacts = global.owner.filter(c => c[2] === true)
+let numbers = contacts.map(c => c[0])
+let names = await conn.getName(numbers)
+
+for (let i = 0; i < contacts.length; i++) {
+  let contact = contacts[i]
+  let number = contact[0]
+  let name = names[i].notify
+
+  let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${contact[1] || ''}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${contact[3] || ''}
+item2.EMAIL;type=INTERNET:${contact[4] || ''}
+item2.X-ABLabel:📧 Email
+item3.ADR:;;${contact[5] || ''};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:📍 Region
+item4.URL:${contact[6] || ''}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${contact[7] || ''}
+END:VCARD`.trim()
+
+await conn.sendContactArray(m.chat, [[number, name, '💖 Creadora', 'Solo temas de GataBot', 'centergatabot@gmail.com', '🇪🇨 Ecuador', '🎁 https://github.com/GataNina-Li', '🐱 GataNina-Li']], null, { thumbnail: Buffer.from(contact[8] || '', 'base64'), quoted: m })
+}
+await m.reply(`Hola @${m.sender.split(`@`)[0]} Contactos disponibles`)
 } 
 handler.command = ['contacto', 'contact']  
 export default handler
