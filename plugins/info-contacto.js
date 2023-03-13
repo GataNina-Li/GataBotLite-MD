84d623be4e2d9922a590c2f8c67e6b1d1b93994d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-12 23:44:14 -0500

    Update info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -8,34 +8,9 @@ handler.command = ['contacto', 'contact']
 export default handler*/
 
 import fetch from 'node-fetch'
-import PhoneNumber from 'awesome-phonenumber'
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await conn.getName(who)
-
-//const sentMsg = await conn.sendContactArray(m.chat, [
-//[`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💖 Creadora `, `Solo temas de GataBot`, `centergatabot@gmail.com`, `🇪🇨 Ecuador`, '🎁 https://github.com/GataNina-Li', `🐱 GataNina-Li`],
-//[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🐈 Bot de WhatsApp`, `📵 No hacer Spam por favor`, `gatabot@gmail.com`, `🇪🇨 Ecuador`, '🎁 ' + md, packname]
-//], fkontak)
-//await m.reply(`ʜᴇʟʟᴏ @${m.sender.split(`@`)[0]} Contactos disponibles`)
-
-/*let contacts = global.owner.filter(c => c[2] === true)
-let numero = contacts.map(c => c[0])
-let nombres = []
-
-for (let n of numero) {
-  let nombre = await conn.getName(n)
-  nombres.push(nombre)
-}
-
-const sentMsg = await conn.sendContactArray(m.chat, [
-[`${numero[0]}`, `${nombres[0].notify}`, `💖 Creadora `, `Solo temas de GataBot`, `centergatabot@gmail.com`, `🇪🇨 Ecuador`, '🎁 https://github.com/GataNina-Li', `🐱 GataNina-Li`],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🐈 Bot de WhatsApp`, `📵 No hacer Spam por favor`, `gatabot@gmail.com`, `🇪🇨 Ecuador`, '🎁 ' + md, packname]
-], fkontak)*/
-
 let contacts = global.owner.filter(c => c[2] === true)
 let lista = []
 
@@ -44,13 +19,10 @@ for (let i = 0; i < contacts.length; i++) {
   let number = String(contact[0])
   let name = await conn.getName(number+'@s.whatsapp.net')
   
-  lista.push([number, name, '💖 Creadora', 'Solo temas de GataBot', 'centergatabot@gmail.com', '🇪🇨 Ecuador', '🎁 https://github.com/GataNina-Li', '🐱 GataNina-Li'])
-}
-
+lista.push([number, name, '💖 Creadora', 'Solo temas de GataBot', 'centergatabot@gmail.com', '🇪🇨 Ecuador', '🎁 https://github.com/GataNina-Li', '🎁 https://github.com/GataNina-Li', '🐱 GataNina-Li'])}
 lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), '🐈 Bot de WhatsApp', '📵 No hacer Spam por favor', 'gatabot@gmail.com', '🇪🇨 Ecuador', '🎁 ' + md, packname])
 
 await conn.sendContactArray(m.chat, lista, null, { quoted: m })
-
 await m.reply(`Hola @${m.sender.split(`@`)[0]} Contactos disponibles`)
 } 
 handler.command = ['contacto', 'contact']  
