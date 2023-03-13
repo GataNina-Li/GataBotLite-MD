2a8a7b3c4f38ee7ff89974d299b25f70fe8a048b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-13 00:34:28 -0500

    Update info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -11,6 +11,7 @@ export default handler*/
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let nombre, description, correo, lugar, enlace, otro
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let contacts = global.owner.filter(c => c[2] === true)
 let lista = []
@@ -20,7 +21,15 @@ for (let i = 0; i < contacts.length; i++) {
   let number = String(contact[0])
   let name = await conn.getName(m.number+'@s.whatsapp.net')
   
-lista.push([number, name, '💖 Creadora', 'Solo temas de GataBot', 'centergatabot@gmail.com', '🇪🇨 Ecuador', '🎁 https://github.com/GataNina-Li', '🐱 GataNina-Li'])}
+nombre = 18059196237 == String(contact[0]) ? 'Gata Dios' : 5492266613038 == String(contact[0]) ? 'Owner Oficial (1)' : 5214531173598 == String(contact[0]) ? 'Owner Oficial (1)' : 'Owner' 
+description = 18059196237 == String(contact[0]) ? 'Solo temas de GataBot' : 5492266613038 == String(contact[0]) ? 'Escríbeme si tiene dudas sobre GataBot' : 5214531173598 == String(contact[0]) ? 'Escríbeme si tiene dudas sobre GataBot' : 'Colaborador' 
+correo = 18059196237 || 5492266613038 || 5214531173598  == String(contact[0]) ? 'centergatabot@gmail.com' : 'No definido'
+lugar = 18059196237 == String(contact[0]) ? '🇪🇨 Ecuador' : 5492266613038 == String(contact[0]) ? '🇦🇷 Argentina' : 5214531173598 == String(contact[0]) ? '🇲🇽 México' : '🌎 Global' 
+enlace = 18059196237 == String(contact[0]) ? 'https://github.com/GataNina-Li' : 5492266613038 == String(contact[0]) ? 'https://github.com/elrebelde21' : 5214531173598 == String(contact[0]) ? 'https://github.com/Azami19' : md 
+otro = 18059196237 == String(contact[0]) ? '1' : 5492266613038 == String(contact[0]) ? '2' : 5214531173598 == String(contact[0]) ? '3' : '4' 
+  
+  
+lista.push([number, name, nombre, description, correo, lugar, enlace, otro])}
 lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), '🐈 Bot de WhatsApp', '📵 No hacer Spam por favor', 'gatabot@gmail.com', '🇪🇨 Ecuador', '🎁 ' + md, packname])
 
 await conn.sendContactArray(m.chat, lista, null, { quoted: m })
