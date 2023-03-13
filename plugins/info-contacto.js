0fef4214747b43b2b42c212b459ed6472ca2d293
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-12 22:54:08 -0500

    Update info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -8,6 +8,8 @@ handler.command = ['contacto', 'contact']
 export default handler*/
 
 import fetch from 'node-fetch'
+import PhoneNumber from 'awesome-phonenumber'
+
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
