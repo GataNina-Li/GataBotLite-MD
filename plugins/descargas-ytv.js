7bf68b71da033bccaad7fb23233b55a08615f3c1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-17 23:40:34 -0500

    Update descargas-ytv.js

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -4,20 +4,25 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw lenguajeGB.smsMalused2() + `*${usedPrefix + command} https://youtu.be/ejemplo*\n*${usedPrefix + command} https://www.youtube.com/ejemplo*`
 await conn.reply(m.chat, lenguajeGB.smsAvisoEG() + '*' + lenguajeGB.smsYTV1() + '*', m)
 try {
-let qu = args[1] || '720'
 let qualities = ['1080', '720', '480', '360', '240', '144'];
-let q = qu + 'p';
-let v = args[0];
+let q = '';
 let yt = null;
-let i = 0;
-while (!yt && i < qualities.length) {
+let dl_url = null;
+let ttl = null;
+let size = null;
+
+for (let i = 0; i < qualities.length; i++) {
   q = qualities[i] + 'p';
   yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v));
-  i++;
+  if (yt.video[q] && yt.video[q].fileSizeH) {
+    dl_url = await yt.video[q].download();
+    ttl = await yt.title;
+    size = await yt.video[q].fileSizeH;
+    break;
+  }
 }
-const dl_url = await yt.video[q].download();
-const ttl = await yt.title;
-const size = yt.video[q].fileSizeH || 'Desconocido';
+
+if (dl_url) {
 await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*💫 ${ttl}*\n*⚖️ ${size}*`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m });
 } catch {
 try {
