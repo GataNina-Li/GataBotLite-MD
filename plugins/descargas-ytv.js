fc53caf5965af489c33e47f76ea16a5421b3feda
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-17 23:33:05 -0500

    Update descargas-ytv.js

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -4,14 +4,31 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw lenguajeGB.smsMalused2() + `*${usedPrefix + command} https://youtu.be/ejemplo*\n*${usedPrefix + command} https://www.youtube.com/ejemplo*`
 await conn.reply(m.chat, lenguajeGB.smsAvisoEG() + '*' + lenguajeGB.smsYTV1() + '*', m)
 try {
-let qu = args[1] || '360'
-let q = qu + 'p'
-let v = args[0]
-const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
-const dl_url = await yt.video[q].download()
-const ttl = await yt.title
-const size = await yt.video[q].fileSizeH
-await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*💫 ${ttl}*\n*⚖️ ${size}*`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+let qu = args[1] || '720';
+let sizes = ['1080p', '720p', '480p', '360p', '240p', '144p'];
+let i = 0;
+let size = null;
+let dl_url = null;
+let ttl = null;
+
+while (size == null && i < sizes.length) {
+  let q = sizes[i];
+  const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v));
+  dl_url = await yt.video[q].download();
+  ttl = await yt.title;
+  size = await yt.video[q].fileSizeH;
+  i++;
+}
+
+if (size == null) {
+  q = '360p';
+  const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v));
+  dl_url = await yt.video[q].download();
+  ttl = await yt.title;
+  size = await yt.video[q].fileSizeH;
+}
+
+await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*💫 ${ttl}*\n*⚖️ ${size}*`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m });
 } catch {
 try {
 let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${args[0]}`)    
