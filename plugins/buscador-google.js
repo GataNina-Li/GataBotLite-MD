de67d8de8426cc4e03d476872c8a1492a20e2a72
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-18 23:16:26 -0500

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -5,7 +5,7 @@ let full = /f$/i.test(command)
 let text = args.join` `
 if (!text) return conn.reply(m.chat, lenguajeGB.smsAvisoMG + '*ESCRIBA LO QUE QUIERE BUSCAR, EJEMPLO:*' + `\n*${usedPrefix + command} Qué es Matemáticas?*` , m)
 let url = 'https://google.com/search?q=' + encodeURIComponent(text)
-let search = await googleIt(text, { limit: 10 });
+let search = await googleIt(text, { limit: 30 });
 let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
 m.reply(msg)
 }
