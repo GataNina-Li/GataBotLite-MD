0162e81435959aa8054b00139a9106e1e73ae63e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-18 22:38:48 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/_antivirus.js
+++ plugins/_antivirus.js
@@ -1,6 +1,6 @@
 let handler = m => m
 
-handler.all = async function (m, { isBotAdmin }) {
+handler.all = async function (m, { isBotAdmin }) { 
 // borrado automático cuando hay un mensaje que no se puede ver en el escritorio | auto clear ketika terdapat pesan yang tidak dapat dilihat di wa desktop 
 if (m.messageStubType === 68) {
 let log = {
