5c9eee49ed14f6774cd33dc881f14d57348322d8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-18 23:07:32 -0500

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -1,21 +1,17 @@
 import { googleIt } from '@bochilteam/scraper'
 let handler = async (m, { conn, command, args, usedPrefix, text }) => {
+const fetch = (await import('node-fetch')).default
+let full = /f$/i.test(command)
+let text = args.join` `
 if (!text) return conn.reply(m.chat, lenguajeGB.smsAvisoMG + '*ESCRIBA LO QUE QUIERE BUSCAR, EJEMPLO:*' + `\n*${usedPrefix + command} Qué es Matemáticas?*` , m)
-async function searchGoogle(query) {
+let url = 'https://google.com/search?q=' + encodeURIComponent(text)
+let search = await googleIt(text)
+let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
 try {
-const results = await googleIt({ query });
-m.reply(`Resultados para '${query}':`);
-results.forEach((result, index) => {
-m.reply(`${index + 1}. ${result.title} (${result.link})`);
-});
-} catch (error) {
-console.error(error);
-}
-}
-const texto = { myquery: text }; 
-const query = texto.toString(); 
-searchGoogle(query);
-}
+let ss = await (await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=${lolkeysapi}&url=${url}`)).arrayBuffer()
+await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
+} catch {
+m.reply(msg)
+}}
 handler.command = /^googlef?$/i
-handler.register = true
 export default handler
