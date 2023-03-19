6e7165d3479de885374993eb44809ac0d28b04ba
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-18 22:40:58 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -9,7 +9,7 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 7000) }) * 1000}
 
 let uptime = clockString(_uptime)
-let bio = `${global.packname} ║ ✅ ${uptime} ⌛ ║ ⒼⒷ 𓃠 #estado #menu #jadibot #grupos #owner 💻`
+let bio = `${global.packname} ⁝⁝ ✅ ${uptime} ⌛ ⁝⁝ 𓃠 #estado #menu #jadibot #grupos #owner 💻`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 }
