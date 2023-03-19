e49995da81bdfda1091bc7b2baa6f112544b548f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-18 22:19:20 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -1,12 +1,13 @@
 import fetch from "node-fetch"
 import yts from 'yt-search'
+
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-try{
 if (!text) throw lenguajeGB.smsMalused2() + `*${usedPrefix + command} Billie Eilish - Bellyache*`
 let vid = (await yts(text)).all[0]
 if (!vid) throw lenguajeGB.smsMalError2() + `_${lenguajeGB.smsYT6()}_`
-let { title, description, url, thumbnail, videoId, timestamp, views, published } = vid
 
+try{
+let { title, description, url, thumbnail, videoId, timestamp, views, published } = vid
 let video = `*◜⋯ ⋯ ⋯ Y O U T U B E ⋯ ⋯ ⋯◞*
 
 *◎ ${lenguajeGB.smsYT1()}*
