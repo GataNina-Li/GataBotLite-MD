da6892853893fd12b0171dc52c2abc9dc1cee3d3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-19 18:06:37 -0500

    Update buscador-openai.js

--- plugins/buscador-openai.js
+++ plugins/buscador-openai.js
@@ -2,9 +2,9 @@ import fetch from 'node-fetch'
 let handler = async (m, { text, conn, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, lenguajeGB.smsOpenai1() + `\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai2()}\n\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai3()}` , m)
 try {
-let zykomd = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
-let hasil = await zykomd.json()
-m.reply(`${hasil.result}`.trim())
+let tiores = await fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
+let hasil = await tiores.json()
+m.reply(`${hasil.data.data}`.trim())
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
