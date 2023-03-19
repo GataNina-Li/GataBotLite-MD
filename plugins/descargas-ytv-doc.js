e4dfb0df4a33aad943ed5923cfd4f82ae75d6d00
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-18 21:56:00 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/descargas-ytv-doc.js
+++ plugins/descargas-ytv-doc.js
@@ -11,7 +11,7 @@ const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async
 const dl_url = await yt.video[q].download()
 const ttl = await yt.title
 const size = await yt.video[q].fileSizeH
-let cap = `📡 *VIDEO* 📡\n\n*⎔ ${ttl}*\n*⎔ ${size}*`.trim()
+let cap = `📡 *VIDEO* 📡\n\n*⎔ ${ttl}*\n\n*⎔ ${size}*`.trim()
 await await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m})
 } catch {
 try {
@@ -20,7 +20,7 @@ let lolh = await lolhuman.json()
 let n = lolh.result.title || 'error'
 let n2 = lolh.result.link
 let n3 = lolh.result.size
-let cap = `🎥 *VIDEO* 🎥\n\n*⎔ ${n}*\n*⎔ ${n3}*`.trim()
+let cap = `🎥 *VIDEO* 🎥\n\n*⎔ ${n}*\n\n*⎔ ${n3}*`.trim()
 await conn.sendMessage(m.chat, { document: { url: n2 }, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m})
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
