b9aef60010f5eeb7713a067bc4971ae5a9356c93
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-18 23:59:18 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

---
+++ plugins/buscador-openai.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch'
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+try{
+if (!text) return conn.reply(m.chat, lenguajeGB.smsOpenai1() + `\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai2()}\n\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai3()}` , m)
+try {
+let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
+let resultado = await tiores.json()
+m.reply(`${resultado.result}`.trim())
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
+handler.command = ['openai', 'chatgpt', 'ia', 'ai']
+export default handler
