9c1989793cb1e6628f5e7c665f48c157b2a15dde
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-19 20:01:48 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/buscador-openai.js
+++ plugins/buscador-openai.js
@@ -1,4 +1,4 @@
-/*import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 let handler = async (m, { text, conn, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, lenguajeGB.smsOpenai1() + `\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai2()}\n\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai3()}` , m)
 try {
@@ -11,83 +11,7 @@ console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗
 console.log(e)}}
 handler.command = ['openai', 'chatgpt', 'ia', 'ai']
 handler.register = true
-export default handler*/
-
-import axios from 'axios';
-import { createInterface } from 'readline';
-import mongoose from 'mongoose';
-
-const openaiApiKey = 'tamvan';
-mongoose.connect('mongodb://localhost/chatgpt', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-
-const messageSchema = new mongoose.Schema({
-  conversationId: String,
-  userMessage: String,
-  botMessage: String
-});
-
-const Message = mongoose.model('Message', messageSchema);
-
-async function enviarMensaje(mensaje) {
-  try {
-    const respuesta = await axios.post(
-      'https://api.openai.com/v1/chat/engines/davinci-codex/completions',
-      {
-        prompt: `Conversación ID: ${mensaje.id}\nUsuario: ${mensaje.text}\nChatbot:`,
-        max_tokens: 50,
-        temperature: 0.7,
-        n: 1,
-        stop: '\n',
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${openaiApiKey}`,
-        },
-      }
-    );
-    return respuesta.data.choices[0].text.trim();
-  } catch (error) {
-    console.error('Error al enviar la solicitud:', error);
-    return null;
-  }
-}
-
-async function leerMensaje() {
-  return new Promise((resolve) => {
-    const rl = createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question('Tú: ', (mensaje) => {
-      rl.close();
-      resolve(mensaje);
-    });
-  });
-}
-
-let handler = async (m, { text, conn, usedPrefix, command }) => {
-  try {
-    if (m.type === 'chat' && text) {
-      const userMessage = text.trim();
-      m.reply(`Usuario: ${userMessage}`);
-      let botMessage = await enviarMensaje({ id: m.id, text: userMessage });
-      if (botMessage) {
-        m.reply(`Chatbot: ${botMessage}`);
-        const message = new Message({ conversationId: m.id, userMessage, botMessage });
-        await message.save();
-      }
-    }
-  } catch (error) {
-    console.error('Error en el manejador:', error);
-  }
-}
-
-handler.command = ['openai', 'chatgpt', 'ia', 'ai']
-handler.register = true
-export default handler;
-
+export default handler
 
 
 
