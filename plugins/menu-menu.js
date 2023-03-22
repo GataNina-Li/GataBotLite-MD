27f54fadcc2c13d5ca2948228a1b877490932bf9
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-03-22 19:19:19 -0300

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -16,7 +16,7 @@ let readMore = more.repeat(850)
 let taguser = conn.getName(m.sender)
 //let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 
-conn.sendFile(m.chat, img, `*☆═━┈◈ ╰ ${vs} ㎇ ╯ ◈┈━═☆*
+conn.sendFile(m.chat, pp, img, 'error.jpg', `*☆═━┈◈ ╰ ${vs} ㎇ ╯ ◈┈━═☆*
 *│* Hola ${taguser}\nsi no puede ver los botones del menu es por las nueva actualización del WhatsApp\nYA SE ESTA BUSCADO UNA SOLUCIÓN\nrecomiendo por hora instalar un WhatsApp mods\n\n   
 *╰ ㊂ ▸▸ _INFORMACIÓN DEL MENÚ_ ◂◂*
 *│* ┊
