0c3f973e6eaa5f8590895901efe8b266e5fe511c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-22 15:01:01 -0500

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -3,7 +3,9 @@ import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
 
 let handler = async (m) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+ let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+else who = m.chat
 //let name = await conn.getName(who)
 let name = await '@' + who.split`@`[0]
 let q = m.quoted ? m.quoted : m
