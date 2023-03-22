cc4f97cf1c4a541d165dd0b51f3b4f2a5eab8cb1
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-03-22 17:53:41 -0300

    Add files via upload

---
+++ plugins/descargas-imagen.js
@@ -0,0 +1,19 @@
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙐𝙎𝙀 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙈𝘼𝙉𝙀𝙍𝘼\n*${usedPrefix + command} Gata*`
+const res = await googleImage(text)
+let image = res.getRandom()
+let link = image
+conn.sendFile(m.chat, img, 'error.jpg', `*💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}*`, m)
+/*conn.sendHydrated(m.chat, `💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}`, `𝙂𝙤𝙤𝙜𝙡𝙚 | ${wm}`, link, link, '☘️ 𝙐𝙍𝙇', null, null, [
+['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`],
+['🔍 𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝙩 ', `#pinterest ${text}`],
+['🐈 𝙈𝙚𝙣𝙪', `.menu`],  
+], m)*/
+}
+handler.help = ['gimage <query>', 'imagen <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(gimage|image|imagen)$/i
+handler.exp = 20
+handler.register = true
+export default handler
