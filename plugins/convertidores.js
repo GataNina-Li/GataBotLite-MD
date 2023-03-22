4e93da65b1c9e5e988ff67ff7bdac0bc84902025
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-22 18:28:45 -0500

    Update convertidores.js

--- plugins/convertidores.js
+++ plugins/convertidores.js
@@ -11,7 +11,7 @@ const isCommand1 = /^(to(img|image)?|jpe?g)$/i.test(command)
 const isCommand2 = /^(t?ourl|upload)$/i.test(command)
 const isCommand3 = /^(to(video|mp4)?|mp4|togif)$/i.test(command)
 const isCommand4 = /^(to(gif|taud)?|gif|taud)$/i.test(command)
-//try{
+try{
 switch (true) {     
 case isCommand1:
 const notStickerMessage = lenguajeGB.smsToimg()
@@ -89,10 +89,10 @@ media = await q.download()
 conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: '*ᴀϙᴜɪ ᴇsᴛᴀ sᴜ ɢɪғ ᴄᴏɴ ᴀᴜᴅɪᴏ, ᴀʟ ᴀʙʀɪʀʟᴏ sᴇ ʀᴇᴘʀᴏᴅᴜᴄᴇ ᴄᴏɴ ᴀᴜᴅɪᴏ*' }, { quoted: m })
 break 
     
-}//} catch (e) {
-//await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
-///console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-//console.log(e)}
+}} catch (e) {
+await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}
 }
 
 handler.command = /^(to(img|image)?|jpe?g|t?ourl|upload|to(video|mp4)?|mp4|togif|to(gif|taud)?|gif|taud)$/i
