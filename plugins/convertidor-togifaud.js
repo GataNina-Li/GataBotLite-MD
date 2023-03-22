3f0a72b5ebba3e8238e6669cfc2eaff5249ccf84
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-03-22 18:17:31 -0300

    Add files via upload

---
+++ plugins/convertidor-togifaud.js
@@ -0,0 +1,12 @@
+/* 𝐂𝐑𝐄𝐀𝐃𝐎 𝐏𝐎𝐑 https://github.com/BrunoSobrino */
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+if (!m.quoted) throw `RESPONDE A UN VIDEO QUE DESEE CONVERTIR EN GIF CON AUDIO`
+const q = m.quoted || m
+let mime = (q.msg || q).mimetype || ''
+if (!/(mp4)/.test(mime)) throw `EL TIPO DE ARCHIVOS ${mime} NO ES CORRECTO, RESPONDA A UN VIDEO QUE DESEE CONVENTIR EN GIF CON AUDIO`
+m.reply(global.wait)
+let media = await q.download()
+conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: '*ᴀϙᴜɪ ᴇsᴛᴀ sᴜ ɢɪғ ᴄᴏɴ ᴀᴜᴅɪᴏ, ᴀʟ ᴀʙʀɪʀʟᴏ sᴇ ʀᴇᴘʀᴏᴅᴜᴄᴇ ᴄᴏɴ ᴀᴜᴅɪᴏ*' }, { quoted: m })}
+handler.command = ['togifaud']
+export default handler
