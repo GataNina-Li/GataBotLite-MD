5eb68cf2091e7752ec327d36ae22882e976c2805
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-22 17:26:39 -0500

    Update convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,6 +1,6 @@
 /* 𝐂𝐑𝐄𝐀𝐃𝐎 𝐏𝐎𝐑 https://github.com/BrunoSobrino */
 
-let handler = async (m, { conn, usedPrefix, command }) => {
+/*let handler = async (m, { conn, usedPrefix, command }) => {
 if (!m.quoted) throw `RESPONDE A UN VIDEO QUE DESEE CONVERTIR EN GIF CON AUDIO`
 const q = m.quoted || m
 let mime = (q.msg || q).mimetype || ''
@@ -9,4 +9,4 @@ m.reply(global.wait)
 let media = await q.download()
 conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: '*ᴀϙᴜɪ ᴇsᴛᴀ sᴜ ɢɪғ ᴄᴏɴ ᴀᴜᴅɪᴏ, ᴀʟ ᴀʙʀɪʀʟᴏ sᴇ ʀᴇᴘʀᴏᴅᴜᴄᴇ ᴄᴏɴ ᴀᴜᴅɪᴏ*' }, { quoted: m })}
 handler.command = ['togifaud']
-export default handler
+export default handler*/
