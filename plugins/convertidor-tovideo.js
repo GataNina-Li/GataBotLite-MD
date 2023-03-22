25e34e50e734c24739af765668d0186fc0cbdfe2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-22 16:30:18 -0500

    Update convertidor-tovideo.js

--- plugins/convertidor-tovideo.js
+++ plugins/convertidor-tovideo.js
@@ -3,7 +3,8 @@ import { ffmpeg } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (!m.quoted) throw `RESPONDE A UN STICKER CON MOVIMIENTO PARA CONVERTIR EN VIDEO\n\n*${usedPrefix + command}*` 
 let mime = m.quoted.mimetype || ''
-if (!/webp|audio/.test(mime)) throw `RESPONDE AL AUDIO PARA CONVERTIR EN VIDEL\n\n*${usedPrefix + command}*`
+if (!/webp|gif/.test(mime)) throw `RESPONDE AL AUDIO PARA CONVERTIR EN VIDEL\n\n*${usedPrefix + command}*`
+try{
 let media = await m.quoted.download()
 let out = Buffer.alloc(0)
 if (/webp/.test(mime)) {
@@ -15,11 +16,12 @@ out = await ffmpeg(media, [
 '-crf', '51',
 '-c:a', 'copy',
 '-shortest'
-], 'mp3', 'mp4')
-}
-await conn.sendFile(m.chat, out, 'error.mp4', '*FUE UN ÉXITO!!*', m, 0, { thumbnail: out })
-}
-handler.help = ['tovideo']
-handler.tags = ['sticker']
-handler.command = ['tovideo']
+], 'mp3', 'mp4')}
+await conn.sendFile(m.chat, out, 'error.mp4', '*✓ AQUÍ TIENES TÚ VÍDEO!!*', m, 0, { thumbnail: out })
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
+
+handler.command = /^to(video|mp4)?|mp4|togif$/i
 export default handler
