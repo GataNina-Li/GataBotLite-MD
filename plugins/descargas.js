b4b2bf8713a6321aae6be96f8c9f8d60c6ef9284
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-22 23:32:09 -0500

    Update and rename descargas-imagen.js to descargas.js

---
+++ plugins/descargas.js
@@ -0,0 +1,51 @@
+import { googleImage } from '@bochilteam/scraper'
+import fetch from "node-fetch"
+import yts from 'yt-search'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+const isCommand1 = /^(gimage|image|imagen)$/i.test(command)
+const isCommand2 = /^(play|play2)$/i.test(command)
+
+switch (true) {     
+case isCommand1:
+if (!text) throw lenguajeGB.smsMalused2() + `\n*${usedPrefix + command} Gata*`
+try{
+const res = await googleImage(text)
+let image = res.getRandom()
+let link = image
+conn.sendFile(m.chat, link, 'error.jpg', `*💞 Resultado de:* ${text}`, m)
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}
+break
+    
+case isCommand2:
+if (!text) throw lenguajeGB.smsMalused2() + `*${usedPrefix + command} Billie Eilish - Bellyache*`
+let vid = (await yts(text)).all[0]
+if (!vid) throw lenguajeGB.smsMalError2() + `_${lenguajeGB.smsYT6()}_`
+try{
+let { title, description, url, thumbnail, videoId, timestamp, views, published } = vid
+let video = `*◜⋯ ⋯ ⋯ Y O U T U B E ⋯ ⋯ ⋯◞*
+*◎ ${lenguajeGB.smsYT1()}*
+${title}
+*◎ ${lenguajeGB.smsYT2()}*
+${description}
+*◎ ${lenguajeGB.smsYT3()}*
+${timestamp}
+*◎ ${lenguajeGB.smsYT4()}*
+${views}
+*◎ URL*
+${url}`.trim()
+await conn.sendFile(m.chat, thumbnail, 'error.jpg', video, m)
+await conn.sendMessage(m.chat, { audio: { url: url }, mimetype: "audio/mp4", fileName: `${title}.mp3`, }, { quoted: m })  
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}    
+break
+}}
+
+handler.command = /^(gimage|image|imagen|play|play2)$/i
+handler.register = true
+export default handler
