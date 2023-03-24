df99240947f3ff0cf5dfdcaf6c8464fe4c50cf8b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-24 14:26:52 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

---
+++ plugins/grupos.js
@@ -0,0 +1,74 @@
+let handler = async (m, { conn, command, usedPrefix, args, participants, groupMetadata }) => {
+let pp, groupAdmins, listAdmin, owner
+const isCommand1 = /^(infogrupo|gro?upinfo|info(gro?up|gc))$/i.test(command)
+const isCommand2 = /^(admins|@admins|dmins)$/i.test(command)
+
+async function reportError(e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+}
+
+switch (true) {     
+case isCommand1:
+try{
+pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
+groupAdmins = participants.filter(p => p.admin) 
+listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
+owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+let info = 
+`✤ ${lenguajeGB['smsGI1']()} ✤
+⎔ ${lenguajeGB['smsGI2']()}
+・ ${groupMetadata.id}
+
+⎔ ${lenguajeGB['smsGI3']()}
+・ ${groupMetadata.subject}
+
+⎔ ${lenguajeGB['smsGI4']()}
+・ ${groupMetadata.desc?.toString() || lenguajeGB['smsGI5']()}
+
+⎔ ${lenguajeGB['smsGI6']()}
+・ ${participants.length} ${lenguajeGB['smsGI7']()}
+
+⎔ ${lenguajeGB['smsGI8']()}
+・ @${owner.split('@')[0]}
+
+⎔ ${lenguajeGB['smsGI9']()}
+${listAdmin}
+`.trim()
+  
+await conn.sendFile(m.chat, pp, 'error.jpg', info, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })    
+} catch (e) {
+reportError(e)
+}
+break
+    
+case isCommand2:
+try{
+pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.jpg'
+groupAdmins = participants.filter(p => p.admin)
+listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
+owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+let pesan = args.join` `
+let oi = `${lenguajeGB.smsAddB5()} _${pesan}_`
+
+let textoA = 
+`*⊱ ──── 《.⋅ 🐈 ⋅.》 ──── ⊰*
+ෆ ${lenguajeGB.smsAddB3()}
+ෆ ${oi}
+*⊱ ──── 《.⋅ ${vs} ⋅.》 ──── ⊰*`
+
+let textoB = 
+`${listAdmin}
+⛔ ${lenguajeGB.smsAddB4()} ⛔`.trim()
+await conn.sendFile(m.chat, pp, 'error.jpg', info, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
+} catch (e) {
+reportError(e)
+} 
+break
+}}
+
+handler.command = /^(infogrupo|gro?upinfo|info(gro?up|gc)|admins|@admins|dmins)$/i
+handler.group = true
+handler.register = true
+export default handler
