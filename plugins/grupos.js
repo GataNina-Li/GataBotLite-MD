3b5f6904c5f2bbf171842cd521295c85ecd20eaf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-24 23:53:22 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -81,9 +81,24 @@ reportError(e)
 break
     
 case isCommand4:
+
 let [, code] = text.match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || []
 if (!code) throw lenguajeGB.smsMalused() + usedPrefix + command + ' ' + nna 
 try{
+const extractGroupMetadata = (result) => {
+const group = baileys.getBinaryNodeChild(result, 'group')
+const descChild = baileys.getBinaryNodeChild(group, 'description')
+let desc
+if (descChild) desc = baileys.getBinaryNodeChild(descChild, 'body')?.content
+const metadata = {
+id: group.attrs.id.includes('@') ? group.attrs.id : baileys.jidEncode(group.attrs.id, 'g.us'),
+subject: group.attrs.subject,
+creation: new Date(+group.attrs.creation * 1000).toLocaleString('id', { timeZone: 'America/Los_Angeles' }),
+owner: group.attrs.creator ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.creator).split('@')[0] : undefined,
+desc
+}
+return metadata
+}  
 let res = await conn.query({ tag: 'iq', attrs: { type: 'get', xmlns: 'w:g2', to: '@g.us' }, content: [{ tag: 'invite', attrs: { code } }] }),
 data = extractGroupMetadata(res),
 txt = Object.keys(data).map(v => `*${v.capitalize()}:* ${data[v]}`).join('\n'),
@@ -108,21 +123,7 @@ let groupinfo = `
 await conn.sendFile(m.chat, pp, 'error.jpg', groupinfo, m)
 } catch (e) {
 reportError(e)
-} 
-const extractGroupMetadata = (result) => {
-const group = baileys.getBinaryNodeChild(result, 'group')
-const descChild = baileys.getBinaryNodeChild(group, 'description')
-let desc
-if (descChild) desc = baileys.getBinaryNodeChild(descChild, 'body')?.content
-const metadata = {
-id: group.attrs.id.includes('@') ? group.attrs.id : baileys.jidEncode(group.attrs.id, 'g.us'),
-subject: group.attrs.subject,
-creation: new Date(+group.attrs.creation * 1000).toLocaleString('id', { timeZone: 'America/Los_Angeles' }),
-owner: group.attrs.creator ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.creator).split('@')[0] : undefined,
-desc
-}
-return metadata
-}    
+}   
 break
 }}
 
