6e534a4a4f560c187955665a2261cdbfb14b2ddd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-25 22:49:08 -0500

    Create audio-efectos.js

---
+++ plugins/audio-efectos.js
@@ -0,0 +1,46 @@
+import { promises } from 'fs'
+import { join } from 'path'
+import { exec } from 'child_process'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+try {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!/audio/.test(mime)) throw `*PARA APLICAR EL EFECTO RESPONDA A UN AUDIO O NOTA DE VOZ USANDO ${usedPrefix + command}*`
+let audio = await q.download?.()
+if (!audio) throw '*NO SE PUDO DESCARGAR EL AUDIO*'
+if (!args[0] || !args[1]) throw 'xample: .bass 10 10'
+if (isNaN(args[0]) || isNaN(args[1])) return m.reply('Pake angka')
+let set
+if (/bass/.test(command)) set = `-af equalizer=f=${args[0]}:width_type=o:width=2:g=${args[1]}`
+if (/volume/.test(command)) set = `-af volume=${args[0]} -vcodec copy`
+if (/blown/.test(command)) set = '-af acrusher=.1:1:64:0:log'
+if (/deep/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3'
+if (/earrape/.test(command)) set = '-af volume=12'
+if (/fast/.test(command)) set = `-filter:a "atempo=1.63,asetrate=${args[0]}"`
+if (/fat/.test(command)) set = `-filter:a "atempo=1.6,asetrate=22100"`
+if (/nightcore/.test(command)) set = '-filter:a atempo=1.06,asetrate=44100*1.25'
+if (/reverse/.test(command)) set = '-filter_complex "areverse"'
+if (/robot/.test(command)) set = '-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"'
+if (/slow/.test(command)) set = `-filter:a "atempo=1.0,asetrate=${args[0]}"`
+if (/smooth/.test(command)) set = '-filter:v "minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120\'"'
+if (/tupai|squirrel|chipmunk/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"'
+if (/vibra/.test(command)) set = '-filter_complex "vibrato=f=15"'
+if (/audio8d/.test(command)) set = '-af apulsator=hz=0.125'
+let ran = (new Date * 1) + '.mp3'
+let media = '../tmp/' + ran
+let filename = media + '.mp3'
+await promises.writeFile(media, audio)
+exec(`ffmpeg -i ${media} ${set} ${filename}`, async (err) => {
+await promises.unlink(media)
+if (err) return Promise.reject( `_*Error!*_`)
+let buff = await promises.readFile(filename)
+await m.reply(wait)
+await conn.sendFile(m.chat, buff, ran, null, m, /vn/.test(args[0]), { quoted: m, mimetype: 'audio/mp4' })
+await promises.unlink(filename)
+})
+} catch (e) {
+throw e
+}}
+handler.command = /^(bass|blown|deep|earrape|fas?t|nightcore|reverse|robot|slow|smooth|tupai|squirrel|chipmunk|vibra|volume|audio8d)$/i
+export default handler
