493badb7048597ab79c764d72ecd053a6f28bfc0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-26 04:24:30 -0500

    Update audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -111,7 +111,7 @@ t = ['sine', 'square', 'triangle', 'sawup', 'sawdown'].includes(args[2]) ? args[
 s = isNaN(args[3]) || args[3] < 0 || args[3] > 21 ? 5 : Number(args[3])
 r = isNaN(args[4]) || args[4] < 0 || args[4] > 11 ? 0.3 : Number(args[4])
 //set = `-filter_complex "vibrato=f=${f}:d=${d}:t=${t}:s=${s}:r=${r}"`
-set = `-filter_complex "vibrato=f=${f}"`
+set = `-filter_complex "vibrato=f=${f}:d=${d}"`
 m.reply(`*🎧 VALORES ASIGNADOS:*\n\`\`\`${set}\`\`\`\n1️⃣ *${f}* 2️⃣ *${d}* 3️⃣ *${t}* 4️⃣ *${s}* 5️⃣ *${r}*`)
 }
         
