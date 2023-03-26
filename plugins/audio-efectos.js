f9647bb048bde4c5fdd80e974fb64a5ceb602037
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-25 22:58:14 -0500

    Update audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -1,8 +1,8 @@
-import { promises } from 'fs'
+import { unlinkSync, readFileSync } from 'fs'
 import { join } from 'path'
 import { exec } from 'child_process'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, args, __dirname, usedPrefix, command }) => {
 try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
@@ -12,17 +12,17 @@ if (!audio) throw '*NO SE PUDO DESCARGAR EL AUDIO*'
 //if (!args[0] || !args[1]) throw 'xample: .bass 10 10'
 //if (isNaN(args[0]) || isNaN(args[1])) return m.reply('Pake angka')
 let set
-if (/bass/.test(command)) set = `-af equalizer=f=${args[0]}:width_type=o:width=2:g=${args[1]}`
-if (/volume/.test(command)) set = `-af volume=${args[0]} -vcodec copy`
+if (/bass/.test(command)) set = '-af equalizer=f=94:width_type=o:width=2:g=30'
+if (/vibra/.test(command)) set = '-filter_complex "vibrato=f=15"'
 if (/blown/.test(command)) set = '-af acrusher=.1:1:64:0:log'
 if (/deep/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3'
 if (/earrape/.test(command)) set = '-af volume=12'
-if (/fast/.test(command)) set = `-filter:a "atempo=1.63,asetrate=${args[0]}"`
-if (/fat/.test(command)) set = `-filter:a "atempo=1.6,asetrate=22100"`
+if (/fast/.test(command)) set = '-filter:a "atempo=1.63,asetrate=44100"'
+if (/fat/.test(command)) set = '-filter:a "atempo=1.6,asetrate=22100"'
 if (/nightcore/.test(command)) set = '-filter:a atempo=1.06,asetrate=44100*1.25'
 if (/reverse/.test(command)) set = '-filter_complex "areverse"'
 if (/robot/.test(command)) set = '-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"'
-if (/slow/.test(command)) set = `-filter:a "atempo=1.0,asetrate=${args[0]}"`
+if (/slow/.test(command)) set = '-filter:a "atempo=0.7,asetrate=44100"'
 if (/smooth/.test(command)) set = '-filter:v "minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120\'"'
 if (/tupai|squirrel|chipmunk/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"'
 if (/vibra/.test(command)) set = '-filter_complex "vibrato=f=15"'
