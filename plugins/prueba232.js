0795f6de19465a718a1ea35e77beb3bf9192f329
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-25 21:49:00 -0500

    Create prueba232.js

---
+++ plugins/prueba232.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, isOwner }) => {
+try{
+let groups = Object.values(await conn.groupFetchAllParticipating()),
+txt = `${packname} ${lenguajeGB.smsLisA()}
+${lenguajeGB.smsLisB()} ${groups.length}\n`
+
+for (let i = 0; i < groups.length; i++) {
+txt += ` 
+${lenguajeGB.smsLisC()} ${groups[i].subject}
+${lenguajeGB.smsLisD()} ${groups[i].id}
+${isOwner ? `${lenguajeGB.smsLisE()} ${groups[i].participants.length}` : ''}
+${isOwner ? `${lenguajeGB.smsLisF()} ${!!groups[i].participants.find(v => v.id == conn.user.jid).admin == true ? '✅' : '❌'}\n` : ''}`
+}
+m.reply(txt.trim())
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}}
+handler.command = /^(mundogrupo)$/i
+
+export default handler
