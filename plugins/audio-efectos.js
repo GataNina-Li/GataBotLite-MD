0a935cffe8b993c13b5be884b4d7c7a84944cfd7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-27 02:56:10 -0500

    Update audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -12,7 +12,7 @@ if (/blown/.test(command)) set = '-af "acrusher=.1:1:64:0:log, lowpass=f=3000, h
 if (/deep/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3'
 if (/earrape/.test(command)) set = '-af "compand=attacks=0:points=-90/-90|-20.0/-5.0|0/-3.0|20.0/-1.0:soft-knee=6:gain=3,volume=9dB"' //'-af "compand=attacks=0:points=-90/-90|-20.0/-5.0|0/-3.0|20.0/-1.0:soft-knee=6:gain=3"' //'-af "compand=attacks=0:points=-90/-90|-20.0/-5.0|0/-3.0|20.0/-1.0:soft-knee=6:gain=6"' //'-af volume=12'
 if (/fast/.test(command)) set = '-filter:a "atempo=1.63,asetrate=44100,volume=3dB"'
-if (/fat/.test(command)) set = '-af "equalizer=f=60:width_type=h:width=50:g=-6"' //'-filter:a "atempo=1.6,asetrate=22100"'
+if (/fat/.test(command)) set = '-filter:a "atempo=1.6,asetrate=22100,volume=4dB"'
 if (/nightcore/.test(command)) set = `-filter:a "atempo=1.06,asetrate=44100*1.25,firequalizer=gain_entry='entry(0,6);entry(250,0);entry(500,-3);entry(1000,-3);entry(2000,-2);entry(4000,0);entry(8000,2);entry(16000,3)',equalizer=f=800:t=h:width_type=h:width=50:g=20"` //'-filter:a atempo=1.06,asetrate=44100*1.25'
 if (/reverse/.test(command)) set = '-filter_complex "areverse, highpass=f=200, lowpass=f=8000, afftdn"' //'-filter_complex "areverse"'
 if (/robot/.test(command)) set = `-filter_complex "afftfilt=real='hypot(re,im)*sin(2*PI*2000*t)':imag='hypot(re,im)*cos(2*PI*2000*t)':win_size=512:overlap=0.75, asetrate=44100"` //'-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"'
