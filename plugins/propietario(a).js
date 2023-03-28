e33b5aa2da4c946063ed5c616cfe8f3dedec4fb9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-28 18:46:35 -0500

    Update propietario(a).js

--- plugins/propietario(a).js
+++ plugins/propietario(a).js
@@ -40,8 +40,8 @@ var number = text.split`@`[1]
 var number = text
 }
         
-let bant = `*ETIQUETE A ALGUIEN O RESPONDA AL MENSAJE DEL USUARIO QUE QUIERE BANEAR DE LOS COMANDOS*\n*EJEMPLO:*\n*${usedPrefix + command} @${owner[0][0] + '@s.whatsapp.net'}*`
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(bant, m.chat, { mentions: conn.parseMention(who)})
+let bant = `*ETIQUETE A ALGUIEN O RESPONDA AL MENSAJE DEL USUARIO QUE QUIERE BANEAR DE LOS COMANDOS*\n*EJEMPLO:*\n*${usedPrefix + command} @${conn.user.jid.split`@`[0]}}*`
+if (!m.mentionedJid[0] && !m.quoted) return conn.reply(m.chat, bant, null, { mentions: [bant] })
 //(user === conn.user.jid)
                
 try {
