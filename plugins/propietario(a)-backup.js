d5f607c2d2fa47ed1ab32912e7b581775bf59b87
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-28 14:20:40 -0500

    Update propietario(a)-backup.js

--- plugins/propietario(a)-backup.js
+++ plugins/propietario(a)-backup.js
@@ -2,29 +2,31 @@
 
 import fs from 'fs'
 import fetch from 'node-fetch'
+
 let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
-	let fdoc = {
-  key : {
-  remoteJid: 'status@broadcast',
-  participant : '0@s.whatsapp.net'
-  },
-  message: {
-  documentMessage: {
-  title: '𝙳 𝙰 𝚃 𝙰 𝙱 𝙰 𝚂 𝙴', 
-  jpegThumbnail: fs.readFileSync('./media/menus/Menu1.jpg'),
-                            }
-                          }
-                        }
-	let d = new Date
-            let date = d.toLocaleDateString('id', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric'
-            })
-conn.reply(m.chat, '*enviando backup.*', m)
-conn.reply('593968585383' + '@s.whatsapp.net', `*🗓️ Database:* ${date}`, null)
-          conn.sendFile('593968585383' + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json', quoted: fdoc})
- }
+  let fdoc = {
+    key : {
+      remoteJid: 'status@broadcast',
+      participant : '0@s.whatsapp.net'
+    },
+    message: {
+      documentMessage: {
+        title: '𝙳 𝙰 𝚃 𝙰 𝙱 𝙰 𝚂 𝙴', 
+        jpegThumbnail: fs.readFileSync('./media/menus/Menu1.jpg'),
+      }
+    }
+  }
+  let d = new Date
+  let date = d.toLocaleDateString('id', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+  conn.reply(m.chat, '*enviando backup.*', m)
+  conn.reply('593968585383' + '@s.whatsapp.net', `*🗓️ Database:* ${date}`, null)
+  conn.sendFile('593968585383' + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', m.chat, 0, 0, null, { mimetype: 'application/json', quoted: fdoc, maxForwarded: 100000, uploadOptions: { chunked: true, highWaterMark: 5 * 1024 * 1024, maxFileSize: 50 * 1024 * 1024 * 1024 }})
+}
+
  
  handler.help = ['backup']
 handler.tags = ['owner']
