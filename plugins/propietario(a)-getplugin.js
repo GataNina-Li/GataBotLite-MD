74b22a5b1111660bd889dd8605853b36d4d844a9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-29 18:34:34 -0500

    Update propietario(a)-getplugin.js

--- plugins/propietario(a)-getplugin.js
+++ plugins/propietario(a)-getplugin.js
@@ -102,6 +102,93 @@ handler.rowner = true
 
 export default handler*/
 
+/*import fs from 'fs'
+import path from 'path'
+import { promisify } from 'util'
+
+const readdir = promisify(fs.readdir)
+const readFile = promisify(fs.readFile)
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  if (!text) throw `Por favor, proporciona el nombre del comando para buscar el archivo correspondiente\nEjemplo: ${usedPrefix + command} info`
+
+  const pluginsDir = './plugins'
+  const files = await readdir(pluginsDir)
+
+  let matchingFile;
+
+  // Busca ARC
+  if (text.endsWith('-exec.js')) {
+    const cmd = text.replace(/\.js$/, '') // Elimina el .js del final
+    const filename = `${cmd}.js`
+    try {
+      const fileContent = await fs.readFileSync(path.join(process.cwd(), pluginsDir, filename))
+      await conn.sendMessage(m.chat, { document: fileContent, mimetype: 'text/javascript', fileName: filename }, { quoted: m })
+      await m.reply(`Código del archivo ${filename}:\n\n${fileContent.toString()}`)
+      return
+        
+    } catch {
+        try{
+        const plugin = (await import(path.join(process.cwd(), pluginsDir, matchingFile))).default
+
+    const filename = matchingFile.replace('.js', '')
+    const fileContent = await readFile(path.join(process.cwd(), pluginsDir, matchingFile), 'utf-8')
+   
+    await conn.sendMessage(m.chat, { document: fileContent, mimetype: 'text/javascript', fileName: filename }, { quoted: m })
+    await m.reply(`Código del archivo ${filename}.js:\n\n${fileContent.toString()}`)
+    } catch (err) {
+     m.reply('*ERROR*')   
+    }
+    }*/
+        
+        
+     // m.reply(`Error al buscar el archivo de manera directa '${filename}': ${err.message}`)
+   // }
+ // }
+
+  // Busca CYE
+  /*for (let file of files) {
+    const plugin = (await import(path.join(process.cwd(), pluginsDir, file))).default
+    try {
+      if (plugin && plugin.command && plugin.command.test(text) && text.match(plugin.command)) {
+        matchingFile = file;
+        break
+      }
+    } catch (err) {
+      m.reply(`Error en el archivo ${file}: ${err.message}`)
+    }
+  }
+
+  
+  if (!matchingFile) {
+    //console.log(`El comando '${text}' no fue encontrado`)
+    return m.reply(`El comando '${text}' no fue encontrado`)
+  }*/
+
+  /*try {
+    const plugin = (await import(path.join(process.cwd(), pluginsDir, matchingFile))).default
+
+    const filename = matchingFile.replace('.js', '')
+    const fileContent = await readFile(path.join(process.cwd(), pluginsDir, matchingFile), 'utf-8')
+   
+    await conn.sendMessage(m.chat, { document: fileContent, mimetype: 'text/javascript', fileName: filename }, { quoted: m })
+    await m.reply(`Código del archivo ${filename}.js:\n\n${fileContent.toString()}`)
+  
+  } catch (err) {
+    console.log(`Error al enviar el archivo '${matchingFile}': ${err.message}`)
+    return m.reply(`Ocurrió un error al enviar el archivo '${matchingFile}'`)
+  }*/
+/*}
+
+handler.help = ['getplugin'].map(v => v + ' <nombre del comando>')
+handler.tags = ['host']
+handler.command = /^(getplugin|gp)$/i
+
+handler.rowner = true
+
+export default handler*/
+
+
 import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
@@ -126,13 +213,25 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
       await conn.sendMessage(m.chat, { document: fileContent, mimetype: 'text/javascript', fileName: filename }, { quoted: m })
       await m.reply(`Código del archivo ${filename}:\n\n${fileContent.toString()}`)
       return
-    } catch (err) {
-      m.reply(`Error al buscar el archivo de manera directa '${filename}': ${err.message}`)
+    } catch {
+      try {
+        matchingFile = files.find(f => f.startsWith(cmd))
+        if (!matchingFile) throw `No se encontró ningún archivo con el nombre '${cmd}'`
+
+        const plugin = (await import(path.join(process.cwd(), pluginsDir, matchingFile))).default
+        const filename = matchingFile.replace('.js', '')
+        const fileContent = await readFile(path.join(process.cwd(), pluginsDir, matchingFile), 'utf-8')
+
+        await conn.sendMessage(m.chat, { document: fileContent, mimetype: 'text/javascript', fileName: filename }, { quoted: m })
+        await m.reply(`Código del archivo ${filename}.js:\n\n${fileContent.toString()}`)
+      } catch (err) {
+        m.reply(`Ocurrió un error al buscar el archivo '${filename}': ${err.message}`)
+      }
     }
   }
 
   // Busca CYE
-  for (let file of files) {
+  /*for (let file of files) {
     const plugin = (await import(path.join(process.cwd(), pluginsDir, file))).default
     try {
       if (plugin && plugin.command && plugin.command.test(text) && text.match(plugin.command)) {
@@ -148,9 +247,9 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
   if (!matchingFile) {
     //console.log(`El comando '${text}' no fue encontrado`)
     return m.reply(`El comando '${text}' no fue encontrado`)
-  }
+  }*/
 
-  try {
+  /*try {
     const plugin = (await import(path.join(process.cwd(), pluginsDir, matchingFile))).default
 
     const filename = matchingFile.replace('.js', '')
@@ -162,7 +261,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
   } catch (err) {
     console.log(`Error al enviar el archivo '${matchingFile}': ${err.message}`)
     return m.reply(`Ocurrió un error al enviar el archivo '${matchingFile}'`)
-  }
+  }*/
 }
 
 handler.help = ['getplugin'].map(v => v + ' <nombre del comando>')
@@ -172,6 +271,7 @@ handler.command = /^(getplugin|gp)$/i
 handler.rowner = true
 
 export default handler
+
 
 
 
