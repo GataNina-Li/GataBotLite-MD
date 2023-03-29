c6332535f1ee8dd94f51a997bd3222fe0816c9d7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-29 01:50:55 -0500

    Update propietario(a).js

--- plugins/propietario(a).js
+++ plugins/propietario(a).js
@@ -5,12 +5,12 @@ let fkontak, who, user, number, bot, bant, ownerNumber, aa, users, usr, q, mime,
 fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 const isCommand1 = /^(backup|respaldo|copia)$/i.test(command)
 const isCommand2 = /^(ban(user|usuario|earuser|earusuario))$/i.test(command) 
-const isCommand3 = /^((unban|desban)(user|usuario|earuser|earusuario))$/i.test(command) 
-const isCommand4 = /^(seradmin|autoadmin|tenerpoder)$/i.test(command)
-const isCommand5 = /^((set|cambiar|nueva|new)(bio|botbio|biobot))$/i.test(command)
-const isCommand6 = /^((set|cambiar|nuev(a|o)?|new)(name|botname|namebot|nombre|nombrebot|botnombre))$/i.test(command)
-const isCommand7 = /^((set|cambiar|nueva|new)(ppbot|botpp|fotobot|botfoto))$/i.test(command)
-const isCommand8 = /^(update|actualizar|ups)$/i.test(command)
+const isCommand3 = /^(seradmin|autoadmin|tenerpoder)$/i.test(command)
+const isCommand4 = /^((set|cambiar|nueva|new)(bio|botbio|biobot))$/i.test(command)
+const isCommand5 = /^((set|cambiar|nuev(a|o)?|new)(name|botname|namebot|nombre|nombrebot|botnombre))$/i.test(command)
+const isCommand6 = /^((set|cambiar|nueva|new)(ppbot|botpp|fotobot|botfoto))$/i.test(command)
+const isCommand7 = /^(update|actualizar|ups)$/i.test(command)
+const isCommand8 = /^(banchat|banearchat)$/i.test(command)
 
 async function reportError(e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
@@ -79,42 +79,6 @@ console.log(e)
 break
         
 case isCommand3:
-try{
-function no(number){
-return number.replace(/\s/g,'').replace(/([@+-])/g,'')}
-text = no(text)
-if(isNaN(text)) {
-number = text.split`@`[1]
-} else if(!isNaN(text)) {
-number = text
-}
-user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
-bot = conn.user.jid.split`@`[0] 
-bant = lenguajeGB.smsPropdesban1(usedPrefix, command, bot)
-if (!text && !m.quoted) return conn.reply(m.chat, bant, null, { mentions: [user] })               
-try {
-if(text) {
-user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-user = m.quoted.sender
-} else if(m.mentionedJid) {
-user = number + '@s.whatsapp.net'
-}} catch (e) {
-} finally {
-number = user.split('@')[0]
-users = global.db.data.users
-if (users[user].banned === false) conn.reply(m.chat, lenguajeGB.smsPropdesban2(number), null, { mentions: [user] }) 
-users[user].banned = false
-usr = m.sender.split('@')[0]     
-await conn.reply(m.chat, lenguajeGB.smsPropdesban3(), null, { mentions: [user] })   
-await conn.reply(user, lenguajeGB.smsPropdesban4(number, usr), null, { mentions: [user, m.sender] })
-}} catch (e) {
-await conn.reply(m.chat, lenguajeGB.smsPropdesban5(usedPrefix, command, number), null, m)
-console.log(e) 
-}        
-break
-        
-case isCommand4:
 if (m.fromMe) return
 if (isAdmin) throw lenguajeGB.smsAutoAdmin1()
 try {  
@@ -123,7 +87,7 @@ await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 await m.reply(lenguajeGB.smsAutoAdmin2())}        
 break
         
-case isCommand5:
+case isCommand4:
 if (!text) throw `*ESCRIBA EL TEXTO QUE QUIERE QUE SE MUESTRE EN LA BIOGRAFÍA DE* ${packname}`
 if (text.length > 139) throw `*LA BIOGRAFÍA ES MUY LARGA, RESUMA LA INFORMACIÓN POR FAVOR*`
 try {
@@ -134,7 +98,7 @@ reportError(e)
 }        
 break
         
-case isCommand6:
+case isCommand5:
 if (!text) throw `*ESCRIBA EL TEXTO QUE QUIERE QUE SE MUESTRE COMO NOMBRE DE USUARIO EN* ${packname}`
 if (text.length > 25) throw `*EL NOMBRE ES MUY LARGO, RESUMA LA INFORMACIÓN POR FAVOR*`
 try {
@@ -145,7 +109,7 @@ reportError(e)
 }        
 break
         
-case isCommand7:
+case isCommand6:
 bot = conn.user.jid
 q = m.quoted ? m.quoted : m
 mime = (q.msg || q).mimetype || ''
@@ -157,19 +121,25 @@ await conn.reply(m.chat, '✅ ```FOTO DE PERFIL DEL BOT CAMBIADA CON ÉXITO```',
 } else throw `*RECUERDE RESPONDER A UNA IMAGEN USANDO EL COMANDO ${usedPrefix + command}*`        
 break
         
-case isCommand8:
+case isCommand7:
 try {  
 if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-conn.reply(m.chat, stdout.toString(), m)}
+await conn.reply(m.chat, stdout.toString(), m)}
 } catch {
 var update = execSync('git remote set-url origin' + md + '.git && git pull')
 await m.reply(update.toString())
 }        
 break
+        
+case isCommand8:
+global.db.data.chats[m.chat].isBanned = true
+await conn.reply(m.chat, '*ESTE CHAT FUE BANEADO, NO PODRÁN USAR LOS COMANDOS HASTA DESBANEAR EL CHAT*', m)        
+break
+        
 }}
 
-handler.command = /^(backup|respaldo|copia|ban(user|usuario|earuser|earusuario)|(unban|desban)(user|usuario|earuser|earusuario)|seradmin|autoadmin|tenerpoder|(set|cambiar|nueva|new)(bio|botbio|biobot)|(set|cambiar|nuev(a|o)?|new)(name|botname|namebot|nombre|nombrebot|botnombre)|(set|cambiar|nueva|new)(ppbot|botpp|fotobot|botfoto)|update|actualizar|ups)$/i
+handler.command = /^(backup|respaldo|copia|ban(user|usuario|earuser|earusuario)|seradmin|autoadmin|tenerpoder|(set|cambiar|nueva|new)(bio|botbio|biobot)|(set|cambiar|nuev(a|o)?|new)(name|botname|namebot|nombre|nombrebot|botnombre)|(set|cambiar|nueva|new)(ppbot|botpp|fotobot|botfoto)|update|actualizar|ups|banchat|banearchat)$/i
 handler.owner = true
 
 export default handler
