869306ba576e72f821e3eaab0244e1464b9c98b1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-29 00:07:03 -0500

    Delete owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw '*YA ES ADMIN DEL GRUPO MI Propietario(a) *'
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^admin.|atad|autoadmin$/i
-handler.rowner = true
-handler.botAdmin = true
-export default handler
