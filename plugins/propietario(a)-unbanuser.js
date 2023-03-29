32d6dc301d5e99e58dd1d47ae2179dc18553e510
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-03-28 23:46:01 -0500

    Update propietario(a)-unbanuser.js

--- plugins/propietario(a)-unbanuser.js
+++ plugins/propietario(a)-unbanuser.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, text, command, usedPrefix }) => {
+/*let handler = async (m, { conn, text, command, usedPrefix }) => {
 if (!text) throw `${mg}𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝘼𝙇𝙂𝙐𝙄𝙀𝙉 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*\n\n𝙏𝘼𝙂 𝙎𝙊𝙈𝙀𝙊𝙉𝙀 𝙏𝙊 𝙐𝙉𝘽𝘼𝙉\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} @tag*`
 let who
 if (m.isGroup) who = m.mentionedJid[0]
@@ -13,4 +13,4 @@ handler.tags = ['owner']
 handler.command = /^unbanuser$/i
 handler.group = true
 handler.admin = true 
-export default handler
+export default handler*/
