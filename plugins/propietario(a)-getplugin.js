bfded898753e63e87d667bb07bd12ec7ca9c2d3f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-08 10:32:15 -0500

    Update propietario(a)-getplugin.js

--- plugins/propietario(a)-getplugin.js
+++ plugins/propietario(a)-getplugin.js
@@ -1,6 +1,6 @@
 //CÓDIGO CREADO Y ADAPTADO POR https://github.com/GataNina-Li
 
-/*import fs from 'fs'
+import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
 import fse from 'fs-extra'
@@ -58,83 +58,7 @@ return m.reply(`Ocurrió un error al enviar el archivo '${matchingFile}'`)
 handler.command = /^(getplugin|gp|obtenercodigo|obtenercode|getpg)$/i
 handler.owner = true
 
-export default handler*/
-
-
-
-/*import fs from 'fs'
-import path from 'path'
-import { promisify } from 'util'
-import fse from 'fs-extra'
-
-const readdir = promisify(fs.readdir)
-const readFile = promisify(fse.readFile)
-
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) throw `*POR FAVOR RSCRIBA EL NOMBRE DEL ARCHIVO O EL COMANDO PARA ENVIAR EL CÓDIGO*\n*EJEMPLO*\n\n*${usedPrefix + command} menu-menu.js*\n*${usedPrefix + command} menu*`
-
-const pluginsDir = './plugins'
-const files = await readdir(pluginsDir)
-try {
-  
-const nombreArchivo = text.replace(/\.js$/, '') 
-const contenidoArchivo = `${nombreArchivo}.js`
-
-const contenido = await readFile(path.join(process.cwd(), pluginsDir, contenidoArchivo))
-await conn.sendMessage(m.chat, { document: contenido, mimetype: 'text/javascript', fileName: contenidoArchivo }, { quoted: m })
-await m.reply(`\`\`\`CÓDIGO DEL ARCHIVO ${contenidoArchivo}\`\`\`\n${String.fromCharCode(8206).repeat(850)}\n${contenido.toString()}`)
-return
-} catch (err)  {
- 
-let matchingFile;
-  if (!matchPluginCommand(command, text)) {
-    if (Array.isArray(command)) {
-      for (let cmd of command) {
-        if (matchPluginCommand(cmd, text)) {
-          matchingFile = files.find(file => (await import(path.join(process.cwd(), pluginsDir, file))).default.command === cmd)
-          break
-        }
-      }
-    }
-    if (!matchingFile && command instanceof RegExp && command.test(text)) {
-      matchingFile = files.find(file => (await import(path.join(process.cwd(), pluginsDir, file))).default.command instanceof RegExp && (await import(path.join(process.cwd(), pluginsDir, file))).default.command.test(text))
-    }
-  } else {
-    matchingFile = files.find(file => (await import(path.join(process.cwd(), pluginsDir, file))).default.command === command)
-  }
-
-  if (!matchingFile) {
-    return m.reply(`*EL CÓDIGO PARA '${text}' NO FUE ENCONTRADO*`)
-  }
-
-  try{
-    const plugin = (await import(path.join(process.cwd(), pluginsDir, matchingFile))).default
-    const filename = matchingFile.replace('.js', '')
-    const fileContent = await readFile(path.join(process.cwd(), pluginsDir, matchingFile), 'utf-8')  
-    let fileContentT = await fs.readFileSync(`./plugins/${filename}.js`)
-
-    await conn.sendMessage(m.chat, { document: fileContentT, mimetype: 'text/javascript', fileName: filename + '.js' }, { quoted: m })
-    await m.reply(`\`\`\`CÓDIGO DEL ARCHIVO ${filename}.js\`\`\`\n${String.fromCharCode(8206).repeat(850)}\n${fileContent.toString()}`)
-  } catch (err) {
-    console.log(`Error al enviar el archivo '${matchingFile}': ${err.message}`)
-    return m.reply(`Ocurrió un error al enviar el archivo '${matchingFile}'`)
-  }
-}}}
-
-handler.command = /^(getplugin|gp|obtenercodigo|obtenercode|getpg)$/i
-handler.owner = true
-
 export default handler
-
-function matchPluginCommand(command, text) {
-  if (Array.isArray(command)) {
-    return command.some(cmd => text === cmd)
-  } else if (command instanceof RegExp) {
-    return command.test(text)
-  } else {
-    return text === command
-  }
-}*/
 
 
 
