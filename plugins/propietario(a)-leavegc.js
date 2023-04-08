4cb3975c1ca1d48c0173ce8f75f771853ed043e1
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-08 14:32:13 -0300

    Update propietario(a)-leavegc.js

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*GATABOT ABANDONA EL GRUPO FUE UN GUSTO ESTA AQUI HASTA PRONTO 👋*') 
+await conn.reply(id, '*${wm} ABANDONA EL GRUPO FUE UN GUSTO ESTA AQUI HASTA PRONTO 👋*') 
 await conn.groupLeave(id)}
 handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
 handler.group = true
