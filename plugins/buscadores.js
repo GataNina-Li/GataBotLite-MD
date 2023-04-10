a49621cb40f1ec1c34d6a7c93938db01bacd6bfc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-10 17:01:30 -0500

    Update buscadores.js

--- plugins/buscadores.js
+++ plugins/buscadores.js
@@ -1,10 +1,14 @@
 import { googleIt } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
+import axios from 'axios'
+
 let handler = async (m, { conn, command, usedPrefix, args, text }) => {
 const isCommand1 = /^(googlef?)$/i.test(command)
 const isCommand2 = /(openai|chatgpt|ia|ai)/i.test(command)
 const isCommand3 = /^(bot|simi|simsimi|alexa|bixby|cortana|siri|okgoogle)$/i.test(command)
+const isCommand4 = /^(githubstalk|usuariogithub|usergithub)$/i.test(command)
 
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 async function reportError(e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
@@ -51,7 +55,61 @@ m.reply(`${tes}`)
 reportError(e)
 }     
 break
+        
+case isCommand4:
+if (!text) return conn.reply(m.chat, '*⚠️ INGRESE EL NOMBRE DE UN USUARIO DE GITHUB*', m)
+await m.reply('*B U S C A N D O ✅*')
+try{
+let err = 'No encontrado'
+let request = await githubstalk(text) 
+let { username, following, followers, type, bio, company, blog, location, email, public_repo, public_gists, profile_pic } = request
+let thumb = await (profile_pic)
+let cont = `*╭ • • • ◜G I T H U B◞ • • • ╮*
+➸ *nombre de usuario*: ${username || err}
+➸ *Bio*: ${bio || err}
+➸ *Compañía*: ${company || err}
+➸ *Email:* ${email || err}
+➸ *blogs:* ${blog || err}
+➸ *Repositorios públicos:* ${public_repo || err}
+➸ *Esenciales públicas:* ${public_gists || err}
+➸ *seguidores:* ${followers || err}
+➸ *siguiente:* ${following || err}
+➸ *Ubicación:* ${location || err}
+➸ *tipo:* ${type || err}`
+await conn.sendFile(m.chat, thumb || gataMenu.getRandom(), 'githubstalk.jpg', cont, fkontak) 
+} catch (e) {
+reportError(e)
+}  
+async function githubstalk(user) {
+return new Promise((resolve, reject) => {
+axios.get('https://api.github.com/users/'+user)
+.then(({ data }) => {
+let hasil = {
+ username: data.login,
+ nickname: data.name,
+ bio: data.bio,
+ id: data.id,
+ nodeId: data.node_id,
+ profile_pic: data.avatar_url,
+ url: data.html_url,
+ type: data.type,
+ admin: data.site_admin,
+ company: data.company,
+ blog: data.blog,
+ location: data.location,
+ email: data.email,
+ public_repo: data.public_repos,
+ public_gists: data.public_gists,
+ followers: data.followers,
+ following: data.following,
+ ceated_at: data.created_at,
+ updated_at: data.updated_at
+}
+resolve(hasil)
+})
+})        
+break
 }}
 
-handler.command = /^(googlef?|openai|chatgpt|ia|ai|bot|simi|simsimi|alexa|bixby|cortana|siri|okgoogle)$/i
+handler.command = /^(googlef?|openai|chatgpt|ia|ai|bot|simi|simsimi|alexa|bixby|cortana|siri|okgoogle|githubstalk|usuariogithub|usergithub)$/i
 export default handler
