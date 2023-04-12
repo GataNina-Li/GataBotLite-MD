be82cfa7f8deab2d13c88e547a468825c5c59c4f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-12 04:11:52 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -1,7 +1,6 @@
-//import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fsPromises } from "fs";
 const fs = { ...fsPromises, existsSync };
-import path, { join } from 'path'
+import path, { join } from 'path' 
 
 let handler = async (m, { conn, command, usedPrefix, args, text, isOwner }) => {
 const isCommand1 = /^(deletesesion|eliminarsesion|borrarsesion|delsesion|delsession|cerrarsesion)$/i.test(command)  
