74a97c903e263206bed645ceedf35aacf5875478
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-12 03:44:37 -0500

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -19,7 +19,7 @@ let uniqid = `${who.split`@`[0]}`
 const path = `./GataJadiBot/${uniqid}`
 let comd = `${lenguajeGB.lenguaje() == 'es' ? 'serbot' : 'jadibot'}`
 
-/*if (!fs.existsSync(path)) {
+if (!fs.promises.existsSync(path)) {
 await conn.sendMessage(m.chat, { text: lenguajeGB.smsFoldErr(usedPrefix, comd) }, { quoted: m })}
 if (global.conn.user.jid !== conn.user.jid) return conn.sendMessage(m.chat, {text: lenguajeGB.smsJBDel() + `\n\n*https://api.whatsapp.com/send/?phone=${global.conn.user.jid.split`@`[0]}&text=${usedPrefix + command}&type=phone_number&app_absent=0*`}, { quoted: m }) 
 else {
@@ -29,22 +29,7 @@ fs.rmdir("./GataJadiBot/" + uniqid, { recursive: true, force: true })
 await conn.sendMessage(m.chat, { text : lenguajeGB.smsJBCerrarS() } , { quoted: m })
 } catch (e) {
 reportError(e)
-}  */
-if (!fs.existsSync(path)) {
-  await conn.sendMessage(m.chat, { text: lenguajeGB.smsFoldErr(usedPrefix, comd) }, { quoted: m })
-}
-if (global.conn.user.jid !== conn.user.jid) {
-  return conn.sendMessage(m.chat, {text: lenguajeGB.smsJBDel() + `\n\n*https://api.whatsapp.com/send/?phone=${global.conn.user.jid.split`@`[0]}&text=${usedPrefix + command}&type=phone_number&app_absent=0*`}, { quoted: m })
-} else {
-  await conn.sendMessage(m.chat, { text: lenguajeGB.smsJBAdios() }, { quoted: m })
-}
-try {
-  await fs.rmdir("./GataJadiBot/" + uniqid, { recursive: true, force: true })
-  await conn.sendMessage(m.chat, { text : lenguajeGB.smsJBCerrarS() } , { quoted: m })
-} catch (e) {
-  reportError(e)
-}
-
+}  
 break
     
 case isCommand2:
