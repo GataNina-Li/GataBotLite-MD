83f66305c37bb1f63fb7f95b4e2f83e9b9c03e6b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-11 20:50:33 -0500

    Update jadibot-broadcast.js

--- plugins/jadibot-broadcast.js
+++ plugins/jadibot-broadcast.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, usedPrefix, text }) => {
   ${users.map(v => '🐈 Wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${encodeURIComponent(usedPrefix)}estado`).join('\n')}
   \n*𝗦𝗘 𝗙𝗜𝗡𝗔𝗟𝗜𝗭𝗢 𝗖𝗢𝗡 𝗘𝗟 𝗘𝗡𝗩𝗜𝗢 𝗘𝗡 ${users.length * 1.5} 𝗦𝗘𝗚𝗨𝗡𝗗𝗢𝗦 𝗔𝗣𝗥𝗢𝗫𝗜𝗠𝗔𝗗𝗔𝗠𝗘𝗡𝗧𝗘`.trim(), m)
   }
-  handler.command = /^bcbot$/i
+  handler.command = /^bcsubbot|bcsubot$/i
   handler.owner = true
   
   export default handler
