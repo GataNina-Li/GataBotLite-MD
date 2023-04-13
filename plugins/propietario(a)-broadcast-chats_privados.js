8539a15651d3b19d057844b6f9afcbc6a8eb1cde
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-12 22:11:20 -0500

    Update propietario(a)-broadcast-chats_privados.js

--- plugins/propietario(a)-broadcast-chats_privados.js
+++ plugins/propietario(a)-broadcast-chats_privados.js
@@ -3,13 +3,13 @@ let cc3 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks3 = text ? text : cc3.text  
 let chats = Object.keys(await conn.chats)
 await conn.reply(m.chat, "*ENVIANDO MENSAJE, ESPERE UN MOMENTO...*", m)
-for (let id of chats) {
+/*for (let id of chats) {
 await new Promise(resolve => setTimeout(resolve, 2000)) 
 await m.reply(`✅ *COMUNICADO OFICIAL* ✅\n\n` + teks3)      
 }
 let totalPri = chats.length
-await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*`)
-/*let start = new Date().getTime() 
+await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*`)*/
+let start = new Date().getTime() 
 for (let id of chats) {
 await new Promise(resolve => setTimeout(resolve, 2000)) 
 await conn.reply(id, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks3, m)
@@ -22,11 +22,12 @@ time = (time / 60).toFixed(2) + ' minutos'
 } else {
 time = time.toFixed(2) + ' segundos'
 }
-await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*\n\nTiempo total de envío: ${time}`)*/
+await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*\n\nTiempo total de envío: ${time}`)
 }
 
-handler.command = ['bcchats'] ///^(bcchats)$/i
+handler.command = /^(bcchats)$/i 
 handler.owner = true
 
+export default handler
 
   
