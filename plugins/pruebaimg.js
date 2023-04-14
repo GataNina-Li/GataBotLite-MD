3ff515ce222717c4d4f3a0f2268f0b496070fcbf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-13 21:42:40 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -2,14 +2,20 @@ import Jimp from 'jimp';
 
 const handler = async (m, { conn, text }) => {
   const image = await Jimp.create(1200, 800, 0xffffffff);
+  //image.print(
+   // await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK),
+   // 0, 0, { text: 'ⒸⒹ La parte de la biología que estudia los animales es la zoología.', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
+   // 1200, 800
+ // );
   image.print(
-    await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK),
-    0, 0, { text: 'ⒸⒹ La parte de la biología que estudia los animales es la zoología.', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
-    1200, 800
-  );
+  await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK),
+  0, 0, { text: 'ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓜⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
+  1200, 800
+);
   const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
   conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje de prueba', m);
 }
 
 handler.command = /^pruebaimg$/i;
 export default handler;
+
