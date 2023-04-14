39169be20596f9f9bd9c217e13d8054a4d330e19
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-13 23:45:42 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -1,8 +1,8 @@
 import Jimp from 'jimp';
 import path from 'path';
-
+const __dirname = global.__dirname(import.meta.url)
 const handler = async (m, { conn, text }) => {
-  const fontPath = path.join(__dirname, '../src/Monospace.ttf');
+  const fontPath = join(__dirname, '../src/Monospace.ttf');
   const font = await Jimp.loadFont(fontPath);
   const image = await Jimp.create(1200, 800, 0xffffffff);
   image.print(
