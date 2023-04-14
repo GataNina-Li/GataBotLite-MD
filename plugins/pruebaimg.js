7643a9fafb36a9654013302d0cf257a3f30900f9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-14 14:05:08 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -18,7 +18,7 @@ const handler = async (m, { conn, text }) => {
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
   const textHeight = Jimp.measureTextHeight(font, 'W') 
   const lineHeight = Math.max(textHeight + 10, 50) 
-  font.setHeight(lineHeight) 
+  font.setHeight(50)
   image.print(font, 0, 0, { text: 'asirhf\nwjfny\nepodu', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE }, 1200, 800)
   const buffer = await image.getBufferAsync(Jimp.MIME_JPEG)
   await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m)
