ecf4d751963e4a1d4a39546bdfae4eee37226c76
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-13 21:05:10 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -4,7 +4,7 @@ const handler = async (m, { conn, text }) => {
   const image = await Jimp.create(200, 100, 0xFFFFFFFF);
   image.print(
     await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK),
-    0, 0, { text: 'HOLA MUNDO', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
+    0, 0, { text: 'En la clasificación científica de los seres vivos, los animales (Animalia) o metazoos (Metazoa) constituyen un reino que reúne un amplio grupo de organismos que son eucariotas, heterótrofos, pluricelulares y tisulares (excepto los poríferos). Se caracterizan por su amplia capacidad de movimiento, por no tener cloroplasto (aunque hay excepciones, como en el caso de Elysia chlorotica) ni pared celular, y por su desarrollo embrionario; que atraviesa una fase de blástula y determina un plan corporal fijo (aunque muchas especies pueden sufrir una metamorfosis posterior como los artrópodos). Los animales forman un grupo natural estrechamente emparentado con los hongos (reino Fungi). Animalia es uno de los cinco reinos del dominio Eukaryota, y a él pertenece el ser humano. La parte de la biología que estudia los animales es la zoología.', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
     200, 100
   );
   const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
