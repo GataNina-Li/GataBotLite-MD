d92f654ff52bc21db18890ccd0fa04a9fbf828fe
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-14 13:56:40 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -1,3 +1,19 @@
+/*import Jimp from 'jimp'
+
+const handler = async (m, { conn, text }) => {
+const image = await Jimp.create(1200, 800, 0xffffffff)
+const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+image.print( font, 0, 0, { text: `asirhf
+wjfny
+epodu`, alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE }, 1200, 800)
+const buffer = await image.getBufferAsync(Jimp.MIME_JPEG)
+await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m)
+}
+
+handler.command = /^pruebaimg$/i
+export default handler*/
+
+
 import Jimp from 'jimp'
 
 const handler = async (m, { conn, text }) => {
