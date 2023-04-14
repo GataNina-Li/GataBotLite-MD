6715578d3920605756b983c2c85ce23a81780063
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-13 22:59:37 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -1,10 +1,11 @@
 import Jimp from 'jimp';
 
 const handler = async (m, { conn, text }) => {
+  const font = await Jimp.loadFont('src/Monospace.ttf');
   const image = await Jimp.create(1200, 800, 0xffffffff);
   image.print(
-    await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE),
-    0, 0, { text: 'La parte de la biología que estudia los animales es la zoología.', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
+    font,
+    0, 0, { text: 'GataBot', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
     1200, 800
   );
   const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
