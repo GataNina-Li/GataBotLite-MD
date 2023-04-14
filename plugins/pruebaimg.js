670557cd5bfbe15a5a3586ac0907b2c916e8fa3c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-14 00:02:14 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -1,18 +1,12 @@
-import Jimp from 'jimp';
-import path from 'path';
+import Jimp from 'jimp'
 
 const handler = async (m, { conn, text }) => {
-  const fontPath = '/data/data/com.termux/files/home/GataBotLite-MD/src/Monospace.ttf'
-  const font = await Jimp.loadFont(fontPath);
-  const image = await Jimp.create(1200, 800, 0xffffffff)
-  image.print(
-    font,
-    0, 0, { text: 'GataBot', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
-    1200, 800
-  );
-  const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
-  conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje de prueba', m);
-};
+const image = await Jimp.create(1200, 800, 0xffffffff)
+const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+image.print( font, 0, 0, { text: 'GataBot', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE }, 1200, 800)
+const buffer = await image.getBufferAsync(Jimp.MIME_JPEG)
+await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m)
+}
 
 handler.command = /^pruebaimg$/i
 export default handler
