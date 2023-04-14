a4580d1457961d8d5771f24bc7eeadf099eeec4b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-13 23:14:49 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -1,16 +1,17 @@
 import Jimp from 'jimp';
+import path from 'path';
 
 const handler = async (m, { conn, text }) => {
-  const font = await Jimp.loadFont('./src/Monospace.ttf')
+  const font = await Jimp.loadFont(path.join(__dirname, './src/Monospace.ttf'))
   const image = await Jimp.create(1200, 800, 0xffffffff);
   image.print(
     font,
     0, 0, { text: 'GataBot', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE },
     1200, 800
   );
-  const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
-  conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje de prueba', m);
+  const buffer = await image.getBufferAsync(Jimp.MIME_JPEG)
+  conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje de prueba', m)
 }
 
-handler.command = /^pruebaimg$/i;
-export default handler;
+handler.command = /^pruebaimg$/i
+export default handler
