6261a8ae1075ff72de5edd4919f0b3d906ba30a2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-14 14:03:30 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -1,4 +1,4 @@
-import Jimp from 'jimp'
+/*import Jimp from 'jimp'
 
 const handler = async (m, { conn, text }) => {
 const image = await Jimp.create(1200, 800, 0xffffffff)
@@ -9,4 +9,21 @@ await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m)
 }
 
 handler.command = /^pruebaimg$/i
+export default handler*/
+
+import Jimp from 'jimp'
+
+const handler = async (m, { conn, text }) => {
+  const image = await Jimp.create(1200, 800, 0xffffffff)
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+  const textHeight = Jimp.measureTextHeight(font, 'W') 
+  const lineHeight = Math.max(textHeight + 10, 50) 
+  font.setHeight(lineHeight) 
+  image.print(font, 0, 0, { text: 'asirhf\nwjfny\nepodu', alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE }, 1200, 800)
+  const buffer = await image.getBufferAsync(Jimp.MIME_JPEG)
+  await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m)
+}
+
+handler.command = /^pruebaimg$/i
 export default handler
+
