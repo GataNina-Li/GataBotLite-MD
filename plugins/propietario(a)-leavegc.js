23252147a8f6703509779068126fb69035bfc8aa
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-20 16:42:12 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*${wm} ABANDONA EL GRUPO FUE UN GUSTO ESTA AQUI HASTA PRONTO 👋*') 
+await conn.reply(id, lenguajeGB.smsLeave()) 
 await conn.groupLeave(id)}
 handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
 handler.group = true
-handler.rowner = true
+handler.owner = true
 export default handler
