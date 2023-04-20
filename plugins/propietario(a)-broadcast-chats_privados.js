9c71247be7dfe7a6c53c054689a4428b0d9c4b31
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-20 18:30:46 -0500

    Update propietario(a)-broadcast-chats_privados.js

--- plugins/propietario(a)-broadcast-chats_privados.js
+++ plugins/propietario(a)-broadcast-chats_privados.js
@@ -1,15 +1,20 @@
+let totalPri = 0
 let handler = async (m, { conn, text }) => {
 let cc3 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks3 = text ? text : cc3.text  
 let chats = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net'))
+  
 await conn.reply(m.chat, "*ENVIANDO MENSAJE, ESPERE UN MOMENTO...*", m)
-let start = new Date().getTime() 
+let start = new Date().getTime()
 for (let user of chats) {
 await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
 await conn.reply(user, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks3, null)
-}
+totalPri++
+    
+if (totalPri >= 10) { // Si ya se ha enviado el mensaje a 1000 chats, detener el envío
+break
+}} 
 let end = new Date().getTime() 
-let totalPri = chats.length
 let time = Math.floor((end - start) / 1000)
 if (time >= 60) {
 let minutes = Math.floor(time / 60)
@@ -18,7 +23,7 @@ time = `${minutes} minutos y ${seconds} segundos`
 } else {
 time = `${time} segundos`
 }
-await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*\n\nTiempo total de envío: ${time}`)
+await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*\n\nTiempo total de envío: ${time}${totalPri >= 10 ? '\n\n*NO SE ENVIARON A TODOS PARA EVITAR SATURACIÓN*' : ''}`)
 }
 
 handler.command = /^(broadcastchats?|bcc(hats?)?)$/i
