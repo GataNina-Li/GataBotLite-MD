62c1b229333444b6131b2fd2d9b9f9e501a3e20f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-20 19:49:18 -0500

    Update propietario(a).js

--- plugins/propietario(a).js
+++ plugins/propietario(a).js
@@ -17,6 +17,8 @@ const isCommand10 = /^(restablecerdatos|borrardatos|deletedatauser)$/i.test(comm
 const isCommand11 = /^(join|nuevogrupo|newgrupo|unete)$/i.test(command)
 const isCommand12 = /^(bcbot|bcsubbot|bcsubot)$/i.test(command)
 const isCommand13 = /^((broadcast|bc)(group|grup|gc))$/i.test(command)
+const isCommand14 = /^(broadcastchats?|bcc(hats?)?)$/i.test(command)
+const isCommand15 = /^(broadcastall|bc)$/i.test(command)
 
 async function reportError(e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
@@ -224,7 +226,7 @@ case isCommand12:
 if (conn.user.jid !== global.conn.user.jid) throw false
 let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
 if (users.length === 0) {
-await m.reply('*NO HAY SUB BOT DISPONIBLES*')
+await m.reply(lenguajeGB.smsJBCom4())
 return
 }
 let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
@@ -242,7 +244,8 @@ break
         
 case isCommand13:
 let cc2 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-let teks2 = text ? text : cc2.text  
+let teks2 = text ? text : cc2.text 
+if (!text && !m.quoted) return m.reply(`RSPONDA AL MENSAJE O ESCRIBA EL MENSAJE USANDO ${usedPrefix + command}`)
 let d = new Date(new Date + 3600000)
 let locale = lenguajeGB.lenguaje()
 let dia = d.toLocaleDateString(locale, { weekday: 'long' })
@@ -262,9 +265,73 @@ let totalGP = groups.length
 await m.reply(lenguajeGB.smsChatGP3(totalGP))        
 break
         
+case isCommand13:
+let cc3 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+let teks3 = text ? text : cc3.text  
+if (!text && !m.quoted) return m.reply(`RSPONDA AL MENSAJE O ESCRIBA EL MENSAJE USANDO ${usedPrefix + command}`)
+let chats = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net')) 
+await conn.reply(m.chat, "*ENVIANDO MENSAJE, ESPERE UN MOMENTO...*", m)
+let start = new Date().getTime()
+let totalPri = 0
+for (let user of chats) {
+await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
+await conn.reply(user, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks3, null)
+totalPri++
+if (totalPri >= 1000) { 
+break
+}}   
+let end = new Date().getTime() 
+let time = Math.floor((end - start) / 1000)
+if (time >= 60) {
+let minutes = Math.floor(time / 60)
+let seconds = time % 60
+time = `${minutes} minutos y ${seconds} segundos`
+} else {
+time = `${time} segundos`
+}
+await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*\n\nTiempo total de envío: ${time}${totalPri >= 1000 ? '\n\n*NO SE ENVIARON A TODOS LOS CHATS PARA EVITAR SATURACIÓN*' : ''}`)        
+break
+        
+case isCommand15:
+let cc4 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+let teks4 = text ? text : cc4.text 
+if (!text && !m.quoted) return m.reply(`RSPONDA AL MENSAJE O ESCRIBA EL MENSAJE USANDO ${usedPrefix + command}`)
+let groups2 = Object.keys(await conn.groupFetchAllParticipating())
+let chats2 = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net'))
+await conn.reply(m.chat, "*ENVIANDO MENSAJE, ESPERE UN MOMENTO...*", m)
+let start = new Date().getTime()
+let totalPri2 = 0
+for (let group of groups2) {
+await conn.sendMessage(group, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks4, null)
+}
+for (let user of chats2) {
+await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
+await conn.sendMessage(user, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks4, null)
+totalPri2++
+if (totalPri2 >= 1000) { 
+break
+}}  
+let end2 = new Date().getTime()
+let totalPri2 = chats2.length
+let totalGroups2 = groups2.length
+let total2 = totalPri2 + totalGroups2
+let time2 = Math.floor((end2 - start) / 1000)
+if (time2 >= 60) {
+let minutes = Math.floor(time2 / 60)
+let seconds = time2 % 60
+time2 = `${minutes} minutos y ${seconds} segundos`
+} else {
+time2 = `${time2} segundos`
+} 
+await m.reply(`✅ *EL MENSAJE FUE ENVIADO AL:
+\`\`\`PRIVADO >> ${totalPri2}\`\`\`
+\`\`\`GRUPO >> ${totalGroups2}\`\`\`
+\`\`\`TOTAL >> ${total2}\`\`\`\n\nTiempo total de envío: ${time2}${totalPri2 >= 1000 ? '\n\n*NO SE ENVIARON A TODOS LOS CHATS PRIVADOS PARA EVITAR SATURACIÓN*' : ''}`)        
+break
+        
 }}
 
-handler.command = /^(backup|respaldo|copia|ban(user|usuario|earuser|earusuario)|seradmin|autoadmin|tenerpoder|(set|cambiar|nueva|new)(bio|botbio|biobot)|(set|cambiar|nuev(a|o)?|new)(name|botname|namebot|nombre|nombrebot|botnombre)|(set|cambiar|nueva|new)(ppbot|botpp|fotobot|botfoto)|update|actualizar|ups|banchat|banearchat|block|unblock|bloquear|desbloquear|restablecerdatos|borrardatos|deletedatauser|join|nuevogrupo|newgrupo|unete|bcbot|bcsubbot|bcsubot|(broadcast|bc)(group|grup|gc))$/i
+handler.command = /^(backup|respaldo|copia|ban(user|usuario|earuser|earusuario)|seradmin|autoadmin|tenerpoder|(set|cambiar|nueva|new)(bio|botbio|biobot)|(set|cambiar|nuev(a|o)?|new)(name|botname|namebot|nombre|nombrebot|botnombre)|(set|cambiar|nueva|new)(ppbot|botpp|fotobot|botfoto)|update|actualizar|ups|banchat|banearchat|block|unblock|bloquear|desbloquear|restablecerdatos|borrardatos|deletedatauser|join|nuevogrupo|newgrupo|unete|bcbot|bcsubbot|bcsubot|(broadcast|bc)(group|grup|gc)|broadcastchats?|bcc(hats?)?|broadcastall|bc)$/i
 handler.owner = true
 
 export default handler
