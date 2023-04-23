a7f64e11d40b9dc8881ec5e50b5b498a2c18781d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-23 16:53:09 -0500

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 //if (!text) throw `*INGRESE SU NOMBRE Y EDAD PARA ESTAR REGISTRADO*\n*EJEMPLO*\n\n${usedPrefix + command} GataBot.18`  
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.getProfilePicture(who) || img2
+let pp = await conn.profilePictureUrl(m.sender).catch(_ => img2) 
 let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
 let user = global.db.data.users[m.sender]
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
