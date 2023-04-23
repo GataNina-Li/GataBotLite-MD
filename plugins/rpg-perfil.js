9cd471c2c8111edc6663f47280618748682dd85c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-23 18:36:32 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -4,31 +4,26 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 //if (!text) throw `*INGRESE SU NOMBRE Y EDAD PARA ESTAR REGISTRADO*\n*EJEMPLO*\n\n${usedPrefix + command} GataBot.18`  
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-//let pp = await conn.profilePictureUrl(m.sender).catch(_ => img2) 
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => img2)
 let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
 let user = global.db.data.users[m.sender]
 let tag = `${m.sender.split("@")[0]}`
 let aa = tag + '@s.whatsapp.net'
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
-let str = `*⎔ USUARIO* 
+let str = `*⎔ ${lenguajeGB.smsPerfil1()}* 
 • @${tag}
 
-*⎔ NOMBRE* 
+*⎔ ${lenguajeGB.smsPerfil2()}* 
 • ${name}
 
-*⎔ EDAD*
+*⎔ ${lenguajeGB.smsPerfil3()}*
 • ${age}
 
-*⎔ NÚMERO*
+*⎔ ${lenguajeGB.smsPerfil4()}*
 • ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 
-*⎔ ID DE REGISTRO*
+*⎔ ${lenguajeGB.smsPerfil5()}*
 • \`\`\`${sn}\`\`\``.trim()
-const mention = {
-  text: str,
-  user_ids: [m.sender.split('@')[0]]
-}
 await conn.sendFile(m.chat, pp, 'gata.jpg', str, m, false, { mentions: [aa] }) 
 }
 handler.command = /^perfil|profile?$/i
