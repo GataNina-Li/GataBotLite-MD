360a821d5ca418e59314243216eb4fda31baafaa
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-24 01:36:50 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -3,76 +3,40 @@ import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
 
-/*let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+try{
 let stiker = false
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
+
 if (!/webp|image|video/g.test(mime) && !text) return m.reply(lenguajeGB.smsSticker1(usedPrefix, command))
 if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply(lenguajeGB.smsSticker2())
-try {
+
 if (/webp|image|video/g.test(mime)) {
 let img = await q.download?.()
 let out
-try {
 stiker = await sticker(img, false, global.packname, global.author)
-} catch (e) {
-console.error(e)
-} finally {
+  
 if (!stiker) {
 if (/webp/g.test(mime)) out = await webp2png(img)
 else if (/image/g.test(mime)) out = await uploadImage(img)
 else if (/video/g.test(mime)) out = await uploadFile(img)
 if (typeof out !== 'string') out = await uploadImage(img)
 stiker = await sticker(false, out, global.packname, global.author)
-}}
-} else if (args[0]) {
+  
+if (!stiker) errorMessage = lenguajeGB['smsMensError2']()
+}} else if (args[0]) {
 if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-else return m.reply(lenguajeGB.smsSticker3(usedPrefix, command))
-}
+else return m.reply(lenguajeGB.smsSticker3(usedPrefix, command))}
+
+if (stiker) {
+conn.sendFile(m.chat, stiker, 'sticker.webp', '', null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: '• STICKER •', mediaType: 2, sourceUrl: redesMenu.getRandom(), thumbnail: gataImg.getRandom()}}})}
+//} else {
 } catch (e) {
-console.error(e)
-if (!stiker) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: '• STICKER •', mediaType: 2, sourceUrl: redesMenu.getRandom(), thumbnail: gataImg.getRandom()}}})
-else { 
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(stiker)}
-}}*/
-
-let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-  let stiker = false
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || q.mediaType || ''
-  if (!/webp|image|video/g.test(mime) && !text) return m.reply(lenguajeGB.smsSticker1(usedPrefix, command))
-  if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply(lenguajeGB.smsSticker2())
-
-  let errorMessage = ''
-  if (/webp|image|video/g.test(mime)) {
-    let img = await q.download?.()
-    let out
-    stiker = await sticker(img, false, global.packname, global.author)
-    if (!stiker) {
-      if (/webp/g.test(mime)) out = await webp2png(img)
-      else if (/image/g.test(mime)) out = await uploadImage(img)
-      else if (/video/g.test(mime)) out = await uploadFile(img)
-      if (typeof out !== 'string') out = await uploadImage(img)
-      stiker = await sticker(false, out, global.packname, global.author)
-      if (!stiker) errorMessage = lenguajeGB['smsMensError2']()
-    }
-  } else if (args[0]) {
-    if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-    else return m.reply(lenguajeGB.smsSticker3(usedPrefix, command))
-  }
-
-  if (stiker) {
-    conn.sendFile(m.chat, stiker, 'sticker.webp', '', null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: '• STICKER •', mediaType: 2, sourceUrl: redesMenu.getRandom(), thumbnail: gataImg.getRandom()}}})
-  } else {
-    await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
-    console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-  }
-}
-
+console.log(e)
+}}
 
 handler.command = /^(s(tickers?)?(image|video|gif|img)?)$/i
 export default handler
