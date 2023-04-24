b2d70e2f84f5e379bd5f318e4331d8804c6cff48
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-24 12:40:34 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/propietario(a).js
+++ plugins/propietario(a).js
@@ -223,9 +223,9 @@ reportError(e)
 break
 
 case isCommand12:
-if (!text && !m.quoted) return m.reply(`*RSPONDA AL MENSAJE O ESCRIBA EL MENSAJE USANDO ${usedPrefix + command}*`)
+if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))
 if (conn.user.jid !== global.conn.user.jid) throw false
-let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
+users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
 if (users.length === 0) {
 await m.reply(lenguajeGB.smsJBCom4())
 return
@@ -244,7 +244,7 @@ await conn.reply(m.chat, lenguajeGB.smsJBDifu2(numUser, difuUser, tolUser).trim(
 break
         
 case isCommand13:
-if (!text && !m.quoted) return m.reply(`*RSPONDA AL MENSAJE O ESCRIBA EL MENSAJE USANDO ${usedPrefix + command}*`)        
+if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))        
 let cc2 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks2 = text ? text : cc2.text 
 let d = new Date(new Date + 3600000)
@@ -267,16 +267,16 @@ await m.reply(lenguajeGB.smsChatGP3(totalGP))
 break
         
 case isCommand14:
-if (!text && !m.quoted) return m.reply(`*RSPONDA AL MENSAJE O ESCRIBA EL MENSAJE USANDO ${usedPrefix + command}*`)        
+if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))        
 let cc3 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks3 = text ? text : cc3.text  
 let chats = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net')) 
-await conn.reply(m.chat, "*ENVIANDO MENSAJE, ESPERE UN MOMENTO...*", m)
+await conn.reply(m.chat, lenguajeGB.smsBCMensaje2(), m)
 let start = new Date().getTime()
 let totalPri = 0
 for (let user of chats) {
 await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
-await conn.reply(user, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks3, null)
+await conn.reply(user, `${lenguajeGB.smsBCbot7()}\n\n` + teks3, null)
 totalPri++
 if (totalPri >= 3000) { 
 break
@@ -290,25 +290,25 @@ time = `${minutes} minutos y ${seconds} segundos`
 } else {
 time = `${time} segundos`
 }
-await m.reply(`✅ *EL MENSAJE FUE ENVIADO A ${totalPri} CHAT(S) PRIVADO(S)*\n\nTiempo total de envío: ${time}${totalPri >= 3000 ? '\n\n*NO SE ENVIARON A TODOS LOS CHATS PARA EVITAR SATURACIÓN*' : ''}`)        
+await m.reply(lenguajeGB.smsBCMensaje(totalPri, time))        
 break
         
 case isCommand15:
-if (!text && !m.quoted) return m.reply(`*RSPONDA AL MENSAJE O ESCRIBA EL MENSAJE USANDO ${usedPrefix + command}*`)        
+if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))   
 let cc4 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks4 = text ? text : cc4.text 
 let groups2 = Object.keys(await conn.groupFetchAllParticipating())
 let chats2 = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net'))
-await conn.reply(m.chat, "*ENVIANDO MENSAJE, ESPERE UN MOMENTO...*", m)
+await conn.reply(m.chat, lenguajeGB.smsBCMensaje2(), m)
 let start2 = new Date().getTime()
 let usersTag2 = participants.map(u => conn.decodeJid(u.id))
 let totalPri2 = 0
 for (let group of groups2) {
-await conn.reply(group, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks4, { mentions: usersTag2 }, { quoted: m })     
+await conn.reply(group, `${lenguajeGB.smsBCbot7()}\n\n` + teks4, { mentions: usersTag2 }, { quoted: m })     
 }
 for (let user of chats2) {
 await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
-await conn.reply(user, `✅ *COMUNICADO OFICIAL* ✅\n\n` + teks4, null)
+await conn.reply(user, `${lenguajeGB.smsBCbot7()}\n\n` + teks4, null)
 totalPri2++
 if (totalPri2 >= 3000) { 
 break
@@ -325,10 +325,10 @@ time2 = `${minutes} minutos y ${seconds} segundos`
 } else {
 time2 = `${time2} segundos`
 } 
-await m.reply(`✅ *MENSAJE ENVIADO:*
-\`\`\`PRIVADO >> ${totalPrivate2}\`\`\`
-\`\`\`GRUPO >>   ${totalGroups2}\`\`\`
-\`\`\`TOTAL >>   ${total2}\`\`\`\n\nTiempo total de envío: ${time2}${totalPri2 >= 3000 ? '\n\n*NO SE ENVIARON A TODOS LOS CHATS PRIVADOS PARA EVITAR SATURACIÓN*' : ''}`)        
+await m.reply(`${lenguajeGB.smsBCbot1()}
+\`\`\`${lenguajeGB.smsBCbot2()} >> ${totalPrivate2}\`\`\`
+\`\`\`${lenguajeGB.smsBCbot3()} >>   ${totalGroups2}\`\`\`
+\`\`\`${lenguajeGB.smsBCbot4()} >>   ${total2}\`\`\`\n\n*${lenguajeGB.smsBCbot5()} ${time2}*\n${totalPri2 >= 3000 ? `\n*${lenguajeGB.smsBCbot6()}*` : ''}`)        
 break
         
 }}
