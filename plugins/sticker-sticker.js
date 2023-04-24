a961e281ad88e75a3b7956f5f3e3846a40f7a8f2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-24 01:48:34 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -22,7 +22,8 @@ else if (/image/g.test(mime)) out = await uploadImage(img)
 else if (/video/g.test(mime)) out = await uploadFile(img)
 if (typeof out !== 'string') out = await uploadImage(img)
 stiker = await sticker(false, out, global.packname, global.author)
- 
+  
+if (!stiker) errorMessage = lenguajeGB['smsMensError2']()
 }} else if (args[0]) {
 if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
 else return m.reply(lenguajeGB.smsSticker3(usedPrefix, command))}
