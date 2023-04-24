d830794a865447f1c83ef3cf6734eeb7bd5095e8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-23 19:12:56 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -1,93 +1,47 @@
 import { createHash } from 'crypto'
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
-let handler = async function (m, { text, usedPrefix, command }) {
-	function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
+let handler = async function (m, { conn, text, usedPrefix, command }) {
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
 }
-	let namae = conn.getName(m.sender)
-	const sections = [
-	{
-	title: "SELECCIONA TU EDAD AQUI!",
-	rows: [
-	    {title: "Años Random", rowId: '.reg ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])}
-	]
-    },
-    {
-	title: "ADULTO",
-	rows: [
-	    {title: "30 Años", rowId: '.reg ' + namae + '.30 '},
-	    {title: "29 Años", rowId: '.reg ' + namae + '.29 '},
-	    {title: "28 Años", rowId: '.reg ' + namae + '.28 '},
-	{title: "27 Años", rowId: '.reg ' + namae + '.27 '},
-	{title: "26 Años", rowId: '.reg ' + namae + '.26 '},
-	{title: "25 Años", rowId: '.reg ' + namae + '.25 '},
-	{title: "24 Años", rowId: '.reg ' + namae + '.24 '},
-	{title: "23 Años", rowId: '.reg ' + namae + '.23 '},
-	{title: "22 Años", rowId: '.reg ' + namae + '.22 '},
-	{title: "21 Años", rowId: '.reg ' + namae + '.21 '}
-	]
-    },
-    {
-	title: "JOVEN",
-	rows: [
-	    {title: "20 Años", rowId: '.reg ' + namae + '.20 '},
-	    {title: "19 Años", rowId: '.reg ' + namae + '.19 '},
-	    {title: "18 Años", rowId: '.reg ' + namae + '.18 '},
-	{title: "17 Años", rowId: '.reg ' + namae + '.17 '},
-	{title: "16 Años", rowId: '.reg ' + namae + '.16 '},
-	{title: "15 Años", rowId: '.reg ' + namae + '.15 '},
-	{title: "14 Años", rowId: '.reg ' + namae + '.14 '},
-	{title: "13 Años", rowId: '.reg ' + namae + '.13 '},
-	{title: "12 Años", rowId: '.reg ' + namae + '.12 '},
-	{title: "11 Años", rowId: '.reg ' + namae + '.11 '},
-	{title: "10 Años", rowId: '.reg ' + namae + '.10 '},
-	{title: "9 Años", rowId: '.reg ' + namae + '.9 '}
-	]
-    },
-]
+let tag = `${m.sender.split("@")[0]}`
+let aa = tag + '@s.whatsapp.net'
 
-const listMessage = {
-  text: `┆ POR FAVOR SELECCIONA TU EDAD PULSADL EL BOTON...\n┆TU NOMBRE: ${conn.getName(m.sender)}\n┆QUIERES PONER OTRO NOMBRE?\n┆ESCRIBI *${usedPrefix + command} Nombre.Años*\n╰⸺ ⊹ ⸺  ⊹ ⸺ ⊹ ⸺ ⊹ ⸺ ⊹ 》*`,
-  footer: global.wm,
-  title: "╭⸺ ⊹ ⸺  ⊹ ⸺ ⊹ ⸺ ⊹ ⸺ ⊹ 》*",
-  buttonText: "Pulsa Aqui!",
-  sections
-}
+let user = global.db.data.users[m.sender]
+if (user.registered === true) throw `${iig}YA ESTAS REGISTRADO(A)!!\nSI QUIERE ANULAR SU REGISTRO USE ESTE COMANDO\n*${usedPrefix}unreg numero de serie*\n\nSI NI RECUEDAD SU NUMERO DE SERIE USE ESTE COMANDO\n*${usedPrefix}myns`
+let [_, name, splitter, age] = text.match(Reg)
+if (!name) throw '*INGRESE SU NOMBRE*'
+if (!age) throw '*INGRESE SU EDAD*'
+age = parseInt(age)
+if (age > 50) throw '*USTED ES MUY MAYOR*'
+if (age < 10) throw '*USTED ES MUY MENOR*'
+if (name.length >= 30) throw '*ESCRIBA UN NOMBRE MÁS CORTO*' 
+user.name = name.trim() + 'ͧͧͧͦꙶͣͤ✓ᚲᴳᴮ'
+user.age = age
+user.regTime = + new Date
+user.registered = true
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => gataMenu.getRandom())
+let caption = `✅ *V E R I F I C A D O* ✅
 
-  let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `${iig}YA ESTAS REGISTRADO(A)!!\nSI QUIERE ANULAR SU REGISTRO USE ESTE COMANDO\n*${usedPrefix}unreg numero de serie*\n\nSI NI RECUEDAD SU NUMERO DE SERIE USE ESTE COMANDO\n*${usedPrefix}myns`
-  if (!Reg.test(text)) return conn.sendMessage(m.chat, listMessage, m)
-  let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '🐈 El nombre no puede esta vacio'
-  if (!age) throw '🐈 La edad no puede esta vacias'
-  age = parseInt(age)
-  if (age > 100) throw '👴🏻 Pa esta viejos'
-  if (age < 5) throw '🚼  Basado, los bebes saber escribir.✍️😳'
-  if (name.length >= 30) throw '🐈 Fua que basado, el nombre es muy largo que quiere un puente como nombre😹' 
-  user.name = name.trim()
-  user.age = age
-  user.regTime = + new Date
-  user.registered = true
-  let sn = createHash('md5').update(m.sender).digest('hex')
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender
-global.db.data.users[m.sender].exp += 400
-  let caption = `╭━━━[ *𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 | 𝙑𝙀𝙍𝙄𝙁𝙔* ]━━━━⬣
-┃ *NOMBRE* 
-┃ ${name}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *EDAD* 
-┃ *${age} años*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *BONO* 
-┃ *$350 XP*
-╰━━━━━━━━━━━━━━━━━━⬣`
-//let author = global.author
-await conn.sendButton(m.chat, caption, `ᴛᴜ ɴᴜᴍᴇʀᴏ ᴅᴇ sᴇʀɪᴇ ᴛᴇ sᴇʀᴠɪʀᴀ ᴇɴ ᴄᴀsᴏ ǫᴜᴇ ǫᴜɪᴇʀᴀ ʙᴏʀʀᴀʀ sᴜ ʀᴇɢɪsᴛʀᴏ\nᴇᴊᴇᴍᴘʟᴏ ${usedPrefix}unreg ɴᴜᴍᴇʀᴏ ᴅᴇ sᴇʀɪᴇ\n${wm}`, [['𝘼𝙝𝙤𝙧𝙖 𝙚𝙨𝙩𝙤𝙮 𝙑𝙚𝙧𝙞𝙛𝙞𝙘𝙖𝙙𝙤(𝙖)!! ✅', '/profile']], m)
+*⎔ ${lenguajeGB.smsPerfil1()}* 
+• @${tag}
+
+*⎔ ${lenguajeGB.smsPerfil2()}* 
+• ${name}
+
+*⎔ ${lenguajeGB.smsPerfil3()}*
+• ${age}
+
+*⎔ INSIGNIA DE VERIFICACIÓN*
+• 'ͧͧͧͦꙶͣͤ✓ᚲᴳᴮ'
+
+*⎔ ${lenguajeGB.smsPerfil5()}*
+• \`\`\`${sn}\`\`\``.trim()
+await conn.sendFile(m.chat, pp, 'gata.jpg', caption, m, false, { mentions: [aa] }) 
+await m.reply(`*SU ID DE REGISTRO SERVIRÁ EN CASO QUE DESE MODIFICAR O ELIMINAR SU REGISTRO USANDO ${usedPrefix}unreg*`) 
 await m.reply(`${sn}`) 
 }
-handler.help = ['daftar', 'register'].map(v => v + ' <nama>.<umur>')
-handler.tags = ['rg']
-
 handler.command = /^(verify|verificar|reg(ister)?)$/i
-
 export default handler
