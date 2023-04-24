464c0e2303cdc808cf9657566da4979a4ada8e16
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-24 12:23:15 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,11 +1,9 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
-await m.reply(`⬇️ *ESE ES SU NUMERO DE SERIE* ⬇️`.trim())
+await m.reply(lenguajeGB.smsIDserie())
 await m.reply(`${sn}`.trim())
 }
-handler.help = ['myns']
-handler.tags = ['xp']
 handler.command = /^(myns|ceksn|numid|idregistro|idregister)$/i
 handler.register = true
 export default handler
