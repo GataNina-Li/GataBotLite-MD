5ac3be60c6cf2051e67e9b1b642c66d13299ab82
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-23 23:36:26 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,10 +1,10 @@
-import { sticker } from '../lib/sticker.js'
+/*import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let stiker = false
-
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 10 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*')
@@ -39,8 +39,51 @@ await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() 
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(stiker)}
 }}
-handler.command = /^(s(ticker)?(image|video|gif|img)?|stickers?(image|video|gif))$/i
+handler.command = /^(s(tickers?)?(image|video|gif|img)?)$/i
 export default handler
 
 const isUrl = (text) => {
-return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}
+return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}*/
+
+
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let stiker = false
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || q.mediaType || ''
+  if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 10 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*')
+  if (/webp|image|video/g.test(mime)) {
+    let img = await q.download?.()
+    if (!img) throw `RESPONDER A UN VÍDEO, IMAGEN, O ESCRIBA ${usedPrefix + command} JUNTO A UN ENLACE QUE TERMINE EN .jpg .jpeg .gif .png`
+    let out
+    stiker = await sticker(img, false, global.packname, global.author)
+    if (!stiker) {
+      if (/webp/g.test(mime)) out = await webp2png(img)
+      else if (/image/g.test(mime)) out = await uploadImage(img)
+      else if (/video/g.test(mime)) out = await uploadFile(img)
+      if (typeof out !== 'string') out = await uploadImage(img)
+      stiker = await sticker(false, out, global.packname, global.author)
+    }
+  } else if (args[0]) {
+    if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+    else return m.reply(`*EL ENLACE NO ES VALIDO, DEBE DE TERMINAR EN .jpg .jpeg .gif .png EJEMPLO:\n${usedPrefix + command} ${img}*`)
+  }
+  if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+  else { 
+    await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+    console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+    console.log(stiker)
+  }
+}
+
+handler.command = /^(s(tickers?)?(image|video|gif|img)?)$/i
+
+export default handler
+
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()
+
