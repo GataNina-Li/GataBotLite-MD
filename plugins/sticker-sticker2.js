6ec571f2ee5f9232e522c0d6f6cb3c98ddcfa21e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-23 20:51:46 -0500

    Update sticker-sticker2.js

--- plugins/sticker-sticker2.js
+++ plugins/sticker-sticker2.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
     if (/webp|image|video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 30) return m.reply('Maksimal 10 detik!')
       let img = await q.download?.()
       if (!img) throw `balas gambar/video/stiker dengan perintah ${usedPrefix + command}`
       let out
