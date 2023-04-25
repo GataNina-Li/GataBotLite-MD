6b587a33e85ca7b1f30dc8d557a52625fe8dde7f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-25 17:11:22 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,16 +7,17 @@ let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
 let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
+let toUser = `${m.sender.split("@")[0]}`
+let aa = toUser + '@s.whatsapp.net'
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-if (m.text.includes(linkThisGroup)) return m.reply('Este es el enlace de invitación del grupo!')
+if (m.text.includes(linkThisGroup)) return m.reply('*ESTE ENLACE ES DE ESTE GRUPO, SÓLO POR ESO NO SERÁ ELIMINADO(A)*')
 if (!isBotAdmin) return m.reply(`${lenguajeGB['smsAllAdmin']()}`)  
 if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
 if (chat.antiLink && isGroupLink && !isAdmin) { 
 if (isBotAdmin) {
-//await m.reply(`${lenguajeGB['smsEnlaceWat']()} ${await this.getName(m.sender)}`)
-await conn.reply(m.chat, `${lenguajeGB['smsEnlaceWat']()} *@${await this.getName(m.sender)}*`, null, { mentions: [m.sender] })
+await conn.reply(m.chat, `${lenguajeGB['smsEnlaceWat']()} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 } else if (!bot.restrict) return m.reply(`${lenguajeGB['smsSoloOwner']()}`)
