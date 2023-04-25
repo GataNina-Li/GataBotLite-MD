45e5570780b0cfb9d1f98f426c066df048ae8a90
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-25 19:11:17 -0300

    Add files via upload

---
+++ plugins/_anti-internacional.js
@@ -0,0 +1,89 @@
+let handler = m => m
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
+const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
+}
+if (!m.isGroup) return !1
+let chat = global.db.data.chats[m.chat]
+if (isBotAdmin && chat.antifake) {
+let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
+		
+if (m.sender.startsWith('2' || '2')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return 
+}
+   
+if (m.sender.startsWith('265' || '265')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('90' || '90')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+	
+if (m.sender.startsWith('91' || '91')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('92' || '92')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('93' || '93')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('94' || '94')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('7' || '7')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('49' || '49')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('49' || '49')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('6' || '6')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('222' || '222')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+
+if (m.sender.startsWith('92' || '92')) {
+await conn.reply(m.chat, texto, fkontak,  m)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 	
+   
+}}
+export default handler
