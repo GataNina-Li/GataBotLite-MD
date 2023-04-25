37f15250056c940586e94e46ed5b500a7573fb8f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-25 16:58:30 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -13,13 +13,9 @@ const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.
 if (m.text.includes(linkThisGroup)) return m.reply('Este es el enlace de invitación del grupo!')
 if (!isBotAdmin) return m.reply(`${lenguajeGB['smsAllAdmin']()}`)  
 if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
-if (chat.antiLink && isGroupLink && !isAdmin) {
+if (chat.antiLink && isGroupLink && !isAdmin) { 
 if (isBotAdmin) {
-//if (m.text.includes(linkThisGroup)) return !0
-// 
-await m.reply(`${lenguajeGB['smsEnlaceWat']()} ${await this.getName(m.sender)}`)  
-}  
-if (isBotAdmin) {
+await m.reply(`${lenguajeGB['smsEnlaceWat']()} ${await this.getName(m.sender)}`)   
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 } else if (!bot.restrict) return m.reply(`${lenguajeGB['smsSoloOwner']()}`)
