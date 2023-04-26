f167f69766777e5d7ad08428e4a1c308ee93b21e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-25 22:47:26 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,5 +1,6 @@
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, participants} ) {
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, groupMetadata } ) {
+const participants = groupMetadata.participants.map(v => v.id)
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 if (!m.isGroup) return !1
