8151cfba4c0103723dab9b4166522be063d5857b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-25 20:11:23 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,4 +1,4 @@
-/*let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 export async function before(m, { isAdmin, isBotAdmin }) {
 if (m.isBaileys && m.fromMe)
 return !0
@@ -12,15 +12,17 @@ let aa = toUser + '@s.whatsapp.net'
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
-if (chat.antiLink && isGroupLink && !isAdmin) { 
+if (chat.antiLink && isGroupLink) {   
+if (isBotAdmin && bot.restrict) {   
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`) 
 if (m.text.includes(linkThisGroup)) return m.reply(lenguajeGB['smsWaMismoEnlace']())
-//if (!isBotAdmin) return m.reply(`${lenguajeGB['smsAllAdmin']()}`)  
-if (isBotAdmin) {
 await conn.reply(m.chat, `${lenguajeGB['smsEnlaceWat']()} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!bot.restrict) return m.reply(`${lenguajeGB['smsSoloOwner']()}`)
-}
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')  
+} else if (!isBotAdmin) {
+return m.reply(`${lenguajeGB['smsAvisoFG']()} ${lenguajeGB['smsAllAdmin']()}`)
+} else if (!bot.restrict) {
+return m.reply(`${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`)
+}}  
 return !0
-}*/
+}
