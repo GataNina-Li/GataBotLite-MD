1f132c710be61aaf37b6d0b701a940188894a49f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-26 01:58:30 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,21 +1,39 @@
-let handler = m => m
+/*let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, groupMetadata } ) {
-const participants = groupMetadata.participants.map(v => v.id)
-const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
-}
+let   groupMetadata.participants.map(v => v.id)
+let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`    
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
-if (isBotAdmin && chat.antifake) {
-let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
+let bot = global.db.data.settings[conn.user.jid] || {}
+if (isBotAdmin && chat.antifake && !isAdmin && !isOwner && !isROwner && bot.restrict) {
+
+if (m.sender.startsWith('6' || '6')) {
+await conn.reply(m.chat, texto, m)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
   
-for (const participant of participants) {
-if (/^(2|33|44|51)/.test(participant)) {
-await conn.reply(m.chat, texto, fkontak)
-setTimeout(async () => {
-await conn.groupParticipantsUpdate(m.chat, [participant], 'remove')
-}, 2000) 
 }}
+export default handler*/
+
+let handler = m => m
+handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, groupMetadata }) {
+  
+let chat = global.db.data.chats[m.chat]
+let bot = global.db.data.settings[conn.user.jid] || {}
+let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
+
+if (isBotAdmin && chat.antifake && !isAdmin && !isOwner && !isROwner && bot.restrict) { 
+while (chat.antifake && bot.restrict) {
+  
+let participantIds = groupMetadata.participants.map(p => p.id)
+for (let participantId of participantIds) {
+if (participantId.startsWith('51' || '51')) {
+await conn.reply(m.chat, texto, m)
+await conn.groupParticipantsUpdate(m.chat, [participantId], 'remove')
+  
+}}
+await new Promise(resolve => setTimeout(resolve, 2000))
   
 }
-}
+}}
 export default handler
+
