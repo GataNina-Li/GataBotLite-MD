7e67dbe67b993ffafae463cae42bc0fbc379efc0
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-26 19:03:47 -0300

    Update propietario(a)-restart.js

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, isROwner, text }) => {
 if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
 //conn.readMessages([m.key])   
-  conn.reply(lenguajeGB.smsreiniciar()) 
+await conn.reply(m.chat, lenguajeGB.smsreiniciar(), m)
 process.send('reset')
-} else throw 'eh'
+} else throw 'eh' 
 }
 handler.help = ['restart']
 handler.tags = ['owner']
