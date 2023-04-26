597afcc2779539196d35bff9ad961c0174c23c35
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-26 10:01:46 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,39 +1,37 @@
-/*let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, groupMetadata } ) {
-let   groupMetadata.participants.map(v => v.id)
-let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`    
+let handler = m => m
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[conn.user.jid] || {}
+let toUser = `${m.sender.split("@")[0]}`
+let aa = toUser + '@s.whatsapp.net'
+
 if (isBotAdmin && chat.antifake && !isAdmin && !isOwner && !isROwner && bot.restrict) {
+let texto = `*${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} @${toUser} ${lenguajeGB['smsInt2']()}*`
+	
+if (m.sender.startsWith('1' || '1')) {
+await conn.reply(m.chat, texto, null, { mentions: [aa] })
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
 if (m.sender.startsWith('6' || '6')) {
-await conn.reply(m.chat, texto, m)
+await conn.reply(m.chat, texto, null, { mentions: [aa] })
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-  
-}}
-export default handler*/
+		
+if (m.sender.startsWith('9' || '9')) {
+await conn.reply(m.chat, texto, null, { mentions: [aa] })
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
-/*let handler = m => m
-handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, groupMetadata }) {
-  
-let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[conn.user.jid] || {}
-let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
+if (m.sender.startsWith('7' || '7')) {
+await conn.reply(m.chat, texto, null, { mentions: [aa] })
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+	
+if (m.sender.startsWith('4' || '4')) {
+await conn.reply(m.chat, texto, null, { mentions: [aa] })
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+	
+if (m.sender.startsWith('2' || '2')) {
+await conn.reply(m.chat, texto, null, { mentions: [aa] })
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
-if (isBotAdmin && chat.antifake && !isAdmin && !isOwner && !isROwner && bot.restrict) { 
-while (chat.antifake && bot.restrict) {
-  
-let participantIds = groupMetadata.participants.map(p => p.id)
-for (let participantId of participantIds) {
-if (participantId.startsWith('51' || '51')) {
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [participantId], 'remove')
-  
 }}
-await new Promise(resolve => setTimeout(resolve, 2000))
-  
-}
-}}
-export default handler*/
-
+export default handler
