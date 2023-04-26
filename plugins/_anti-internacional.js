c5d80070bb125c3793682f7acf2c705d66f098bf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-25 22:24:37 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,19 +1,22 @@
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, participants} ) {
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.antifake) {
 let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
-		
-if (m.sender.startsWith('2' || '2')) {
-await conn.reply(m.chat, texto, fkontak,  m)
-let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (responseb[0].status === "404") return 
-}
+
+for (const participant of chat.participants) {
+if (/^(2|33|44|89|51)/.test(participant)) {
+await conn.reply(chat.jid, texto, fkontak)
+setTimeout(async () => {
+let responseb = await conn.groupParticipantsUpdate(chat.jid, [participant], 'remove')
+if (responseb[0].status === "404") return
+}, 2000) 
+}}
    
-if (m.sender.startsWith('265' || '265')) {
+/*if (m.sender.startsWith('265' || '265')) {
 await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return   
@@ -84,6 +87,7 @@ await conn.reply(m.chat, texto, fkontak,  m)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return   
 } 	
-   
-}}
+*/   
+}
+}
 export default handler
