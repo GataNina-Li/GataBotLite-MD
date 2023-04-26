b9b963eb7e2bf62b3062aa46693b75bee9cf3081
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-25 23:06:59 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -1,4 +1,4 @@
-/*let handler = m => m
+let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner } ) {
 //const participants = groupMetadata.participants.map(v => v.id)
 const participants = await conn.groupMetadata(m.chat).participants.map((user) => user.jid);
@@ -15,66 +15,20 @@ conn.on('group-participants-update', async (event) => {
   if (action === 'add' && participants) {
     for (const participant of participants) {
       if (/^(2|33|44|51)/.test(participant)) {
-        await conn.groupParticipantsUpdate(jid, [participant], 'remove');
+        await conn.groupParticipantsUpdate(m.chat, [participant], 'remove');
       }
     }
   }
 })
-
-//for (const participant of participants) {
-//if (/^(2|33|44|51)/.test(participant)) {
-//await conn.reply(m.chat, texto, fkontak)
-//setTimeout(async () => {
-//await conn.groupParticipantsUpdate(m.chat, [participant], 'remove')
-//}, 2000) 
-//}}
+  
+/*for (const participant of participants) {
+if (/^(2|33|44|51)/.test(participant)) {
+await conn.reply(m.chat, texto, fkontak)
+setTimeout(async () => {
+await conn.groupParticipantsUpdate(m.chat, [participant], 'remove')
+}, 2000) 
+}}*/
   
 }
 }
-export default handler*/
-
-import { WAConnection, GroupSettingChange, MessageType } from '@adiwajshing/baileys';
-import { WA_DEFAULT_EPHEMERAL, waChatKey, mentionedJid } from '@adiwajshing/baileys/build/WAConnection/Constants';
-import fs from 'fs';
-import { promisify } from 'util';
-const writeFileAsync = promisify(fs.writeFile);
-const readFileAsync = promisify(fs.readFile);
-
-let handler = m => m;
-
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner } ) {
-  const participants = await conn.groupMetadata(m.chat).participants.map((user) => user.jid);
-  const fkontak = { 
-    "key": { 
-      "participants": "0@s.whatsapp.net", 
-      "remoteJid": "status@broadcast", 
-      "fromMe": false, 
-      "id": "Halo" 
-    }, 
-    "message": { 
-      "contactMessage": { 
-        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
-      }
-    }, 
-    "participant": "0@s.whatsapp.net"
-  }
-
-  if (!m.isGroup) return !1
-  let chat = global.db.data.chats[m.chat]
-  if (isBotAdmin && chat.antifake) {
-    let texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
-    conn.on('group-participants-update', async (event) => {
-      const { jid, action, participants } = event;
-      if (action === 'add' && participants) {
-        for (const participant of participants) {
-          if (/^(2|33|44|51)/.test(participant)) {
-            await conn.groupParticipantsUpdate(jid, [participant], 'remove');
-          }
-        }
-      }
-    });
-  }
-}
-
-export default handler;
-
+export default handler
