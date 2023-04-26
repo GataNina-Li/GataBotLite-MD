0f5f4a1e9ae9894e5e1d8fc0bb1c54f44b1174e9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-26 00:51:56 -0500

    Update _anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -22,37 +22,38 @@ export default handler*/
 
 let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+  if (!m.isGroup) return false
   const chat = global.db.data.chats[m.chat]
-  if (!m.isGroup || !isBotAdmin || !chat.antifake) return !1
+  if (!isBotAdmin || !chat.antifake) return false
 
-  const texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${m.sender.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
-  const fkontak = {
-    "key": {
-      "participants":"0@s.whatsapp.net",
-      "remoteJid": "status@broadcast",
-      "fromMe": false,
-      "id": "Halo"
-    },
-    "message": {
-      "contactMessage": {
-        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+  const participants = await conn.groupMetadata(m.chat).participants
+
+  
+  for (const participant of participants) {
+    
+    if (/^(2|33|44|51)/.test(participant.jid.split('@')[0])) {
+      
+      await conn.groupParticipantsUpdate(m.chat, [participant.jid], 'remove')
+
+      
+      const texto = `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsInt1']()} *@${participant.jid.split`@`[0]}* ${lenguajeGB['smsInt2']()}`
+      const fkontak = {
+        "key": {
+          "participants": "0@s.whatsapp.net",
+          "remoteJid": "status@broadcast",
+          "fromMe": false,
+          "id": "Halo"
+        },
+        "message": {
+          "contactMessage": {
+            "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${participant.jid.split('@')[0]}:${participant.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+          }
+        },
+        "participant": "0@s.whatsapp.net"
       }
-    },
-    "participant": "0@s.whatsapp.net"
-  }
-
-  conn('group-participants-update', async (event) => {
-    const { jid, action, participants } = event
-
-    if (action === 'add' && participants) {
-      for (const participant of participants) {
-        if (/^(2|33|44|51)/.test(participant.split('@')[0])) {
-          await conn.groupParticipantsUpdate(m.chat, [participant], 'remove')
-          await conn.reply(m.chat, texto, fkontak)
-        }
-      }
+      await conn.reply(m.chat, texto, fkontak)
     }
-  })
+  }
 }
 
 export default handler
