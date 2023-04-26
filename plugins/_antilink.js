abe6b420bf634cca5345b252f4ea1dffca4148b4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-04-25 20:03:20 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,4 +1,4 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+/*let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 export async function before(m, { isAdmin, isBotAdmin }) {
 if (m.isBaileys && m.fromMe)
 return !0
@@ -23,4 +23,4 @@ await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 } else if (!bot.restrict) return m.reply(`${lenguajeGB['smsSoloOwner']()}`)
 }
 return !0
-}
+}*/
