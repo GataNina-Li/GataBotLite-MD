9a5818d1a2f038613c31764842888d8b60789117
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-09 12:56:46 -0500

    Update descargas.js

--- plugins/descargas.js
+++ plugins/descargas.js
@@ -227,42 +227,40 @@ case isCommand9:
 if (!args[0]) throw lenguajeGB.smsMalused2() + `*${usedPrefix + command} https://youtu.be/ejemplo*\n*${usedPrefix + command} https://www.youtube.com/ejemplo*`
 await conn.reply(m.chat, lenguajeGB.smsAvisoEG() + '*' + lenguajeGB.smsYTV1() + '*', m)
 try {
-qu = args[1] || ''
-q = qu + 'p'
-v = args[0]
-yt = await youtubedl(v)
-.catch(async (_) => await youtubedlv2(v))
-.catch(async (_) => await youtubedlv3(v))
-if (yt.video['2160p']) {
-dl_url = await yt.video['2160p'].download().catch(async (_) => {
-if (yt.video['1440p']) {
-dl_url = await yt.video['1440p'].download().catch(async (_) => {
-if (yt.video['1080p']) {
-dl_url = await yt.video['1080p'].download().catch(async (_) => {
-if (yt.video['720p']) {
-dl_url = await yt.video['720p'].download().catch(async (_) => {
-if (yt.video['480p']) {
-dl_url = await yt.video['480p'].download().catch(async (_) => {
-if (yt.video['360p']) {
-dl_url = await yt.video['360p'].download()
-size = await yt.video['360p'].fileSizeH
-}
-})
-size = await yt.video['480p'].fileSizeH
-}
-});
-size = await yt.video['720p'].fileSizeH
-}
-});
-size = await yt.video['1080p'].fileSizeH
-}
-});
-size = await yt.video['1440p'].fileSizeH
-}
-});
-size = await yt.video['2160p'].fileSizeH
-}
-ttl = await yt.title
+  qu = args[1] || '';
+  q = qu + 'p';
+  v = args[0];
+  yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v)).catch(async (_) => await youtubedlv3(v));
+  if (yt.video['2160p']) {
+    dl_url = await yt.video['2160p'].download().catch(async (_) => {
+      if (yt.video['1440p']) {
+        dl_url = await yt.video['1440p'].download().catch(async (_) => {
+          if (yt.video['1080p']) {
+            dl_url = await yt.video['1080p'].download().catch(async (_) => {
+              if (yt.video['720p']) {
+                dl_url = await yt.video['720p'].download().catch(async (_) => {
+                  if (yt.video['480p']) {
+                    dl_url = await yt.video['480p'].download().catch(async (_) => {
+                      if (yt.video['360p']) {
+                        dl_url = await yt.video['360p'].download();
+                        size = await yt.video['360p'].fileSizeH;
+                      }
+                    });
+                    size = await yt.video['480p'].fileSizeH;
+                  }
+                });
+                size = await yt.video['720p'].fileSizeH;
+              }
+            });
+            size = await yt.video['1080p'].fileSizeH;
+          }
+        });
+        size = await yt.video['1440p'].fileSizeH;
+      }
+    });
+    size = await yt.video['2160p'].fileSizeH;
+  }
+  ttl = await yt.title;
 await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*💫 ${ttl}*\n*⚖️ ${size}*`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
 //await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: 'Prueba', thumbnail: await fetch(yt.thumbnail)}, { quoted: m })
 /*qu = args[1] || '720'
