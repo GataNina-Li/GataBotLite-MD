40ff17f396b9dca6147ac2bfff24478480732958
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-13 04:41:41 -0500

    Update prueba_grupo_online.js

--- plugins/prueba_grupo_online.js
+++ plugins/prueba_grupo_online.js
@@ -1,12 +1,12 @@
-let handler = async (m, { conn, args, isAdmin, isOwner }) => {
+let handler = async (m, { conn, args, isAdmin, isOwner, participants }) => {
 /*if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 dfail('admin', m, conn)
 throw false
 }
 }*/
-let id = args && /.*@g.us/.test(args[0]) ? args[0] : m.chat
-let online = [...Object.keys(conn.chats.get(id).presences), conn.user.jid]
+//let id = args && /.*@g.us/.test(args[0]) ? args[0] : m.chat
+let online = [...Object.keys(conn.chats(participants).presences), conn.user.jid]
 await conn.reply(m.chat, '*USUARIOS EN LÍNEA* \n' + online.map(v => '- @' + v.replace(/@.+/, '')).join`\n` + '------------', m, { contextInfo: { mentionedJid: online }})
 }
 handler.command = /^((list)?online)$/i
