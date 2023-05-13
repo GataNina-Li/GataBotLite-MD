61ab96167cd22cbbda7cdde5173c497fd1cab1e3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-13 04:52:47 -0500

    Update prueba_grupo_online.js

--- plugins/prueba_grupo_online.js
+++ plugins/prueba_grupo_online.js
@@ -1,14 +1,23 @@
-let handler = async (m, { conn, args, isAdmin, isOwner, participants }) => {
+let handler = async (m, { conn, args, isAdmin, isOwner, participants, text }) => {
 /*if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 dfail('admin', m, conn)
 throw false
 }
 }*/
-//let id = args && /.*@g.us/.test(args[0]) ? args[0] : m.chat
-let online = participants.filter((participant) => participant.jid !== conn.user.jid && participant.presence?.isOnline)
-let onlineJids = online.map((participant) => participant.jid)
-await conn.reply(m.chat, '*USUARIOS EN LÍNEA* \n' + onlineJids.map((jid) => '- @' + jid.replace(/@.+/, '')).join('\n') + '------------', m, { contextInfo: { mentionedJid: onlineJids } })
+//let online = participants.filter((participant) => participant.jid !== conn.user.jid && participant.presence?.isOnline)
+//let onlineJids = online.map((participant) => participant.jid)
+//await conn.reply(m.chat, '*USUARIOS EN LÍNEA* \n' + onlineJids.map((jid) => '- @' + jid.replace(/@.+/, '')).join('\n') + '------------', m, { contextInfo: { mentionedJid: onlineJids } })
+//}
+//let users = participants.map(u => u.presences.id).filter(v => v !== conn.user.jid)
+//m.reply(`*Teks:*\n${text ? `${text}\n` : ''}\nEN LINEA\n` + users.map(v => '│♪ @' + v.replace(/@.+/, '')).join`\n` + '\nEN LINEA', null, {
+//mentions: users })
+
+let onlineUsers = participants.filter(user => user.presence && user.presence.isOnline && user.jid !== conn.user.jid)
+let mentionList = onlineUsers.map(user => `@${user.jid.replace(/@.+/, '')}`).join('\n')
+
+let replyText = `Usuarios en línea:\n\n${mentionList}`
+await conn.reply(m.chat, replyText, m, { mentions: onlineUsers })
 }
 handler.command = /^((list)?online)$/i
 
