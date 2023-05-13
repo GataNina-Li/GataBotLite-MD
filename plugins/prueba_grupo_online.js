5c57ab6b396f998fd2530995d65c009a36b2f02e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-13 05:05:12 -0500

    Update prueba_grupo_online.js

--- plugins/prueba_grupo_online.js
+++ plugins/prueba_grupo_online.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, participants }) => {
+/*let handler = async (m, { conn, participants }) => {
   let onlineUsers = participants
     .filter(user => user.presence && user.presence.isOnline && user.jid !== conn.user.jid)
     .map(user => user.jid.replace(/@.+/, ''))
@@ -12,4 +12,42 @@ let handler = async (m, { conn, participants }) => {
 
 handler.command = /^(listonline)$/i
 
+export default handler*/
+
+let handler = async (m, { conn }) => {
+  if (!m.quoted) throw 'RESPONDA A UN MENSAJE'
+  if (!m.quoted.fromMe) throw false
+  if (!m.quoted.id) throw false
+  let members = m.quoted.chat.endsWith('g.us') ? (await conn.groupMetadata(m.quoted.chat)).participants.length - 1 : m.quoted.chat.endsWith('@broadcast') ? -1 : 1
+  let { reads, deliveries } = await conn.messageInfo(m.quoted.chat, m.quoted.id)
+  let txt = `
+*Leído por:*
+${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `@${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
+${members > 1 ? `${members - reads.length} restante` : ''}
+
+*Entregado a:*
+${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `wa.me/${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
+${members > 1 ? `${members - reads.length - deliveries.length} restante` : ''}
+`.trim()
+  m.reply(txt, null, {
+    contextInfo: {
+      mentionedJid: conn.parseMention(txt)
+    }
+  })
+}
+handler.command = /^((list)?online)$/i
+
 export default handler
+
+function formatDate(n, locale = 'es') {
+  let d = new Date(n)
+  return d.toLocaleDateString(locale, {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  })
+}
