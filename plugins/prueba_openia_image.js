b1ddbb2d6f6e2b0a961aa71f31db35b59e0070ae
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-14 13:28:16 -0500

    Update prueba_openia_image.js

--- plugins/prueba_openia_image.js
+++ plugins/prueba_openia_image.js
@@ -1,33 +1,38 @@
-import { Configuration, OpenAIApi } from 'openai'
-const configuration = new Configuration({ apiKey: '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' }) //apiKey = https://platform.openai.com/account/api-keys , organization = https://platform.openai.com/account/org-settings
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: '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'
+});
+
 const openai = new OpenAIApi(configuration);
 
 let handler = async (m, { conn, text, command }) => {
-    try {
-        if (!text) throw new Error(`Creación de una imagen a partir de IA.\n\nEjemplo:\n.img Gata\n\n\n\nCrear imagen a partir de IA\n\nExample:\nCasa de madera en la montaña nevada`);
-        
-        await m.reply(wait)
-        const response = await openai.createImage({
-            prompt: text,
-            n: 1,
-            size: "1024x1024",
-        });
-        
-        //conn.sendButtonImg(m.chat, , 'Done', wm, 'Menu', '.m', m)
-      await conn.sendFile(m.chat, response.data.data[0].url, 'error.jpg', wm, m)
-        
-    } catch (error) {
-        if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-            console.log(`${error.response.status}\n\n${error.response.data}`);
-        } else {
-            console.log(error);
-            m.reply(error.message);
-        }
+  try {
+    if (!text) throw new Error(`Creación de una imagen a partir de IA.\n\nEjemplo:\n.img Gata\n\nCrear imagen a partir de IA\n\nExample:\nCasa de madera en la montaña nevada`);
+
+    await conn.reply(m.chat, 'Espera un momento...', m);
+
+    const response = await openai.createImage({
+      prompt: text,
+      num_images: 1,
+      width: 1024,
+      height: 1024,
+    });
+
+    await conn.sendFile(m.chat, response.data.images[0].file, 'image.jpg', 'Aquí está la imagen generada:', m);
+
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+      console.log(`${error.response.status}\n\n${error.response.data}`);
+    } else {
+      console.log(error);
+      conn.reply(m.chat, error.message, m);
     }
-}
+  }
+};
 
-handler.command = /^(dalle|aiimg|aiimage|ai-img|openaiimage|ai-image|img|gambar)$/i
+handler.command = /^(dalle|aiimg|aiimage|ai-img|openaiimage|ai-image|img|gambar)$/i;
 
-export default handler
+export default handler;
