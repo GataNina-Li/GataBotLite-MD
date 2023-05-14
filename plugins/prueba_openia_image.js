4e6bbffd0b03fcbb718223594b491bbdab888852
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-14 13:49:08 -0500

    Update prueba_openia_image.js

--- plugins/prueba_openia_image.js
+++ plugins/prueba_openia_image.js
@@ -1,39 +1,12 @@
-import { Configuration, OpenAIApi } from 'openai';
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, command, args }) => {
+if (!args[0]) throw `Ejemplo: ${command} Gata`
+await m.reply('Searching...')
+let res = `https://api.lolhuman.xyz/api/dall-e?apikey=SGWN&text=${text}`
+await conn.sendFile(m.chat, res, 'image.jpg', 'Aquí está la imagen generada:', m)
+  
+}
+handler.command = /^(dalle|aiimg|aiimage|ai-img|openaiimage|ai-image|img|gambar)$/i
 
-const configuration = new Configuration({
-  apiKey: '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'
-});
-
-const openai = new OpenAIApi(configuration);
-
-const handler = async (m, { conn, text, command }) => {
-  try {
-    if (!text) {
-      throw new Error(`Ejemplo: ${command} Gata`);
-    }
-
-    await conn.reply(m.chat, 'Espera un momento...', m);
-
-    const response = await openai.createImage({
-      prompt: text,
-      numImages: 1,
-      width: 1024,
-      height: 1024,
-    });
-
-    await conn.sendFile(m.chat, response.data.images[0].file, 'image.jpg', 'Aquí está la imagen generada:', m);
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-      console.log(`${error.response.status}\n\n${error.response.data}`);
-    } else {
-      console.log(error);
-      conn.reply(m.chat, error.message, m);
-    }
-  }
-};
-
-handler.command = /^(dalle|aiimg|aiimage|ai-img|openaiimage|ai-image|img|gambar)$/i;
-
-export default handler;
+export default handler
+       
