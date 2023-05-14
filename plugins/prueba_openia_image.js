69a69c21d5f982245c8cbdf3c225a3d31c79f403
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-14 15:01:19 -0500

    Update prueba_openia_image.js

--- plugins/prueba_openia_image.js
+++ plugins/prueba_openia_image.js
@@ -1,15 +1,25 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
+async function reportError(e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+}
+  
 if (!args[0]) return conn.reply(m.chat, `${lenguajeGB['smsMalused2']()}\n*${usedPrefix + command} Un gato morado con celeste en Júpiter, iluminando el cosmos con su encanto de efecto de anime.*`, m)
-//let res = `https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`
 await m.reply(wait)
-//await conn.sendFile(m.chat, res, 'image.jpg', '💻 *IMAGEN CREADA CON AI!!* ✨', m)
+try{
 let response = await fetch(`https://botcahx.cyclic.app/dalle?text=${encodeURIComponent(text)}`)
 let image = await response.buffer()
-await conn.sendFile(m.chat, image, 'aneh.jpg',  wm, m)
-  
-}
-handler.command = /^(dalle|aiimg|aiimage|ai-img|openaiimage|ai-image|img|gambar)$/i
+await conn.sendFile(m.chat, image, 'image.jpg', '💻 *IMAGEN CREADA CON AI!!* ✨', m)
+} catch {
+try{
+let res = `https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`  
+await conn.sendFile(m.chat, res, 'image.jpg', '💻 *IMAGEN CREADA CON AI!!* ✨', m)
+} catch (e) {
+reportError(e)} 
+}}
+handler.command = /^(dalle|openiamage|aiimage|aiimg|aimage|iaimagen|openaimage|openaiimage)$/i
 
 export default handler
        
