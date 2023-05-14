66e74fb6537dba73157c9d58fe94d6add9a27b07
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-14 14:33:07 -0500

    Update prueba_openia_image.js

--- plugins/prueba_openia_image.js
+++ plugins/prueba_openia_image.js
@@ -1,9 +1,12 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
 if (!args[0]) return conn.reply(m.chat, `${lenguajeGB['smsMalused2']()}\n*${usedPrefix + command} Un gato morado con celeste en Júpiter, iluminando el cosmos con su encanto de efecto de anime.*`, m)
-let res = `https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`
+//let res = `https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`
 await m.reply(wait)
-await conn.sendFile(m.chat, res, 'image.jpg', '💻 *IMAGEN CREADA CON AI!!*', m)
+//await conn.sendFile(m.chat, res, 'image.jpg', '💻 *IMAGEN CREADA CON AI!!* ✨', m)
+let response = await fetch(`https://botcahx.cyclic.app/dalle?text=${encodeURIComponent(text)}`)
+let image = await response.buffer()
+await conn.sendFile(m.chat, image, 'aneh.jpg',  wm, m)
   
 }
 handler.command = /^(dalle|aiimg|aiimage|ai-img|openaiimage|ai-image|img|gambar)$/i
