36c9d9eed61c538c333bca82accf962249fc0a0e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-14 00:07:07 -0500

    Update descargas.js

--- plugins/descargas.js
+++ plugins/descargas.js
@@ -259,8 +259,8 @@ const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + 
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}*${lenguajeGB['smsTiktok']()}*`, m)    
 await conn.sendFile(m.chat, url, 'tiktok.mp4', `
 💜 *${nickname}*`.trim(), m)
-await conn.sendMessage(m.chat, { audio: { url: url } }, m)
-    
+//await conn.sendMessage(m.chat, { audio: { url: url } }, m)
+await conn.sendMessage(m.chat, { audio: { url: url }, fileName: 'tiktok.mp3', mimetype: 'audio/mp4', ptt: false }, { quoted: m })    
 //await conn.sendMessage(m.chat, { audio: { url: url }, fileName: 'tiktok.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })
 //await conn.sendFile(m.chat, url, 'tiktok.mp3', '', m, true, { mimetype: 'audio/mp4' })
 //await conn.sendFile(m.chat, url, nickname + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
