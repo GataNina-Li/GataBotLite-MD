dc3a7931091730b1fbc0e789c7a44152d366d1a0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-17 17:05:20 -0500

    Update prueba_invitacion.js

--- plugins/prueba_invitacion.js
+++ plugins/prueba_invitacion.js
@@ -23,7 +23,7 @@ let user = m.sender.split`@`[0]
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat)
  
 await conn.reply(NumeroUser + '@s.whatsapp.net', `¡Hola! @${NumeroUser} Soy ${packname}, un Bot para WhatsApp. Parece que @${user} intentó agregarte, pero no pude hacerlo. No te preocupes, ¡aquí tienes una invitación para unirte! ¡Te esperamos con ansias en el grupo!\n\n${link}`, m, {mentions: [NumeroUser + '@s.whatsapp.net', m.sender]})
-await conn.reply(m.chat, `*INVITACIÓN ENVIADA AL PRIVADO DE @${NumeroUser}*`, m, {mentions: [m.sender]})
+await conn.reply(m.chat, `*INVITACIÓN ENVIADA AL PRIVADO DE @${NumeroUser}*`, m, {mentions: [NumeroUser + '@s.whatsapp.net', m.sender]})
 }
 handler.command = /^(add|agregar|invitar|invite|añadir|\+)$/i
 handler.group = true
