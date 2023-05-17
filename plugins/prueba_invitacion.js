791b5dd27babf6d3d8920086ec1093438a927031
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-17 18:47:01 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/prueba_invitacion.js
+++ plugins/prueba_invitacion.js
@@ -1,11 +1,10 @@
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+let handler = async (m, { conn, args, text, usedPrefix, command, groupMetadata }) => {
 let who 
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
 let name = await conn.getName(m.sender)	
 if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`, fkontak, m)
 
-//if (/^[a-zA-Z]+$/.test(text)) return conn.reply(m.chat, "El texto solo contiene letras. Por favor, proporciona un número válido.", m)
 if (args.length >= 1) {
 if (/[a-zA-Z]/.test(text)) return conn.reply(m.chat, "El texto solo contiene letras. Por favor, proporciona un número válido.", m)
 text = args.slice(0).join(" ")
@@ -18,7 +17,7 @@ let NumeroUser = text.replace(/\D/g, '')
 let user = m.sender.split`@`[0]
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat)
  
-await conn.reply(NumeroUser + '@s.whatsapp.net', `\`\`\`¡Hola! @${NumeroUser} Soy ${packname}, un Bot para WhatsApp. Parece que @${user} intentó agregarte, pero no pude hacerlo. No te preocupes, ¡aquí tienes una invitación para unirte! ¡Te esperamos con ansias en el grupo!\`\`\`\n\n*${link}*`, m, {mentions: [NumeroUser + '@s.whatsapp.net', m.sender]})
+await conn.reply(NumeroUser + '@s.whatsapp.net', `\`\`\`¡Hola! @${NumeroUser} Soy ${packname}, un Bot para WhatsApp. Parece que @${user} intentó agregarte al Grupo ${groupMetadata.subject} pero no pude hacerlo. No te preocupes, ¡aquí tienes una invitación para unirte!\n\n¡Te esperamos con ansias en el grupo!\`\`\`\n\n*${link}*`, m, {mentions: [NumeroUser + '@s.whatsapp.net', m.sender]})
 await conn.reply(m.chat, `*INVITACIÓN ENVIADA AL PRIVADO DE @${NumeroUser}*`, m, {mentions: [NumeroUser + '@s.whatsapp.net', m.sender]})
 }
 handler.command = /^(add|agregar|invitar|invite|añadir|\+)$/i
