92b427cb0ad3991b763bdf1ac56e3a8e50985c76
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-17 12:02:57 -0500

    Update prueba_fb.js

--- plugins/prueba_fb.js
+++ plugins/prueba_fb.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import fbDownloader from 'fb-downloader-scrapper'
+import { facebook } from "@xct007/frieren-scraper"
 var handler = async (m, { conn, command, text, args, usedPrefix }) => {
 if (!text) throw `${usedPrefix}${command} https://www.facebook.com/100010929794713/posts/1885825845125057/`
 //let res = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=${lolkeysapi}&url=${args[0]}`)
@@ -7,10 +7,11 @@ if (!text) throw `${usedPrefix}${command} https://www.facebook.com/1000109297947
 //let vid = _json.result[0]
 //if (vid == '' || !vid || vid == null) vid = _json.result[1]
 //conn.sendFile(m.chat, vid, 'error.mp4', wm, m)    
-let res = await fbDownloader(args[0])
-for (let result of res.download) {
-let ur = result.url    
-await conn.sendFile(m.chat, ur, 'error.mp4', packname, m)}
+const d2ata = await facebook.v1(args[0])
+let r2es = ''
+if (d2ata.urls && d2ata.urls.length > 0) {
+r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`}
+conn.sendFile(m.chat, r2es, 'error.mp4', packname, m)
 }
 
 handler.command = /^(facebook5)$/i
