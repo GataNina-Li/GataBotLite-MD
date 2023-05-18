e2c66ec0e38d05f66dc744717c9cbdb4dc6f88d0
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-18 05:55:13 -0300

    Add files via upload

--- plugins/a-prueba.js
+++ plugins/a-prueba.js
@@ -5,21 +5,21 @@ export async function before(m, { conn }) {
 	let edtr = `@${m.sender.split`@`[0]}`
 	let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 	if (m.messageStubType == 21) {
-		await this.sendMessage(m.chat, { text: `${edtr} cambiar el grupo sujeto a :\n*${m.messageStubParameters[0]}*`, mentions: [m.sender] }, { quoted: fkontak })
+		await this.sendMessage(m.chat, { text: `${edtr} cambio el nombre del grupo a:\n*${m.messageStubParameters[0]}*`, mentions: [m.sender] }, { quoted: fkontak })
 	} else if (m.messageStubType == 22) {
 		await this.sendMessage(m.chat, { text: `${edtr} has cambiado la foto del Grupo`, mentions: [m.sender] }, { quoted: fkontak }) 
 	} else if (m.messageStubType == 1 || m.messageStubType == 23 || m.messageStubType == 132) {
 		await this.sendMessage(m.chat, { text: `${edtr} has cambiado el link del Grupo!\n\n`, mentions: [m.sender] }, { quoted: fkontak })
 	} else if (m.messageStubType == 24) {
-		await this.sendMessage(m.chat, { text: `${edtr} cambio la descripción del Grupo.\n\n${m.messageStubParameters[0]}`, mentions: [m.sender] }, { quoted: fkontak })
+		await this.sendMessage(m.chat, { text: `${edtr} cambio la descripción del Grupo a:\n\n${m.messageStubParameters[0]}`, mentions: [m.sender] }, { quoted: fkontak })
 	} else if (m.messageStubType == 25) {
-		await this.sendMessage(m.chat, { text: `${edtr} ha arreglado a *${m.messageStubParameters[0] == 'on' ? 'hanya admin' : 'semua peserta'}* Quién puede editar información grupal.`, mentions: [m.sender] }, { quoted: fkontak })
+		await this.sendMessage(m.chat, { text: `${edtr} ahora  *${m.messageStubParameters[0] == 'on' ? 'solo admin' : 'cualquier persona'}* puede edita la información del Grupo.`, mentions: [m.sender] }, { quoted: fkontak })
 	} else if (m.messageStubType == 26) {
-		await this.sendMessage(m.chat, { text: `${edtr} cambio *${m.messageStubParameters[0] == 'on' ? 'menutup' : 'membuka'}* grupo!\nhecho ${m.messageStubParameters[0] == 'on' ? 'hanya admin yang' : 'semua peserta'} puede enviar mensajes.`, mentions: [m.sender] }, { quoted: fkontak })
+		await this.sendMessage(m.chat, { text: `${edtr} ahora el grupo esta *${m.messageStubParameters[0] == 'on' ? 'cerrado' : 'abierto'}*\n ${m.messageStubParameters[0] == 'on' ? 'solo los admin' : 'todos'} puede enviar mensajes.`, mentions: [m.sender] }, { quoted: fkontak })
 	} else if (m.messageStubType == 29) {
-		await this.sendMessage(m.chat, { text: `${edtr} te doy admin a @${m.messageStubParameters[0].split`@`[0]}.`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })
+		await this.sendMessage(m.chat, { text: `${edtr} Le doy admin a @${m.messageStubParameters[0].split`@`[0]}.`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })
 	} else if (m.messageStubType == 30) {
-		await this.sendMessage(m.chat, { text: `${edtr} has quitado de admin a @${m.messageStubParameters[0].split`@`[0]}.`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })
+		await this.sendMessage(m.chat, { text: `${edtr} le quito admin a @${m.messageStubParameters[0].split`@`[0]}.`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })
 	} else if (m.messageStubType == 72) {
 		await this.sendMessage(m.chat, { text: `${edtr} cambiar la duración del mensaje temporalmente a *@${m.messageStubParameters[0]}*`, mentions: [m.sender] }, { quoted: fkontak })
 	} else if (m.messageStubType == 123) {
@@ -32,5 +32,3 @@ export async function before(m, { conn }) {
 		});
 	}
 }
-
-//export const disabled = true
