1ac5c297a8415703e7a63ce722cb8e416d89af42
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-26 02:14:35 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -1,5 +1,4 @@
 let linkRegex = /https?:/i
-let linkRegex2 = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i 
 export async function before(m, { isAdmin, isBotAdmin, text }) {
 if (m.isBaileys && m.fromMe)
 return !0
@@ -10,14 +9,13 @@ let bang = m.key.id
 let toUser = `${m.sender.split("@")[0]}`
 let aa = toUser + '@s.whatsapp.net'
 let bot = global.db.data.settings[this.user.jid] || {}
-const isGroupLink = linkRegex.exec(m.text) || linkRegex2.exec(m.text)
+const isGroupLink = linkRegex.exec(m.text) 
 const grupo = `https://chat.whatsapp.com`
 if (isAdmin && chat.antiLink2 && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
 if (chat.antiLink2 && isGroupLink && !isAdmin) {  
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-const linkThisGroup2 = `${await this.groupInviteCode(m.chat)}`
-if (m.text.includes(linkThisGroup) || m.text.includes(linkThisGroup2)) return m.reply(lenguajeGB['smsWaMismoEnlace']())  
+if (m.text.includes(linkThisGroup)) return m.reply(lenguajeGB['smsWaMismoEnlace']())  
 }    
 if (!isBotAdmin) return m.reply(`${lenguajeGB['smsAvisoFG']()} ${lenguajeGB['smsAllAdmin']()}`)
 if (isBotAdmin && bot.restrict) {
@@ -29,18 +27,6 @@ if (responseb[0].status === "404") return
 }
 return !0
 }
-
-function extraerIDWhatsApp(texto) {
-var inicio = texto.indexOf("https://chat.whatsapp.com/") + "https://chat.whatsapp.com/".length
-var fin = texto.indexOf(" ", inicio);
-if (fin === -1) {
-fin = texto.length // Si no hay espacio después de la ID, tomar hasta el final del texto
-}
-var id = texto.substring(inicio, fin)
-return id
-}
-
-
 
 
 
