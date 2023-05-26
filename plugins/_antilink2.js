b525d2e2f2a3e6176533bccd911aa1ceef061a7b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-26 01:54:00 -0500

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -16,7 +16,8 @@ if (isAdmin && chat.antiLink2 && m.text.includes(grupo)) return m.reply(`${lengu
 if (chat.antiLink2 && isGroupLink && !isAdmin) {  
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-if (m.text.includes(linkThisGroup)) return m.reply(lenguajeGB['smsWaMismoEnlace']())  
+const linkThisGroup2 = `${await this.groupInviteCode(m.chat)}`
+if (m.text.includes(linkThisGroup) || m.text.includes(linkThisGroup2)) return m.reply(lenguajeGB['smsWaMismoEnlace']())  
 }    
 if (!isBotAdmin) return m.reply(`${lenguajeGB['smsAvisoFG']()} ${lenguajeGB['smsAllAdmin']()}`)
 if (isBotAdmin && bot.restrict) {
@@ -27,6 +28,16 @@ if (responseb[0].status === "404") return
 } else if (!bot.restrict) return m.reply(`${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`)
 }
 return !0
+}
+
+function extraerIDWhatsApp(texto) {
+var inicio = texto.indexOf("https://chat.whatsapp.com/") + "https://chat.whatsapp.com/".length
+var fin = texto.indexOf(" ", inicio);
+if (fin === -1) {
+fin = texto.length // Si no hay espacio después de la ID, tomar hasta el final del texto
+}
+var id = texto.substring(inicio, fin)
+return id
 }
 
 
