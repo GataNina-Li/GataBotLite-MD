6295cd1e178a3f94b9e05a4cb8c20da70a77d291
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-27 04:25:42 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -497,7 +497,7 @@ if (!text)
 text = (chat.sDemote || this.sdemote || conn.sdemote || '@user ```is no longer Admin```')
 text = text.replace('@user', '@' + participants[0].split('@')[0])
 if (chat.detect)
-//this.sendMessage(id, { text, mentions: this.parseMention(text) })
+this.sendMessage(id, { text, mentions: this.parseMention(text) })
 break
 }}
 
@@ -514,8 +514,8 @@ if (!id) continue
 let chats = global.db.data.chats[id], text = ''
 if (!chats?.detect) continue
 if (groupUpdate.desc) text = (chats.sDesc || this.sDesc || conn.sDesc || '```Description has been changed to```\n@desc').replace('@desc', groupUpdate.desc)
-//if (groupUpdate.subject) text = (chats.sSubject || this.sSubject || conn.sSubject || '```Subject has been changed to```\n@subject').replace('@subject', groupUpdate.subject)
-//if (groupUpdate.icon) text = (chats.sIcon || this.sIcon || conn.sIcon || '```Icon has been changed to```').replace('@icon', groupUpdate.icon)
+if (groupUpdate.subject) text = (chats.sSubject || this.sSubject || conn.sSubject || '```Subject has been changed to```\n@subject').replace('@subject', groupUpdate.subject)
+if (groupUpdate.icon) text = (chats.sIcon || this.sIcon || conn.sIcon || '```Icon has been changed to```').replace('@icon', groupUpdate.icon)
 if (groupUpdate.revoke) text = (chats.sRevoke || this.sRevoke || conn.sRevoke || '```Group link has been changed to```\n@revoke').replace('@revoke', groupUpdate.revoke)
 if (!text) continue
 await this.sendMessage(id, { text, mentions: this.parseMention(text) })
