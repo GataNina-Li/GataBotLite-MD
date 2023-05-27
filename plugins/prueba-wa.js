ba519b7184e5625e6d2de1fa29f3380098dcc5a8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-27 12:38:34 -0500

    Update prueba-wa.js

--- plugins/prueba-wa.js
+++ plugins/prueba-wa.js
@@ -6,7 +6,8 @@ let bang = m.key.id;
   
 if (isBotAdmin && m.isGroup) {
 if (m.text && m.text.toLowerCase().includes("wa.me/settings") || m.text.toLowerCase().includes("wa.me/setting")) {
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } })
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 return null
 }
 }}
