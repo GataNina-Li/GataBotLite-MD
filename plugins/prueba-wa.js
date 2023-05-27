0d59cf3beb730410cd3a53a4b358870cc6d470e7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-27 12:21:52 -0500

    Create prueba-wa.js

---
+++ plugins/prueba-wa.js
@@ -0,0 +1,13 @@
+let handler = m => m
+handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
+
+let delet = m.key.participant;
+let bang = m.key.id;
+  
+if (isBotAdmin && m.isGroup) {
+if (m.text && m.text.toLowerCase().includes("wa.me")) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+return null
+}
+}}
+export default handler;
