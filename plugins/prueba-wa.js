264e11436d88ea58a2be05e3980512d9478ce453
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-27 12:25:10 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/prueba-wa.js
+++ plugins/prueba-wa.js
@@ -5,7 +5,7 @@ let delet = m.key.participant;
 let bang = m.key.id;
   
 if (isBotAdmin && m.isGroup) {
-if (m.text && m.text.toLowerCase().includes("wa.me")) {
+if (m.text && m.text.toLowerCase().includes("wa.me/settings") || m.text.toLowerCase().includes("wa.me/setting")) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
 return null
 }
