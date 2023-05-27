009fbb23d7b80e668f90e6db7066f00cb3f0ed78
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-27 03:57:26 -0500

    Update descargas.js

--- plugins/descargas.js
+++ plugins/descargas.js
@@ -403,6 +403,7 @@ if (!text) return m.reply(lenguajeGB.smsMalused2() + `*${usedPrefix + command} B
 let res = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${lolkeysapi}&query=${encodeURIComponent(text)}`)
 let json = await res.json()
 if (!json) throw `${lenguajeGB['smsAvisoFG']()}*NO SE ENCONTRÓ RESULTADOS. INTENTE CON OTRO NOMBRE O ENLACE*`
+if (!json) return 0
 try{
 let get_result = json.result
 let ini_txt = `🔎 \`\`\`RESULTADO DE: ${text}\`\`\``
