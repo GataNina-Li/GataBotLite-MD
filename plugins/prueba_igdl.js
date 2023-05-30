d34b8d185c1104f48445f987a8e7f416956d9107
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 12:44:02 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/prueba_igdl.js
+++ plugins/prueba_igdl.js
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch'
-import fg from 'api-dylux'
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+import instagramGetUrl from 'instagram-url-direct'
 
 let handler = async (m, { conn, args, command, usedPrefix }) => {
 /*let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
@@ -10,9 +9,11 @@ let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${arg
 let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
 await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)*/
   
-let res = await fg.igdl(args[0])
-    for (let result of res.url_list) {
-    conn.sendFile(m.chat, result, 'igdl.mp4', `✅ Resultado`, m)
+const resultss = await instagramGetUrl(args[0]).url_list[0]    
+let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt2 = `🔗 *Url:* ${shortUrl2}`.trim()
+await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
+  
 }
 handler.command = /^(igdl)$/i
 export default handler
