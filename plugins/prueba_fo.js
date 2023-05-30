58df079e4175ae42e92231ab7cc3323404d46e04
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 16:35:07 -0500

    Update prueba_fo.js

--- plugins/prueba_fo.js
+++ plugins/prueba_fo.js
@@ -2,15 +2,22 @@ let handler = async (m, {conn, text}) => {
 let cometido, victima
 cometido = `@${m.sender.split('@')[0]}`
 victima = `${m.mentionedJid.map((user)=>(user === m.sender) ? text : `${user.split('@')[0]}`).join(', ')}`
-let coger = `🥵 Acaban de coger a ${text} !🥵 
-    
-${text} ¡te han cogido! 😏
 
-${cometido}
+if (text.length >= 1) {
+text = text.slice(0).join(" ")
+} else if (m.quoted && m.quoted.text) {
+victima = m.quoted.text
+}
+ 
+let coger = `🥵 *Acaban de coger a ${text}* 🥵 
+    
+*${text} ¡te han cogido!* 😏
+
+*${cometido}*
 ●
 █▄
 █ ▄█▀█● @${victima}
-¡te han cogido! 😫🍆`.trim()
+*¡Te han cogido!* 😫🍆`.trim()
 await conn.reply(m.chat, coger, m, { mentions: [m.sender, victima + '@s.whatsapp.net', text + '@s.whatsapp.net']})
 //m.reply(coger, null, {mentions: conn.parseMention(coger)});
 }
