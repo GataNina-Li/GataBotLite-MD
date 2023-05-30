2f56de565749579fef2eb7ad978e8bb79b0164a9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 12:10:44 -0500

    Update and rename prueba_ig_fotos.js to prueba_igdl.js

---
+++ plugins/prueba_igdl.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
+let json = await human.json()
+let videoig = json.result
+let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
+}
+handler.command = /^(igdl)$/i
+export default handler
