435d394ab1db5124606ab52b5d739a15c024a201
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 12:18:02 -0500

    Update prueba_igdl.js

--- plugins/prueba_igdl.js
+++ plugins/prueba_igdl.js
@@ -1,12 +1,18 @@
 import fetch from 'node-fetch'
+import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
+/*let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
 let json = await human.json()
 let videoig = json.result
 let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
 let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
-await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)*/
+  
+const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
+let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt3 = `🔗 *Url:* ${shortUrl3}`.trim()
+for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt3, m)
 }
 handler.command = /^(igdl)$/i
 export default handler
