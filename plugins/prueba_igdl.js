9914bebf67d26d419a2947e9edab181fc2cd6d57
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 12:20:23 -0500

    Update prueba_igdl.js

--- plugins/prueba_igdl.js
+++ plugins/prueba_igdl.js
@@ -10,7 +10,8 @@ let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
 await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)*/
   
 const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
-let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+//let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
 let txt3 = `🔗 *Url:* ${shortUrl3}`.trim()
 for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt3, m)
 }
