c3c5f4ab75a20eb8ff9ea77e43a0f5c76f4bb65d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 16:21:20 -0500

    Update prueba_fo.js

--- plugins/prueba_fo.js
+++ plugins/prueba_fo.js
@@ -1,14 +1,14 @@
 let handler = async (m, {conn, text}) => {
-let coger = `🥵 Acaban de coger a ${text}!🥵 
+let coger = `🥵 Acaban de coger a @${text}!🥵 
     
-${text} ¡te han cogido! 😏
+@${text} ¡te han cogido! 😏
 
-${m.sender.split('@')[0]}
+@${m.sender.split('@')[0]}
 ●
 █▄
-█ ▄█▀█● ${m.mentionedJid.map((user)=>(user === m.sender) ? text : `+${user.split('@')[0]}`).join(', ')}
+█ ▄█▀█● ${m.mentionedJid.map((user)=>(user === m.sender) ? text : `@${user.split('@')[0]}`).join(', ')}
 ¡te han cogido! 😫🍆`.trim()
-await conn.reply(m.chat, coger, m, { mentions: [m.sender, m.user]})
+await conn.reply(m.chat, coger, m, { mentions: [m.sender, m.user + '@s.whatsapp.net', text + '@s.whatsapp.net']})
 //m.reply(coger, null, {mentions: conn.parseMention(coger)});
 }
 
