0a1ca7e7067bdb6e2c9cd7c60501b1eb48bd7cb8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 13:34:15 -0500

    Update descargas.js

--- plugins/descargas.js
+++ plugins/descargas.js
@@ -430,8 +430,11 @@ let videoig = json3.result
 let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
 let txt1 = `🔗 *URL:* ${shortUrl1}`.trim()
 await m.reply(wait)
-await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)        
+try{
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)  
 } catch (e) {
+await conn.sendMessage(m.chat, { image: { url: videoig }, caption: txt1 }, { quoted: m })    
+}} catch (e) {
 reportError(e)} 
 break
 }}
