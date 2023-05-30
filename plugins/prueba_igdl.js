4097c4cd2677ccd2e7700e946f5116763266df67
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 12:31:59 -0500

    Update prueba_igdl.js

--- plugins/prueba_igdl.js
+++ plugins/prueba_igdl.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import fg from 'api-dylux'
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, command, usedPrefix }) => {
@@ -9,11 +10,13 @@ let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${arg
 let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
 await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)*/
   
-const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
-//let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
-let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt3 = `🔗 *Url:* ${shortUrl3}`.trim()
-for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt3, m)
+const resultssss = await instagramdl(args[0])
+          .catch(async (_) => await instagramdlv2(args[0]))
+          .catch(async (_) => await instagramdlv3(args[0]))
+          .catch(async (_) => await instagramdlv4(args[0]));
+        //let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text();
+        //let txt4 = `🔗 *Url:* ${shortUrl3}`.trim();
+        for (const {url} of resultssss) await conn.sendFile(m.chat, url, "error.mp4", 'rrrrr', m);
 }
 handler.command = /^(igdl)$/i
 export default handler
