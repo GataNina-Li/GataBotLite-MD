91c9f69013d46ce77cfabccd0a5702c9fd64851b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 17:37:18 -0500

    Update prueba_fo.js

--- plugins/prueba_fo.js
+++ plugins/prueba_fo.js
@@ -5,7 +5,7 @@ cometido = `@${m.sender.split('@')[0]}`
 if (text.length >= 1) {
 //text = text.slice(0).join(" ")  
 } else if (m.quoted && m.quoted.sender) {
-text = m.quoted.sender
+text = `@${m.quoted.sender.split('@')[0]}`
 } else if (m.quoted && m.quoted.fromMe) {
 text = `${text.split('@')[0]}` //`${m.mentionedJid.map((user)=>(user === m.sender) ? text : `${user.split('@')[0]}`).join(', ')}` //m.sender
 }
@@ -21,7 +21,7 @@ let coger = `🥵 *Acaban de coger a ${text}* 🥵
 █ ▄█▀█● *${text}*
 *¡Te han cogido!* 😫🍆`.trim()
 
-await conn.reply(m.chat, coger, m, { mentions: [m.sender, text, ...m.mentionedJid] })
+await conn.reply(m.chat, coger, m, { mentions: [m.sender, m.quoted.sender, text, ...m.mentionedJid] })
 }
 
 handler.command = /^(coger|follar|prueba34)$/i
