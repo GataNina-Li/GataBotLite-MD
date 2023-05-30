5a0ea8ced8123de2a555b8f138f71eadd6dbfe4c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 17:43:16 -0500

    Update prueba_fo.js

--- plugins/prueba_fo.js
+++ plugins/prueba_fo.js
@@ -1,27 +1,27 @@
 let handler = async (m, { conn, text }) => {
-let cometido
+let cometido, text2
 cometido = `@${m.sender.split('@')[0]}`
 
 if (text.length >= 1) {
 //text = text.slice(0).join(" ")  
 } else if (m.quoted && m.quoted.sender) {
-text = `@${m.quoted.sender.split('@')[0]}`
+text2 = `@${m.quoted.sender.split('@')[0]}`
 } else if (m.quoted && m.quoted.fromMe) {
-text = `${text.split('@')[0]}` //`${m.mentionedJid.map((user)=>(user === m.sender) ? text : `${user.split('@')[0]}`).join(', ')}` //m.sender
+text2 = `${text.split('@')[0]}` //`${m.mentionedJid.map((user)=>(user === m.sender) ? text : `${user.split('@')[0]}`).join(', ')}` //m.sender
 }
   
 
-let coger = `🥵 *Acaban de coger a ${text}* 🥵 
+let coger = `🥵 *Acaban de coger a ${text2}* 🥵 
 
-*${text} ¡te han cogido!* 😏
+*${text2} ¡te han cogido!* 😏
 
 *${cometido}*
 ●
 █▄
-█ ▄█▀█● *${text}*
+█ ▄█▀█● *${text2}*
 *¡Te han cogido!* 😫🍆`.trim()
 
-await conn.reply(m.chat, coger, m, { mentions: [m.sender, m.quoted.sender, text, ...m.mentionedJid] })
+await conn.reply(m.chat, coger, m, { mentions: [m.sender, m.quoted.sender, text, text2, ...m.mentionedJid] })
 }
 
 handler.command = /^(coger|follar|prueba34)$/i
