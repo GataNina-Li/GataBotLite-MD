5fc0df228c51f2a2b6551e91f05d1b5cf04fcd45
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 13:27:51 -0500

    Update prueba_ig_img.js

--- plugins/prueba_ig_img.js
+++ plugins/prueba_ig_img.js
@@ -2,7 +2,8 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, command, usedPrefix }) => {
 let loll = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
-conn.sendMessage(m.chat, { image: { url: loll.result }, caption: 'Hecho.' }, { quoted: m })
+//conn.sendMessage(m.chat, { image: { url: loll.result }, caption: 'Hecho.' }, { quoted: m })
+await conn.sendFile(m.chat, loll.result, 'error.jpg', wm, m)
   
 }
 handler.command = /^(igimg)$/i
