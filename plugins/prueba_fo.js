f50181072673adf03ce20582c943d3e1b063fb65
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-30 18:02:57 -0500

    Update prueba_fo.js

--- plugins/prueba_fo.js
+++ plugins/prueba_fo.js
@@ -7,7 +7,7 @@ if (text.length >= 1) {
 } else if (m.quoted && m.quoted.sender) {
 text = `@${m.quoted.sender.split('@')[0]}`
 } else if (m.quoted && m.quoted.fromMe) {
-text = text.split('@')[0] //`${m.mentionedJid.map((user)=>(user === m.sender) ? text : `${user.split('@')[0]}`).join(', ')}` //m.sender
+text = text.replace(/\D/g, '') //`${m.mentionedJid.map((user)=>(user === m.sender) ? text : `${user.split('@')[0]}`).join(', ')}` //m.sender
 }
   
 
