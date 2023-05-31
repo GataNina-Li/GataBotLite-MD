b59ffd176a9b117a3e929265a9f1992862cd282c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-05-31 09:33:20 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/_delete-waBug.js
+++ plugins/_delete-waBug.js
@@ -5,7 +5,7 @@ let delet = m.key.participant;
 let bang = m.key.id;
   
 if (isBotAdmin && m.isGroup) {
-if (m.text && m.text.toLowerCase().includes("wa.me/settings") || m.text.toLowerCase().includes("wa.me/channel")) {
+if (m.text && m.text.toLowerCase().includes("wa.me/setting") || m.text.toLowerCase().includes("wa.me/channel")) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } })
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 return null
