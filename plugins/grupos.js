540333a3e7e814230d31cdfe1d9088c8e21539db
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-06-03 23:03:33 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -161,7 +161,8 @@ link = link.replace(/\.gif$/, '.mp4')
 await m.reply(link)
 let accion1 = `*${cometido} ESTÁ 👋 SALUNDANDO A ${text}*`.trim()
 //await conn.sendFile(m.chat, gif, 'sticker.webp', accion1, m, false, { asSticker: true })
-await conn.sendFile(m.chat, gif, 'sticker.webp', accion1, m)   
+//await conn.sendFile(m.chat, gif, 'sticker.webp', accion1, m)   
+await conn.sendFile(m.chat, gif, 'error.gif', accion1, m)
 //await conn.sendMessage(m.chat, { video: { url: 'https://telegra.ph/file/086f9bf68e6fd49932a51.mp4' }, gifPlayback: true, caption: accion1, mentions: [m.sender, text.replace('@', '') + '@s.whatsapp.net'] }, { quoted: m }) 
 break
     
