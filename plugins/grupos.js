01dfabcfb5a951523e045992d0df55ce790340f5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-06-04 01:22:16 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -143,12 +143,12 @@ let cmd = command.toLowerCase()
 switch (true) {	
 case cmd == "saludar":
 let gif = 'https://pa1.narvii.com/6177/9d35b3265578df4e4092d67c9a7a5619cd1d41d0_hq.gif'
-let accion1 = `${conn.getName(m.sender)} ESTÁ 👋 SALUNDANDO A ${conn.getName(text.replace('@', '') + '@s.whatsapp.net')}
-⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯
+let accion1 = `${conn.getName(m.sender)} está 👋 saludando a ${conn.getName(text.replace('@', '') + '@s.whatsapp.net')}
+
 • ${conn.getName(m.sender)} ➜ ${PhoneNumber('+' + m.sender.replace('@s.whatsapp.net', '')).getNumber('international')}
 • ${conn.getName(text.replace('@', '') + '@s.whatsapp.net')} ➜ ${PhoneNumber('+' + text.replace('@', '') + '@s.whatsapp.net'.replace('@s.whatsapp.net', '')).getNumber('international')}
 
-${wm}`.trim()
+${wm} : @gata_dios`.trim()
 let stikerM = await sticker(null, gif, accion1)
 await conn.sendFile(m.chat, stikerM, 'sticker.webp', '', m, { mentions: [m.sender, text.replace('@', '') + '@s.whatsapp.net'] }) 
 break
