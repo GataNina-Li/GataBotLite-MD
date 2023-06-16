0e363e83e6471e50f8f02d92780a79a1ef7a7ae2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-06-16 18:37:39 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -2,10 +2,10 @@ import { createHash } from 'crypto'
 let handler = async function (m, { args, usedPrefix, command }) {
 let idreg = `${lenguajeGB.lenguaje() == 'es' ? 'idregistro' : 'idregister'}`
 let regbot = `${lenguajeGB.lenguaje() == 'es' ? 'verificar nombre.edad' : 'verify name.age'}`
-if (!args[0]) throw lenguajeGB.smsUnreg1(usedPrefix, idreg)
+if (!args[0]) return m.reply(lenguajeGB.smsUnreg1(usedPrefix, idreg))
 let user = global.db.data.users[m.sender]
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
-if (args[0] !== sn) throw lenguajeGB.smsUnreg2(usedPrefix, idreg)
+if (args[0] !== sn) return m.reply(lenguajeGB.smsUnreg2(usedPrefix, idreg))
 user.registered = false
 m.reply(lenguajeGB.smsUnreg3(usedPrefix, regbot))
 }
