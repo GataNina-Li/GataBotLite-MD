8c8901f54754046b0a7398d11114850890310de8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-06-16 21:01:36 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 (ML)

--- config.js
+++ config.js
@@ -8,43 +8,32 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js' 
 
-//⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰   
-
+// ES ➜ Agregué primero el número del Bot como prioridad  
+// ES ➜ Si desea recibir reportes debe de usar los tres parámetros (Número, nombre y true)
+// EN ➜ Add the Bot number as priority first
+// EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
 global.owner = [
 ['593993684821', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈', true], 
 ['5492266466080', '𝙊𝙬𝙣𝙚𝙧 (1) 💻', true],  
 ['972529277026', '𝙊𝙬𝙣𝙚𝙧 (2) 💻', true],  
 ['59894808483', '𝘾𝙪𝙨𝙩𝙤𝙢𝙚𝙧 𝙎𝙪𝙥𝙥𝙤𝙧𝙩 🥏', true],
 ['50664668406', 'My Kim Dan', true],
-['593968585383'], 
-['59894808483'],
-['5217441745001'],
-['5492266613038']]
-
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+['593968585383'], ['59894808483'], ['5217441745001'], ['5492266613038']]
 
 global.mods = [] 
 global.prems = []
    
-  
-/*⊱ ━━━━━.⋅ IDIOMA : LENGUAJE ⋅.━━━━ ⊰  
-Agrega el Idioma que quieres que tenga GataBotLite-MD
-Add the language you want GataBotLite-MD to have
-  es = Español         id = Bahasa Indonesia
-  en = English         pt = Português 
-  ar = عرب
-*/
-global.lenguajeGB = es  //<--- [es, en, ar, id, pt] Escribir el código de Idioma
- 
-//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+// ES ➜ Agregue el código de idioma el cual usará GataBot  
+// EN ➜ Add the language code which GataBot will use
+//  es = Español      id = Bahasa Indonesia       ar = عرب
+//  en = English      pt = Português 
+global.lenguajeGB = es  //<-- Predeterminado en idioma Español 
 
-
+// ES ➜ Consigue Apikey en https://platform.openai.com/account/api-keys
 global.openai_key = 'sk-0'
-/* Consigue tu ApiKey en este enlace: https://platform.openai.com/account/api-keys */
 
+// ES ➜ Consigue tu ID de organizacion en: https://platform.openai.com/account/org-settings
 global.openai_org_id = 'org-3'
-/* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
-
 
 global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
@@ -85,23 +74,18 @@ global.APIKeys = {
 }
 
 global.mods = [] 
-
-
-/*************************/
 global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
 global.moment = moment	
 
-//⊱ ━━━━━.⋅ Sticker WM ⋅.━━━━ ⊰
-
 global.packname = '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈'
 global.author = '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨'
 
-//⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
-
-global.official = [ // Agregate si eres Owner
+// ES ➜ Está parte es para mostrar el contacto de alguien al usar #contacto
+// EN ➜ This part is to display someone's contact using #contact
+global.official = [ 
 ['18059196237', 'Gata Dios 💻', 1], 
 ['5492266466080', '𝗗𝗲𝘀𝗮𝗿𝗿𝗼𝗹𝗹𝗮𝗱𝗼𝗿 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 💻', 1],  
 ['59894808483', '𝗖𝘂𝘀𝘁𝗼𝗺𝗲𝗿 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 🥏', 1],
@@ -135,21 +119,11 @@ global.nnnttt4 = 'https://chat.whatsapp.com/J8HOf2T3QYeKu3UN9UBkT6' //Grupo 8 CO
 global.paypal = 'https://paypal.me/OficialGD'
 global.asistencia = 'Wa.me/19393844141' //Dudas? escríbeme...
 
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
-
-
-//⊱ ━━━━━.⋅ Información | Information ⋅.━━━━ ⊰
-
 global.wm = '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈 : 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨'
 global.igfg = '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿'
 global.wait = lenguajeGB['smsMeWait']()
 global.wait2 = lenguajeGB.smsWait()
 global.nomorown = owner[0][0]
-
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
-
-
-//⊱ ━━━━━.⋅ IMG ⋅.━━━━ ⊰
 
 global.imagen1 = fs.readFileSync('./media/menus/Menu3.jpg')
 global.imagen2 = fs.readFileSync('./media/menus/img1.jpg')
@@ -165,48 +139,31 @@ global.imagen11 = fs.readFileSync('./media/menus/img10.jpg')
 global.imagen12 = fs.readFileSync('./media/menus/img11.jpg')
 global.imagen13 = fs.readFileSync('./media/menus/img12.jpg')
 
-//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
-
-
-//━━━━━━━━━━━━━━ img ━━━━━━━━━
-
 global.img = 'https://i.imgur.com/AwlL9kc.jpeg'
 global.img2 = 'https://i.imgur.com/p18q1Ok.jpeg'
-
 global.img3 = 'https://i.imgur.com/01Z8a0a.jpg' //ft rectangular
-
-//global.img3 = 'https://i.imgur.com/oUAGYc2.jpg' //prem   //no
-//global.img4 = 'https://i.imgur.com/i0pccuo.jpg' //prem   //no
-
 global.img5 = 'https://i.imgur.com/80uz37R.jpeg'
 global.img6 = 'https://i.imgur.com/3zSvnGY.jpeg'
 global.img7 = 'https://i.imgur.com/WY4r6up.jpeg'
 global.img8 = 'https://i.imgur.com/qCO3RYa.jpeg'
 global.img9 = 'https://i.imgur.com/dWk51FS.jpeg'
-
 global.img10 = 'https://i.imgur.com/T4NjKMi.jpeg'
 global.img11 = 'https://i.imgur.com/jqyWSlh.jpeg'
 global.img12 = 'https://i.imgur.com/mpCRttm.jpeg'
 global.img13 = 'https://i.imgur.com/O04epJI.jpeg'
 global.img14 = 'https://i.imgur.com/jfbuJRU.jpeg'
 global.img15 = 'https://i.imgur.com/DzqUXkW.jpeg'
-
-//global.img16 = 'https://i.imgur.com/11MRjo4.jpeg' //+18   //no
-
 global.img17 = 'https://i.imgur.com/Y3ZWq7z.jpeg'
 global.img18 = 'https://i.imgur.com/kaUN1Nz.jpeg'
 global.img19 = 'https://i.imgur.com/7yJ22hJ.jpeg'
 global.img20 = 'https://i.imgur.com/qcD353P.jpeg'
 global.img21 = 'https://i.imgur.com/3fJTaX6.jpeg'
-
 global.img21 = 'https://i.imgur.com/akofqcE.jpeg' //paypal
 
 global.welgata = [ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb]
 global.redesMenu = [nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt2, nnnttt3, nnnttt4, md, ig, paypal, yt, asistencia, fb]
 global.gataMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18, img19, img20, img21]
 global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13]
-
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
 global.htki = '*⭑•̩̩͙⊱•••• ☪*'
 global.htka = '*☪ ••••̩̩͙⊰•⭑*'
