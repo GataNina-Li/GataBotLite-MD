dffab524aab13933636fd5e6513183177b3a2405
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-07-05 16:59:19 -0300

    Add files via upload

--- plugins/buscadores.js
+++ plugins/buscadores.js
@@ -2,6 +2,8 @@ import { googleIt } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 import axios from 'axios'
 import yts from 'yt-search'
+import cheerio from 'cheerio'
+import gpt from 'api-dylux'
 import fs from 'fs'
 
 let handler = async (m, { conn, command, usedPrefix, args, text }) => {
@@ -41,9 +43,13 @@ text = args.slice(0).join(" ")
 text = m.quoted.text
 } else return conn.reply(m.chat, lenguajeGB.smsOpenai1() + `\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai2()}\n\n*${usedPrefix + command}* ${lenguajeGB.smsOpenai3()}` , m)
 try {
-let ia1 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`)  
+await conn.sendPresenceUpdate('composing', m.chat)
+        let syms = `Eres GataBotLite-MD, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
+        let res = await gpt.ChatGpt(text, syms)
+         await m.reply(res.text)
+/*let ia1 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`)  
 let resu1 = await ia1.json()
-m.reply(`${resu1.response}`.trim())    
+m.reply(`${resu1.response}`.trim())    */
 } catch {
 try {   
 let ia2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`) //fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
