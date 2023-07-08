73d73f377cf898a69646f418d9c6b7ff9ada7fef
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-08 04:50:30 -0500

    Update prueba_useridioma.js

--- plugins/prueba_useridioma.js
+++ plugins/prueba_useridioma.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, command, usedPrefix, args, text }) => {
-let userPri = global.db.data.users[m.sender] 
+let user = global.db.data.users[m.sender] 
 if (args[0] == 'es') {
-userPri.languageUser = 'es'
+user.languageUser = 'es'
 return m.reply('*Exito*')
 } else {
-userPri.languageUser = 'en'
+user.languageUser = 'en'
 return m.reply('*Error*')
 }
   
