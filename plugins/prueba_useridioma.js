4c1bcf91f562d809686573de3e7ecd84783b3dec
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-08 02:52:47 -0500

    Create prueba_useridioma.js

---
+++ plugins/prueba_useridioma.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn, command, usedPrefix, args, text }) => {
+let userPri = global.db.data.users[m.sender]
+let idioma = args[0]  
+userPri.languageUser = idioma
+if (userPri.languageUser == args[0]) {
+return m.reply('*Exito*')
+} else {
+return m.reply('*Error*')
+}
+  
+}
+handler.command = /^(prueba22)$/i
+export default handler
