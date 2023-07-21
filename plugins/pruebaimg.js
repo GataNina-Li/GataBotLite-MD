a8fb6c0bce42433c70c358b16bc08c5f6d131a1b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-21 02:22:38 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -72,8 +72,11 @@ const handler = async (m, { conn, text }) => {
   });
 
   
-  const imageWidth = 3000;
-  const imageHeight = Math.max(totalTextHeight + 100, 1200); 
+  //const imageWidth = 3000;
+  //const imageHeight = Math.max(totalTextHeight + 100, 1200); 
+
+  const imageWidth = 3000 + Math.floor(formattedText.length / 100) * 200
+  const imageHeight = Math.max(totalTextHeight + 100 + Math.floor(formattedText.length / 100) * 50, 1200)
 
   
   const image = await Jimp.create(imageWidth, imageHeight, 0xffffffff);
