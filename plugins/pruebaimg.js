b4a142e42dbd16fd3ca1423ad0b18b2c5a2bd610
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-21 02:00:16 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -11,28 +11,38 @@ await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m)
 handler.command = /^pruebaimg$/i
 export default handler*/
 
-import Jimp from 'jimp'
+import Jimp from 'jimp';
 
 const handler = async (m, { conn, text }) => {
-  const image = await Jimp.create(1200, 800, 0xffffffff)
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+  const image = await Jimp.create(1200, 800, 0xffffffff);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
 
   
-  const formattedText = text.replace(/\\n/g, '\n')
+  const formattedText = text.replace(/\\n/g, '\n');
 
-  image.print(
-    font,
-    0,
-    0,
-    {
-      text: formattedText,
-      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-      multiline: true, 
-    },
-    1200,
-    800
-  );
+  
+  const lines = formattedText.split('\n');
+
+  
+  let yPosition = 0;
+
+ 
+  lines.forEach((line) => {
+    image.print(
+      font,
+      0,
+      yPosition,
+      {
+        text: line,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      },
+      1200,
+      800
+    );
+
+   
+    yPosition += font.getHeight();
+  });
 
   const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
   await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m);
