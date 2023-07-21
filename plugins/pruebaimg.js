65cdfea1c8bf1f40d45324566830323cc6f099e5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-21 02:06:48 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -11,7 +11,7 @@ await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m)
 handler.command = /^pruebaimg$/i
 export default handler*/
 
-import Jimp from 'jimp';
+/*import Jimp from 'jimp';
 
 const handler = async (m, { conn, text }) => {
   const image = await Jimp.create(1200, 800, 0xffffffff);
@@ -52,4 +52,56 @@ const handler = async (m, { conn, text }) => {
 };
 
 handler.command = /^pruebaimg$/i;
+export default handler;*/
+
+import Jimp from 'jimp';
+
+const handler = async (m, { conn, text }) => {
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+
+  const formattedText = text.replace(/\\n/g, '\n');
+
+  const lines = formattedText.split('\n');
+
+  let totalTextHeight = 0;
+
+  
+  lines.forEach((line) => {
+    const textHeight = Jimp.measureTextHeight(font, line);
+    totalTextHeight += textHeight;
+  });
+
+  
+  const imageWidth = 1200;
+  const imageHeight = Math.max(totalTextHeight + 100, 800); 
+
+  
+  const image = await Jimp.create(imageWidth, imageHeight, 0xffffffff);
+
+  let yPosition = 0;
+
+  
+  lines.forEach((line) => {
+    image.print(
+      font,
+      0,
+      yPosition,
+      {
+        text: line,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      },
+      imageWidth,
+      imageHeight
+    );
+
+    const textHeight = Jimp.measureTextHeight(font, line);
+    yPosition += textHeight;
+  });
+
+  const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+  await conn.sendFile(m.chat, buffer, 'img.jpg', 'Mensaje', m);
+};
+
+handler.command = /^pruebaimg$/i;
 export default handler;
+
