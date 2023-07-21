a974180718ba5c9f0b7e8e9515d560d35c85962b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-21 04:41:56 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -285,7 +285,7 @@ const handler = async (m, { conn, text }) => {
   const baseWidth = 4000; // Ancho inicial del lienzo
   const baseHeight = 1000; // Alto inicial del lienzo
 
-  const imageWidth = baseWidth + Math.floor(300 / 50) * 45; // Ajustar el ancho del lienzo en función del límite de caracteres por línea
+  const imageWidth = baseWidth + Math.floor(300 / 50) * 30; // Ajustar el ancho del lienzo en función del límite de caracteres por línea
   const imageHeight = Math.max(baseHeight, totalTextHeight + 100 + Math.floor(300 / 50) * 4); // Ajustar el alto del lienzo en función del tamaño del texto
 
   // Crear la imagen con el tamaño ajustado
