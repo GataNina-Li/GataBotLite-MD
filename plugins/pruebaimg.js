f6a309475f91870c40f986840621f42e570256d8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-21 04:42:58 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -282,7 +282,7 @@ const handler = async (m, { conn, text }) => {
     });
   });
 
-  const baseWidth = 4000; // Ancho inicial del lienzo
+  const baseWidth = 3500; // Ancho inicial del lienzo
   const baseHeight = 1000; // Alto inicial del lienzo
 
   const imageWidth = baseWidth + Math.floor(300 / 50) * 30; // Ajustar el ancho del lienzo en función del límite de caracteres por línea
