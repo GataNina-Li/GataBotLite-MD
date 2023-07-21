8f43287246a305674eccec04d43505ad54d82604
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-07-21 04:40:22 -0500

    Update pruebaimg.js

--- plugins/pruebaimg.js
+++ plugins/pruebaimg.js
@@ -272,7 +272,7 @@ const handler = async (m, { conn, text }) => {
 
   lines.forEach((line) => {
     // Dividir la línea si supera el límite de 300 caracteres
-    const splitLines = splitLine(line, 200);
+    const splitLines = splitLine(line, 100);
     wrappedLines.push(...splitLines);
 
     // Calcular la altura total del texto
