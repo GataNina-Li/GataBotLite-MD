0c593af572c58dac106e75799cde2f735a1a6988
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-01 11:59:04 -0500

    Update prueba-wattpad.js

--- plugins/prueba-wattpad.js
+++ plugins/prueba-wattpad.js
@@ -6,8 +6,8 @@ if (!text) throw `*Falta texto*`
 let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&lc=es&apikey=uwgflzFEh6`)
 let anu = await res.json()
 anu = anu.data.map((v) => `Título: ${v.title}\n*Leídos:* ${v.reads}\n*Votos:* ${v.vote}\n*Capítulos:* ${v.chapter}\n*Enlace:* ${v.link}\n*Description:* ${v.desc}`).join`\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n`
-// let akuariapiresult2 = await translate(`${anu}`, { to: 'es', autoCorrect: true })
- await m.reply(anu) 
+let akuariapiresult2 = await translate(`${anu}`, { to: 'es', autoCorrect: true })
+await m.reply(akuariapiresult2) 
 //await conn.sendFile(m.chat, null, 'dorrat.jpg',  anu, m)
 }
 handler.command = /^(wattpad)$/i
