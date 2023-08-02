20a8578337e9ddb65d3d08c1f66e05a978000ca1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-02 11:52:22 -0500

    Update buscadores.js

--- plugins/buscadores.js
+++ plugins/buscadores.js
@@ -22,7 +22,7 @@ console.log(e)
 
 switch (true) {     
 case isCommand1:
-if (!text) return conn.reply(m.chat, lenguajeGB.smsAvisoMG() + lenguajeGB.smsMalused2() + `\n*${usedPrefix + command} Qué es Matemáticas?*`, m)
+/*if (!text) return conn.reply(m.chat, lenguajeGB.smsMalused2() + `\n*${usedPrefix + command} Qué es Matemáticas?*`, m)
 try {
 const fetch = (await import('node-fetch')).default
 let full = /f$/i.test(command);
@@ -36,7 +36,21 @@ return `*${title}*\n_${desc}_\n_${link}_`
 m.reply(msg)
 } catch (e) {
 reportError(e)
-}
+}*/
+const fetch = (await import('node-fetch')).default
+  const text = args.join` `;
+  if (!text) return conn.reply(m.chat, lenguajeGB.smsMalused2() + `\n*${usedPrefix + command} Qué es Matemáticas?*`, m)
+  const url = 'https://google.com/search?q=' + encodeURIComponent(text)
+  const search = await googleIt(text)
+  const msg = search.articles.map(({title, url, description}) => {
+    return `*${title}*\n_${url}_\n_${description}_`
+  }).join('\n\n');
+  try {
+    const ss = `https://image.thum.io/get/fullpage/${url}`
+    await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
+  } catch {
+    m.reply(msg);
+  }
 break
     
 case isCommand2:
