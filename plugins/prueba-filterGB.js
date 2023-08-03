c95ef703afaa5337ad8a8ec4fec714ed24bf4b58
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-03 12:10:20 -0500

    Update prueba-filterGB.js

--- plugins/prueba-filterGB.js
+++ plugins/prueba-filterGB.js
@@ -2,36 +2,45 @@ import translate from '@vitalets/google-translate-api'
 const prohibited = ['caca', 'polla', 'porno', 'porn', 'gore', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito', 'pussy']
 
 let handler = async (m, { conn, text, usedPrefix, command }) => { 
-function detectProhibitedWords(text_) {
-const lowercaseText = text.toLowerCase()
+async function detectProhibitedWords(text) {
+ 
+  const lowercaseText = text.toLowerCase();
 
-async function translateWord(word) {
-const translations = await translate(word, { to: 'en' })
-return translations.text.toLowerCase()
-}
-const translationPromises = prohibited.map(translateWord)
-return Promise.all(translationPromises)
-.then(translatedProhibitedWords => {
-if (translatedProhibitedWords.some(word => lowercaseText.includes(word))) {
-return '⚠️😾'
-} else {
-return null
-}
-})
-.catch(error => {
-console.error('Error en la traducción:', error)
-return null
-})
+  
+  async function translateWordToEnglish(word) {
+    try {
+      const translation = await translate(word, { to: 'en' });
+      return translation.text.toLowerCase();
+    } catch (error) {
+      console.error('Error en la traducción:', error);
+      return null;
+    }
+  }
+
+ 
+  const translationPromises = prohibited.map(translateWordToEnglish);
+
+ 
+  return Promise.all(translationPromises)
+    .then(translatedProhibitedWords => {
+     
+      if (translatedProhibitedWords.some(translatedWord => lowercaseText.includes(translatedWord))) {
+        return '⚠️😾';
+      } else {
+        return null; 
+      }
+    });
 }
 
-const text_ = text
-detectProhibitedWords(text_)
-.then(response => {
-if (response) {
-console.log('Se detectó una palabra prohibida:', response)
-} else {
-console.log('El texto no contiene palabras prohibidas.')
-}})
+
+detectProhibitedWords(text)
+  .then(response => {
+    if (response) {
+      console.log('Se detectó una palabra prohibida:', response);
+    } else {
+      console.log('El texto no contiene palabras prohibidas.');
+    }
+  });
 
 }
 
