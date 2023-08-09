0b5943cfa857c5d62b7a5cf05616264a5d38edbc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-08 23:27:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -292,16 +292,18 @@ console.log(chalk.bold.green(`${lenguajeGB.smspurgeOldFiles1()} ${file} ${lengua
 }) }) }) }
 
 // Prueba para omitir el mensaje de cierre de las sessions
-//function omitirMessage(messageToOmit) {
-//const originalConsoleLog = console.log
-//console.log = function(message) {
-//if (message.includes(messageToOmit)) {
-//return
-//}}}
+function omitirMessage(messageToOmit) {
+const originalConsoleLog = console.log
+console.log = function(message) {
+if (message.includes(messageToOmit)) {
+return
+}
+originalConsoleLog.apply(console, arguments)
+}}
 
-//setInterval(async () => {
-//omitirMessage("Closing stale open session for new outgoing prekey bundle")
-//}, 1000) 
+setInterval(async () => {
+omitirMessage("Closing stale open session for new outgoing prekey bundle")
+}, 1000) 
 
 setInterval(async () => {
 await clearTmp()        
