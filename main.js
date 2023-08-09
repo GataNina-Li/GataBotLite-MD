1224ac0d7cf2cab2f4925aac2ad34a28a6992897
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-08 23:16:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -30,9 +30,11 @@ serialize()
 
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') {
 return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString()
-} global.__dirname = function dirname(pathURL) {
+} 
+global.__dirname = function dirname(pathURL) {
 return path.dirname(global.__filename(pathURL, true))
-} global.__require = function require(dir = import.meta.url) {
+} 
+global.__require = function require(dir = import.meta.url) {
 return createRequire(dir)
 }
 
