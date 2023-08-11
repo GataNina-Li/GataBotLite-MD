1408b09992cc0af16671e498baad54ff55b919d1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-11 02:45:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -141,7 +141,7 @@ if (connection == 'open') {
 console.log(chalk.bold.yellow(lenguajeGB['smsConexion']()))}
 if (connection == 'close') {
 const files = fs.readdirSync(authFile) // Utilizar readdirSync  
-global.reloadHandler(true) ? '' : console.log(chalk.bold.yellow(lenguajeGB['smsConexionOFF']()))
+(global.reloadHandler(true) || false) ? '' : console.log(chalk.bold.yellow(lenguajeGB['smsConexionOFF']()))
   
 //if (files.includes('creds.json') && global.reloadHandler(false) === false) {
 //console.log(chalk.bold.yellow(lenguajeGB['smsConexionOFF']()))}
