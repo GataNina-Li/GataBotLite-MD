65319c8d0239611aa9cf656b0919a40172a349ab
Bayo015 <142086379+Bayo015@users.noreply.github.com>
2023-08-12 06:31:03 -0300

    Mw_bot

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
 // EN ➜ Add the Bot number as priority first
 // EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
 global.owner = [
-['593993684821', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈', true],
-['593980586516', '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], 
-['593968585383'], ['5492266466080'], ['5492266613038']]
+['593993684821', 'bayo', true],
+['593980586516', 'Mw_bot', true], 
+['593968585383'], ['12502882702'], ['5492266613038']]
 
 global.mods = [] 
 global.prems = []
