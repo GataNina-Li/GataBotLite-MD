6943191ffb60e4f1474db8e84648b71cb137ef1a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-15 13:14:12 -0500

    Update prueba-promocionar.js

--- plugins/prueba-promocionar.js
+++ plugins/prueba-promocionar.js
@@ -12,7 +12,13 @@ let message = text.replace(linkRegex, '').trim();
 if (message.length < 10) return m.reply('_⚠️😿 El mensaje de promoción debe contener al menos 10 letras_')
 
 const linksWithQuotes = text.match(/['"](https:\/\/chat.whatsapp.com\/[0-9A-Za-z]{20,24})['"]/ig) || []
-const filteredMessage = linksWithQuotes.map(link => link.replace(/['"]/g, '')) // Eliminar comillas de los enlaces
+//const mensaje = text.replace(linkRegex, '').trim()
+//const modificarMensaje = mensaje.replace(/['"]/g, '')
+message = text
+.split(' ')
+.filter(link => linksWithQuotes.includes(link))
+.map(link => link.replace(/['"]/, ''))
+.join(' ')
   
 for (const link of enlaces) {
 if (linksWithQuotes.includes(link)) {
@@ -24,7 +30,7 @@ try {
 const res = await conn.groupAcceptInvite(code)
 await delay(2000); // Esperar 4 segundos antes de continuar
       
-await conn.sendMessage(res, { text: filteredMessage }, { quoted: m });
+await conn.sendMessage(res, { text: message }, { quoted: m });
 await delay(4000) // Esperar 2 segundos antes de enviar el mensaje
 
 // Dejar el grupo solo si el bot se unió durante esta iteración
@@ -34,7 +40,7 @@ await delay(5000); // Esperar 6 segundos antes de repetir con otros enlaces
   
 }} catch (error) {
 console.error(error)
-await conn.sendMessage(m.chat, { text: `Ocurrió un error al unirse o enviar el mensaje al grupo ${link}\n\nVerifique que el Grupo no tenga activada la opción de aprobar usuarios o que en el grupo todos puedan enviar mensaje` }, { quoted: m });
+await conn.sendMessage(m.chat, { text: `Ocurrió un error al unirse o enviar el mensaje al grupo https://${link}\n\nVerifique que el Grupo no tenga activada la opción de aprobar usuarios o que en el grupo todos puedan enviar mensaje` }, { quoted: m });
 }} 
 await m.reply('_Mensaje enviado a todos los grupos_')
 }
