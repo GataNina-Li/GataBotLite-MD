9fb46d9150d0cf4eb8447f5482a1bbae87e39d8e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-15 11:17:36 -0500

    Update prueba-promocionar.js

--- plugins/prueba-promocionar.js
+++ plugins/prueba-promocionar.js
@@ -1,6 +1,7 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 let handler = async (m, { conn, text, groupMetadata, participants }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
   
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 if (!text) return m.reply('_⚠️😿 Ingresa enlaces de los grupos y el mensaje a promocionar_')
@@ -28,7 +29,7 @@ try {
 const res = await conn.groupAcceptInvite(code)
 await delay(4000); // Esperar 2 segundos antes de continuar
 
-await conn.sendMessage(res, { text: modificarMensaje, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: m })
+await conn.sendMessage(res, { text: modificarMensaje, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await delay(2000) // Esperar 4 segundos antes de enviar el mensaje
 
 // Dejar el grupo solo si el bot se unió durante esta iteración
