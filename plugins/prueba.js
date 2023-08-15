1639b362e4cb435e79b1d829cea4f7f918035cd5
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-15 19:03:47 -0300

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,6 +1,6 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, participants, text }) => {
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
 let users = participants.map(u => conn.decodeJid(u.id))
@@ -32,7 +32,7 @@ try {
 const res = await conn.groupAcceptInvite(code)
 await delay(2000); // Esperar 4 segundos antes de continuar
       
-await conn.sendMessage(res, { text: md + `\n\n` + text + `\n\nsiguierme el Instagram\n` + ig, mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id) }, { quoted: fkontak });
+await conn.sendMessage(res, { text: md + `\n\n` + text + `\n\nsiguierme el Instagram\n` + ig}, {mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id) }, { quoted: fkontak });
 await delay(2000) // Esperar 2 segundos antes de enviar el mensaje
 
 // Dejar el grupo solo si el bot se unió durante esta iteración
