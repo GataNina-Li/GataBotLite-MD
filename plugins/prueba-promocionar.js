3138764ede2bba7f5be04c6e4fca80c7db679202
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-15 11:11:04 -0500

    Update prueba-promocionar.js

--- plugins/prueba-promocionar.js
+++ plugins/prueba-promocionar.js
@@ -28,7 +28,7 @@ try {
 const res = await conn.groupAcceptInvite(code)
 await delay(2000); // Esperar 2 segundos antes de continuar
 
-await conn.sendMessage(res, { text: modificarMensaje }, { quoted: m })
+await conn.relayMessage(res, { text: modificarMensaje }, { quoted: m })
 await delay(4000) // Esperar 4 segundos antes de enviar el mensaje
 
 // Dejar el grupo solo si el bot se unió durante esta iteración
