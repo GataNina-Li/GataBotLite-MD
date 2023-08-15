25ca094e912d464732a0dd474debc69f909fc902
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-15 10:55:14 -0500

    Update prueba-promocionar.js

--- plugins/prueba-promocionar.js
+++ plugins/prueba-promocionar.js
@@ -2,7 +2,7 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 let handler = async (m, { conn, text, groupMetadata, participants }) => {
   
-const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+const linkRegex = /https:\/\/chat.whatsapp.com\/[0-9A-Za-z]{20,24}/ig
 if (!text) return m.reply('_⚠️😿 Ingresa enlaces de los grupos y el mensaje a promocionar_')
   
 const enlaces = text.match(linkRegex)
