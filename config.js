0a7645df5fe21ae057f952b39ba3ad3963c4030f
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-23 18:45:50 -0300

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 - 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
 // EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
 global.owner = [
 ['593993684821', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈', true],
-['595975740803', '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], 
-['593968585383'], ['5492266466080'], ['5492266613038']]
+['593980586516', '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], 
+['593968585383'], ['5492266466080'], ['5492266613038'], ['595975740803']]
 
 global.mods = [] 
 global.prems = []
