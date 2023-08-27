6fcaad9ee95ccaea9eb455e47cba5f9973f6f65d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-27 00:37:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -151,9 +151,9 @@ console.log('Si piensa agregar varios números separé por "," ejemplo: +593 99 
 const phoneNumberInput = readlineSync.question('Si desea omitir, escriba "0": ')
 if (phoneNumberInput !== '0' && phoneNumberInput !== '"0"') {
 const cleanedNumbers = phoneNumberInput.split(',').map(number => number.replace(/[\s+\-()]/g, '').trim())
-const newNumbersArray = cleanedNumbers.map(number => `'${number}'`).join(', ')
+const newNumbersArray = cleanedNumbers.map(number => cleanedNumbers.length === 1 ? `'${number}'` : `'[${number}]'`).join(', ')
 const regex = /(global\.owner\s*=\s*\[\s*[\s\S]*?\s*\])\s*\]/
-const newConfigContent = configContent.replace(regex, `$1, [${newNumbersArray}]]`)
+const newConfigContent = configContent.replace(regex, cleanedNumbers.length === 1 ? `$1, [${newNumbersArray}]]` : `$1, ${newNumbersArray}]`)
 fs.writeFileSync(configPath, newConfigContent, 'utf8')
 if (cleanedNumbers.length === 1) {
 console.log(`\nSe ha agregado el número "${cleanedNumbers[0]}" como propietario.`)
