d689c8c12a095b6b7e9f05d0ed443bf77e33ab0d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-26 22:56:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -148,6 +148,7 @@ console.log('\nEl idioma ya está configurado.\n')
 
 console.log('Escriba el número que será propietario, ejemplo: +593 99 000 0000')
 console.log('Si piensa agregar varios números separé por "," ejemplo: +593 99 000 0000, +52 1 000 000 0000, +598 00 000 000')
+const phoneNumberInput = readlineSync.question('Si desea omitir, escriba "0": ')
 if (phoneNumberInput !== '0' && phoneNumberInput !== '"0"') {
 const cleanedNumbers = phoneNumberInput.split(',').map(number => number.replace(/[\s+\-()]/g, '').trim())
 let newNumbersArray = ''
