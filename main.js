8b45a68454f64b830403cf1a3581d306c5f14407
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-27 22:47:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -174,7 +174,7 @@ if (registerNumber === "" || registerNumber === null || registerNumber === false
     console.log('Si piensa agregar varios números separados por ",", ejemplo: +593 99 000 0000, +52 1 000 000 0000, +598 00 000 000');
     
     const phoneNumberInput = await questionAsync('Si desea omitir, escriba "0": ')
-        rl.close()
+      //  rl.close()
 
     if (phoneNumberInput !== '0' && phoneNumberInput !== '"0"' && phoneNumberInput !== '') {
       const cleanedNumbers = phoneNumberInput.split(',').map(number => number.replace(/[\s+\-()]/g, '').trim());
