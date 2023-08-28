ebc7f8d4e5d611ea0461a5310d9fcbd19b759090
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-27 22:39:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -157,7 +157,7 @@ process.exit(1)
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 async function main() {
 if (registerNumber === "" || registerNumber === null || registerNumber === false) {
-/*setTimeout(() => {
+setTimeout(() => {
         if (!registerNumber) {
           configContent = configContent.replace('global.registerNumber = ""', 'global.registerNumber = true');
           fs.writeFileSync(configPath, configContent, 'utf8');
@@ -168,7 +168,7 @@ if (registerNumber === "" || registerNumber === null || registerNumber === false
   } else {
     console.log('\nEl registro de número ya se ha realizado o se ha omitido previamente.');
     rl.close();
-  }}*/
+  }}
           
     console.log('Escriba el número que será propietario, ejemplo: +593 99 000 0000');
     console.log('Si piensa agregar varios números separados por ",", ejemplo: +593 99 000 0000, +52 1 000 000 0000, +598 00 000 000');
@@ -192,10 +192,7 @@ if (registerNumber === "" || registerNumber === null || registerNumber === false
       configContent = configContent.replace('global.registerNumber = ""', 'global.registerNumber = true');
       fs.writeFileSync(configPath, configContent, 'utf8');
       console.log('\nSe ha omitido la adición de número/s como propietario/s.');
-    }} else {
-  console.log('\nEl registro de número ya se ha realizado o se ha omitido previamente.');
-  rl.close();
-}
+    }
 
     
 
