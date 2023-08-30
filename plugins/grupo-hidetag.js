09b46d61cb2154ef7b8e846bc3cacbf68ed38521
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-30 13:56:32 -0500

    Update grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -1,12 +1,18 @@
 import {generateWAMessageFromContent} from '@whiskeysockets/baileys'
 import * as fs from 'fs';
-const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
+const handler = async (m, {conn, text, participants, isOwner, isAdmin, args}) => {
 try {
-const users = participants.map((u) => conn.decodeJid(u.id));
-const q = m.quoted ? m.quoted : m || m.text || m.sender;
-const c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender;
-const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c + 'GB'}}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid, {mentions: users});
-await conn.relayMessage(m.chat, msg.message, {messageId: msg.key.id});
+if (args.length >= 1) {
+text = args.slice(0).join(" ")
+} else if (m.quoted && m.quoted.text) {
+text = m.quoted.text
+} else return conn.reply(m.chat, `${lenguajeGB['smsMalused3']()}\n*${usedPrefix + command} ERROR*`, m) 
+m.reply(text)
+//const users = participants.map((u) => conn.decodeJid(u.id));
+//const q = m.quoted ? m.quoted : m || m.text || m.sender;
+//const c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender;
+//const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c }}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid, {mentions: users});
+//await conn.relayMessage(m.chat, msg.message, {messageId: msg.key.id});
 } catch {
 
 // [ By @NeKosmic || https://github.com/NeKosmic/ ]
