65602721c108d81252dd4e189d3cf8aabbf9d4b8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-30 13:43:52 -0500

    Update grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -1,4 +1,4 @@
-import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
+import {generateWAMessageFromContent} from '@whiskeysockets/baileys'
 import * as fs from 'fs';
 const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
 try {
