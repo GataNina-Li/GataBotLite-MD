d4a19410bc978c2bce4de94ea6fb4c831efe94bd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-09 14:41:42 -0500

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -7,6 +7,9 @@ let user = global.db.data.users[m.sender]
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
 if (args[0] !== sn) return m.reply(lenguajeGB.smsUnreg2(usedPrefix, idreg))
 user.registered = false
+user.name = 0
+user.age = 0
+user.GBLanguage = 0
 m.reply(lenguajeGB.smsUnreg3(usedPrefix, regbot))
 }
 handler.command = /^anulareg|unreg(ister)?$/i
