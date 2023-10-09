bed184b63b00b65ef3c6bf6b541798237ea0d986
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-09 02:40:19 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -21,9 +21,9 @@ if (age < 10) return m.reply(lenguajeGB.smsVerify5())
 if (name.length >= 30) return m.reply(lenguajeGB.smsVerify6())
 user.name = name + 'ͧͧͧͦꙶͣͤ✓ᚲᴳᴮ'.trim()
 user.age = age
+return
 }  
 
-if (command.toLowerCase() == 'idiomagb') {  
 const codigosIdiomas = ['es', 'en', 'pt', 'id', 'ar']
 const nombresIdiomas = {
 'es': 'Español',
@@ -39,7 +39,8 @@ listaIdiomasTexto += `[ ${index + 1} ] » ${nombresIdiomas[codigo]}\n`
 let genText = `🌟 *SELECCIONA EL IDIOMA EL CUAL VA INTERACTUAR GATABOT CONTIGO*
 ${listaIdiomasTexto}`
 if (!user.userLanguage) await conn.sendMessage(m.chat, { text: genText }, { quoted: m })	
-
+return
+if (command.toLowerCase() == 'idiomagb') {  
 function asignarGenero(text) {
 if (text == 0 && text > 3) return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}*"${text}" NO ES VÁLIDO PARA ELEGIR, RECUERDE USAR EL EMOJI NUMÉRICO O TEXTO NUMÉRICO PARA SELECCIONAR EL IDIOMA, EJEMPLO*\n\n✓ \`\`\`${usedPrefix}idiomagb 2️⃣\`\`\`\n✓ \`\`\`${usedPrefix}idiomagb 2\`\`\``, m) 
 switch (text) {
