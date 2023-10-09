9bb221a342afec9aedd81ffac8175e2f05ab28a8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-09 02:49:38 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -67,7 +67,7 @@ default:
 return conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}*RECUERDE USAR EL EMOJI NUMÉRICO, EMOJI DE GÉNERO O TEXTO NUMÉRICO PARA SELECCIONAR EL IDIOMA, EJEMPLO*\n\n✓ \`\`\`${usedPrefix}idiomagb 2️⃣\`\`\`\n✓ \`\`\`${usedPrefix}idiomagb 2\`\`\``, m)
 }}
 asignarGenero(text)
-}
+
   
 if (user.userLanguage) {
 m.reply(`Idioma configurado como: ${user.userLanguage}`)
