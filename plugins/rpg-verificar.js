fcfe57b2ea65425d3a0e58445192183fd80e81f1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-09 17:11:24 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -30,13 +30,13 @@ const nombresIdiomas = {
 'ar': 'Árabe'
 }
 let listaIdiomasTexto = ''
-listaIdiomasTexto += '*╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄*\n' 
+listaIdiomasTexto += '*╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄*\n' 
 listaIdiomasTexto += '*┆ 🌐 IDIOMA DINÁMICO 🌐*\n' 
-listaIdiomasTexto += ' *⊢┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄*\n' 
+listaIdiomasTexto += ' *⊢┄┄┄┄┄┄┄┄┄┄┄┄┄┄*\n' 
 codigosIdiomas.forEach((codigo, index) => {
 listaIdiomasTexto += `*┆* \`\`\`[ ${index + 1} ] » ${nombresIdiomas[codigo]}\`\`\`\n`
 })
-listaIdiomasTexto += '*╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄*\n'    
+listaIdiomasTexto += '*╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄*\n'    
 let genText = `🌟 *NUEVA FUNCIÓN - MULTI LENGUAJE DINÁMICO (BETA)*\n
 👉 *ESCRIBA EL NÚMERO PARA ELEGIR EL IDIOMA, EJEMPLO:*
 ✓ \`\`\`${usedPrefix}idiomagb 2️⃣\`\`\`\n✓ \`\`\`${usedPrefix}idiomagb 2\`\`\`\n
