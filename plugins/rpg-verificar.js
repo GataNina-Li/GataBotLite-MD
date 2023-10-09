b0c2614fd321817ef8d18c5ac8790ce5dae44a54
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-09 02:48:39 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -38,8 +38,7 @@ listaIdiomasTexto += `[ ${index + 1} ] » ${nombresIdiomas[codigo]}\n`
 })
 let genText = `🌟 *SELECCIONA EL IDIOMA EL CUAL VA INTERACTUAR GATABOT CONTIGO*
 ${listaIdiomasTexto}`
-if (!user.userLanguage) await conn.sendMessage(m.chat, { text: genText }, { quoted: m })	
-return
+await conn.sendMessage(m.chat, { text: genText }, { quoted: m })	
 
 function asignarGenero(text) {
 if (text == 0 && text > 3) return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}*"${text}" NO ES VÁLIDO PARA ELEGIR, RECUERDE USAR EL EMOJI NUMÉRICO O TEXTO NUMÉRICO PARA SELECCIONAR EL IDIOMA, EJEMPLO*\n\n✓ \`\`\`${usedPrefix}idiomagb 2️⃣\`\`\`\n✓ \`\`\`${usedPrefix}idiomagb 2\`\`\``, m) 
@@ -70,7 +69,6 @@ return conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}*RECUERDE USAR EL EMOJI 
 asignarGenero(text)
 }
   
-if (user.name && user.age) {
 if (user.userLanguage) {
 m.reply(`Idioma configurado como: ${user.userLanguage}`)
 user.regTime = + new Date
