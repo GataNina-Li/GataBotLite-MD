5298e257bbd8effa59dd8f337ea3f957c6d18fe6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-09 21:13:25 -0500

    Update _set.js

--- plugins/_set.js
+++ plugins/_set.js
@@ -1,4 +1,4 @@
-import { en, es, id, ar, pt } from "../idiomas/total-idiomas.js"
+/*import { en, es, id, ar, pt } from "../idiomas/total-idiomas.js"
 let handler = m => m
 handler.before = async function (m, { conn }) {
 let user = global.db.data.users[m.sender]
@@ -19,4 +19,39 @@ global.lenguajeGB = es
 }}, 1000)
 }
 
+export default handler*/
+
+import { en, es, id, ar, pt } from "./idiomas/total-idiomas.js"
+
+let handler = m => m
+handler.before = async function (m, { conn }) {
+    let user = global.db.data.users[m.sender]
+
+    
+    if (m && user && user.GBLanguage) {
+       
+        switch (user.GBLanguage) {
+            case 'es':
+                global.lenguajeGB = es;
+                break;
+            case 'en':
+                global.lenguajeGB = en;
+                break;
+            case 'pt':
+                global.lenguajeGB = pt;
+                break;
+            case 'ar':
+                global.lenguajeGB = ar;
+                break;
+            case 'id':
+                global.lenguajeGB = id;
+                break;
+            default:
+                global.lenguajeGB = es
+        }
+    }
+    
+    return m
+
 export default handler
+
