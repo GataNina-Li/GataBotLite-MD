b603bb850ff54cd2ff92c0f2102c6d67fe0c026b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-09 21:23:28 -0500

    Update _set.js

--- plugins/_set.js
+++ plugins/_set.js
@@ -1,9 +1,9 @@
-/*import { en, es, id, ar, pt } from "../idiomas/total-idiomas.js"
+import { en, es, id, ar, pt } from "../idiomas/total-idiomas.js"
 let handler = m => m
 handler.before = async function (m, { conn }) {
 let user = global.db.data.users[m.sender]
   
-setInterval(() => {
+//setInterval(() => {
 if (user.GBLanguage == 'es') {
 global.lenguajeGB = es
 } else if (user.GBLanguage == 'en') {
@@ -16,42 +16,7 @@ global.lenguajeGB = ar
 global.lenguajeGB = id
 } else {
 global.lenguajeGB = es
-}}, 1000)
-}
-
-export default handler*/
-
-import { en, es, id, ar, pt } from "../idiomas/total-idiomas.js"
-
-let handler = m => m
-handler.before = async function (m, { conn }) {
-    let user = global.db.data.users[m.sender]
-
-    
-    if (m && user && user.GBLanguage) {
-       
-        switch (user.GBLanguage) {
-            case 'es':
-                global.lenguajeGB = es;
-                break;
-            case 'en':
-                global.lenguajeGB = en;
-                break;
-            case 'pt':
-                global.lenguajeGB = pt;
-                break;
-            case 'ar':
-                global.lenguajeGB = ar;
-                break;
-            case 'id':
-                global.lenguajeGB = id;
-                break;
-            default:
-                global.lenguajeGB = es
-        }
-    }
-    
-    return m
+//}}, 1000)
+}}
 
 export default handler
-
