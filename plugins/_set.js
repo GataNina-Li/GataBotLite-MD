41780dd6e77b2795ad351a61dfad56bc9d2a520a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-12 15:54:40 -0500

    Update _set.js

--- plugins/_set.js
+++ plugins/_set.js
@@ -1,21 +1,20 @@
-/*import { en, es, id, ar, pt } from '../lib/idiomas/total-idiomas.js'  
+import { en, es, id, ar, pt } from '../lib/idiomas/total-idiomas.js'  
 export async function before(m, { conn }) {
 let user = global.db.data.users[m.sender]
 let idioma = ''
 if (m.text) {
 if (user.GBLanguage == 'es') {
-idioma = es
+global.lenguajeGB = es
 } else if (user.GBLanguage == 'en') {
-idioma = en
+global.lenguajeGB = en
 } else if (user.GBLanguage == 'pt') {
-idioma = pt
+global.lenguajeGB = pt
 } else if (user.GBLanguage == 'ar') {
-idioma = ar
+global.lenguajeGB = ar
 } else if (user.GBLanguage == 'id') {
-idioma = id
+global.lenguajeGB = id
 } else {
-idioma = es
+global.lenguajeGB = es
 }
-global.lenguajeGB = idioma
-  }
-}*/
+global.lenguajeGB = es
+}}
