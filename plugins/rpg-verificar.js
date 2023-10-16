ab7274602b5199a9fc1346f7b33b4d21cba96f67
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-10-16 00:39:18 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -12,7 +12,13 @@ let nombresIdiomas = {
 }
   
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch((_) => gataImg.getRandom())
+let pp
+try {
+pp = await conn.profilePictureUrl(who, 'image')
+} catch (error) {
+pp = gataImg.getRandom()
+}
+//let pp = await conn.profilePictureUrl(who, 'image').catch((_) => gataImg.getRandom())
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]
 } 
