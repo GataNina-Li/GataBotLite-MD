609af5abdc74dab28b2f44bce36b7e834c242b45
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-11-20 04:15:13 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -58,7 +58,7 @@ await conn.sendMessage(m.chat, { text: genText }, { quoted: m })
 }
 
 //handler.before = async (m) => {
-handler.before = async function(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
+handler.before = async function(m, { user, nombresIdiomas, tag, pp, aa, usedPrefix } ) {
 const sender = m.sender
 registro[sender] = registro[sender] ?? {
 confirmacion: false,
