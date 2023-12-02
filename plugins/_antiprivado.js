eb123234a4e853f8349e41f15580462b5373c541
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-12-02 02:29:00 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,4 +1,4 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+/*export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if (m.isBaileys && m.fromMe) return !0;
 if (m.isGroup) return !1;
@@ -10,4 +10,4 @@ if (bot.antiPrivate && !isOwner && !isROwner) {
 await m.reply(`*${lenguajeGB['smsCreA']()}* *@${m.sender.split`@`[0]}*, ${lenguajeGB['smsprivado']()}\n${nn}`,mentions: [m.sender] }, { quoted: fkontak })
 await this.updateBlockStatus(m.chat, 'block')}
 return !1
-}
+}*/
