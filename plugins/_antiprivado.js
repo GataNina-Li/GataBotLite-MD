fa498460a12aff1719e1b4c5fee82aa8286451d5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-12-02 02:34:02 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,13 +1,14 @@
-/*export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (m.isBaileys && m.fromMe) return !0;
-if (m.isGroup) return !1;
-if (!m.message) return !0;
+if (m.isBaileys && m.fromMe) return !0
+if (m.isGroup) return !1
+if (!m.message) return !0
 if (m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('deletesesion') || m.text.includes('estado') || m.text.includes('bots')) return !0
-const chat = global.db.data.chats[m.chat];
-const bot = global.db.data.settings[this.user.jid] || {};
+const chat = global.db.data.chats[m.chat]
+const bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
 await m.reply(`*${lenguajeGB['smsCreA']()}* *@${m.sender.split`@`[0]}*, ${lenguajeGB['smsprivado']()}\n${nn}`,mentions: [m.sender] }, { quoted: fkontak })
-await this.updateBlockStatus(m.chat, 'block')}
+await this.updateBlockStatus(m.chat, 'block')
+}
 return !1
-}*/
+}
