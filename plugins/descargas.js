d462084d651d432f32866cb129a2a6e72dda7a95
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-01 20:56:49 -0500

    Update descargas.js

--- plugins/descargas.js
+++ plugins/descargas.js
@@ -172,13 +172,13 @@ reportError(e)
 }}}}}
 }  
 if (command == 'play2') {
-try {
+//try {
 let videoURL = `https://api.cafirexos.com/api/v1/ytmp4?url=${yt_play[0].url}`
 await conn.sendMessage(m.chat, { video: videoURL, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `${wm}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
-await m.react(sent)    
+await m.react(sent)
 await message.react(correct) 
-}} catch {
-reportError(e)
+//} catch {
+//reportError(e)
 }
 break
             
