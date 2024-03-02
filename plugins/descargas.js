3cd74f805f1bf209212958b059b487ddefccdbec
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-01 20:16:25 -0500

    Update descargas.js

--- plugins/descargas.js
+++ plugins/descargas.js
@@ -173,8 +173,9 @@ await message.react(correct)
 if (command == 'play2') {
 try {
 let videoURL = `https://api.cafirexos.com/api/v1/ytmp4?url=${yt_play[0].url}`
-await conn.sendMessage(m.chat, { video: videoURL.data, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `${wm}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+//await conn.sendMessage(m.chat, { video: videoURL.data, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `${wm}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
 //await conn.sendMessage(m.chat, { video: videoURL.data, mimetype: 'video/mp4', fileName: 'error.mp4' }, { quoted: m })
+await conn.sendMessage(m.chat, { video: { url: videoURL }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `${wm}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
 await m.react(sent)    
 await message.react(correct) 
 } catch (e) {
