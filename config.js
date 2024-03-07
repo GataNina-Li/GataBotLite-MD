0be52c46ef05e899be300aced71670aebd53d603
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-07 19:21:23 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
 // EN ➜ Add the Bot number as priority first
 // EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
 global.owner = [
-['5492266466080', '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈
+['573026191480', '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈
 ['593968263524', '𝙂𝘽 𝘼𝙨𝙞𝙨𝙩𝙚𝙣𝙘𝙞𝙖 💖🐈', true],
-['593968585383'], ['5217294888993'], ['5492266613038']]
+['593968585383'], ['5217294888993'], ['5492266466080'], ['5492266613038']]
 
 global.mods = [] 
 global.prems = []
