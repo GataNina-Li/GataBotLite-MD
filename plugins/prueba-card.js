9e23d4e28f0106e1e3cb66dc4ea4b05d81343b71
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-13 16:59:54 -0500

    Create prueba-card.js

---
+++ plugins/prueba-card.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch' 
+let handler = async function (m, { conn, text, usedPrefix }) {
+const apiUrl = `https://lookup.binlist.net/${text}`
+fetch(apiUrl).then(response => {
+if (!response.ok) {
+return console.log('La página no esta funcionando actualmente')
+}
+return response.json()
+}).then(data => {
+const formattedResult = `
+📇| BIN: ${userInput}
+💳| MARCA: ${data.scheme}
+🏧| TIPO: ${data.type}
+🌐| PAIS: ${data.country.name}
+🏁| BANDERA: ${data.country.emoji}
+🏦| BANCO: ${data.bank.name}
+`
+m.reply(formattedResult.trim())
+}).catch(error => {
+console.error('Error:', error)
+})
+}
+handler.command = /^(bin|card)$/i
+export default handler
