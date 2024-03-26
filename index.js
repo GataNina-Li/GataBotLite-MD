6af55385432a12b3ad5d1d9c0e55d61e537194b9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-25 20:25:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ say('GataBot\nLite\nMD', {
  gradient: ['red', 'magenta']
 })}
 
-say(`Project Author:\nGataNina-Li (@gata_dios)\n\nDevelopers:\nelrebelde21 (Mario)\nAzamiJs (Azami)\nKatashiFukushima (Katashi)`.trim(), {
+say(`Project Author:\nGataNina-Li (@gata_dios)\n\nDevelopers:\nelrebelde21 (Mario)\nKatashiFukushima (Katashi)`.trim(), {
  font: 'console',
  align: 'center',
  colors: ['candy']
