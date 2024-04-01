1c356418f93bc296d72c93a9ae7d10e3bed03b11
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-31 20:07:15 -0500

    Update convertidores.js

--- plugins/convertidores.js
+++ plugins/convertidores.js
@@ -272,7 +272,7 @@ if (!isUrlValid && !isPromptValid && m.quoted) {
 const parts = text.split("|").map(s => s.trim()) 
 const randomFilterIndex = Math.floor(Math.random() * (filters.length - 1)) + 1
 const randomFilterNumber = randomFilterIndex + 1
-[url, filterid, prompt] = [true, !/\|/.test(text) && /^\d+$/.test(text) ? text : ((text.match(/\|/g)).length === 1) && /^\d+$/.test(parts[0]) ? parts[0] : randomFilterNumber, text.split("|").length === 2 ? parts[1].trim() : ""]
+[url, filterid, prompt] = [true, !/\|/.test(text) && /^\d+$/.test(text) ? text.trim() : ((text.match(/\|/g)).length === 1) && /^\d+$/.test(parts[0]) ? parts[0].trim() : randomFilterNumber, text.split("|").length === 2 ? parts[1].trim() : ""]
 }
 
 if (!url && !m.quoted) {
