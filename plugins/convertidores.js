f932c34126937803a441ed2dd92855f6c197a6a5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-03-31 20:23:45 -0500

    Update convertidores.js

--- plugins/convertidores.js
+++ plugins/convertidores.js
@@ -279,7 +279,7 @@ if (!url && !m.quoted) {
 return m.reply("Debe agregar un enlace de imagen o responder a una imagen.")
 }
 
-if (!url && !filterid) return m.reply('> *Falta seleccionar un filtro!!*\n\n' + filterList)
+if (!filterid) return m.reply('> *Falta seleccionar un filtro!!*\n\n' + filterList)
     
 const selectedFilterIndex = parseInt(filterid) - 1
 if (isNaN(selectedFilterIndex) || selectedFilterIndex < 0 || selectedFilterIndex >= filters.length) {
