6ce1696fb8f647fdf9c4022d92a8220009665f40
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-07 08:01:34 -0500

    Create _content.js

---
+++ plugins/_content.js
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch'
+import moment from 'moment-timezone'
+import axios from 'axios'
+import fs from 'fs'
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto }  = (await import(global.baileys))
+
+var handler = m => m
+handler.all = async function (m) {
+
+const response = await fetch('https://raw.githubusercontent.com/GataNina-Li/GataBot-MD/master/official_accounts.json')  
+const data = await response.json()
+let { accounts, channels, groups, collaboration, sponsors, others } = data.info
+
+global.canal1 = channels.channel1
+global.canal2 = channels.channel2
+global.canal3 = channels.channel3
+global.canal4 = channels.channel4
+
+var canalesInfo = [
+{ link: canal1, id: "120363160031023229@newsletter", name: "INFINITY-WA 💫" },
+{ link: canal2, id: "120363169294281316@newsletter", name: "GB - UPDATE ✨" },
+{ link: canal3, id: "120363203805910750@newsletter", name: "Tips sobre GataBot 🤩" } 
+]
+var indiceAleatorio = Math.floor(Math.random() * canalesInfo.length)
+var channelRD = canalesInfo[indiceAleatorio]
+global.fakeChannel = { contextInfo: { mentionedJid: null, forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: wm, body: vs, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: gataImg, thumbnail: imagen1, sourceUrl: accountsgb }}}, { quoted: m }
+
+}
+export default handler
