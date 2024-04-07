a18e74b196cd1dd5c545a4537ec0781e0a81c771
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-04-07 08:46:27 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -206,7 +206,7 @@ const vi = ['https://telegra.ph/file/405daebd4bc0d69e5d165.mp4',
 'https://telegra.ph/file/1d0ad9f79f65f39895b08.mp4',
 'https://telegra.ph/file/c25afc1685b13210ce602.mp4']
 try {
-await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, fakeChannel2 })
+await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, contextInfo: fakeChannel2 })
 //await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: [m, fakeChannel] }) 
 } catch (error) {
 try {
@@ -233,3 +233,22 @@ let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+/*await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu,
+contextInfo: {
+mentionedJid: [m.sender],
+isForwarded: true,
+forwardedNewsletterMessageInfo: {
+newsletterJid: '120363169294281316@newsletter',
+newsletterName: "GB - UPDATE ✨",
+serverMessageId: -1
+},
+forwardingScore: 999,
+externalAdReply: {
+title: gt,
+body: wm,
+thumbnailUrl: img2,
+sourceUrl: md,
+mediaType: 1,
+renderLargerThumbnail: false
+}}})*/
