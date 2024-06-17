d05a0c4c116cbbda0342c0504c3c0a1bb73faa1d
Memo030194 <toledocastellanosguillermo@gmail.com>
2024-06-17 02:08:18 -0600

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ global.prems = []
 // ❰❰ methodCode ❱❱
 // [ES] > Agregue el número del Bot en "botNumberCode" si desea recibir código de 8 dígitos sin registrar el número en la consola.
 // [EN] > Add the Bot number in "botNumberCode" if you want to receive 8-digit code without registering the number in the console.
-global.botNumberCode = "" //example: "+59309090909"
+global.botNumberCode = "524641687427" //example: "+59309090909"
 global.confirmCode = "" // No tocar esto : Do not touch this line
 
 // ES ➜ Agregue el código de idioma el cual usará GataBot  
