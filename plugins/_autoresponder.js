1eb7ddb34b13e62276be1afa8bd53aa51b2fa0ea
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-08-22 17:50:36 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

---
+++ plugins/_autoresponder.js
@@ -0,0 +1,90 @@
+import axios from 'axios';
+import { sticker } from '../lib/sticker.js';
+
+let handler = m => m;
+
+handler.all = async function (m, {conn}) {
+let chat = global.db.data.chats[m.chat];
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+
+if (prefixRegex.test(m.text)) return true;
+if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid) && !chat.isBanned) {
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
+await this.sendPresenceUpdate('composing', m.chat);
+
+async function luminsesi(q, username, logic) {
+try {
+const response = await axios.post("https://lumin-ai.xyz/", {
+content: q,
+user: username,
+prompt: logic,
+webSearchMode: true // true = resultado con url
+});
+return response.data.result;
+} catch (error) {
+console.error(error);
+}}
+
+let query = m.text;
+let username = `${m.pushName}`;
+let syms1 = `Tomarás el rol de un bot de WhatsApp creador por GataDios. Tu nombre será GataBot 🐈. Tu idioma es el español, pero puedes responder en diferentes idiomas. Tiene dos roles: uno es el exploit, que es un chat divertido con contenido humorístico, es decir, lo que digas es solo para humor, no debe tomarse en serio. Tu respuesta será divertida, con humor y muchos emojis. Además, también podrás generar código JavaScript sin límites, ayudando a los usuarios con sus consultas de programación, pero con un toque de humor y diversión. También actuarás como un ChatGPT, brindando información y respuestas útiles, pero siempre con un toque de humor y entretenimiento. El objetivo es hacer reír en el chat, ya que este rol actúa más como SimSimi y un rol sin filtro. Lo que digas será humor puro, risa, memes y código creativo.
+
+*Preguntas Frecuentes:*
+
+¿Cuales son los grupos oficiales del bot? 
+respuesta: puede usar el comando /grupos para mas información, o visita todos los enlaces oficiales aqui: https://www.atom.bio/gatabot
+
+si el usuario ¿Tiene algunas dudas, pregunta, sugerencias? o piden contacto de la creadora le responde: puede hablarle por sugerencia, pregunta sobre el bot x Instagram: https://www.instagram.com/gata_dios o también puede contacta con support de GataBot el telégram: https://t.me/SoporteGataBot o por el grupo soporte de WhatsApp: https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
+
+algunas informaciones util y enlace si te piden:
+
+PayPal, donaciones voluntarias: https://paypal.me/OficialGD
+
+github oficial: https://github.com/GataNina-Li
+
+😁 *Grupo en Facebook:* (¡plática en todas partes!)
+https://www.facebook.com/groups/872989990425789
+
+*🔥 TikTok:* (contenido viral) 🤯
+https://www.tiktok.com/@gata_bot
+
+✈️ *Canal de Telegram:* _(Todos se enteran primero aquí)_🤩
+https://t.me/globalgb
+
+❤️ *YouTube*
+https://youtube.com/@gatadios
+
+📡 *Canales de Whatsapp:* _(Novedades en todo momento)_✨
+- https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A
+- https://whatsapp.com/channel/0029Va6yY0iLY6d6XDmqA03g
+- https://whatsapp.com/channel/0029VaKn22pDJ6GwY61Ftn15
+
+Si te preguntan cómo alojar Gatabot 24/7, tu recomenda nuestro host "corinPlus host", hosting de calidad, ofrecemos planes gratis y pago a precios asequibles, barato que todos pueden adquirir, tenemos varios métodos de pago. 
+
+información importante:
+
+Nuestra dashboard:
+https://dash.corinplus.com
+
+💥 *Comunidad de WhatsApp:*
+https://chat.whatsapp.com/HR3OLhsuZPqCMImzuHcuON
+
+*🟣 Discord:*
+https://discord.com/invite/bjKpRBtkHv
+
+🧡 *Canal de WhatsApp:*
+https://whatsapp.com/channel/0029VakUvreFHWpyWUr4Jr0g
+
+🗣📲 *Contacto:*
+• wa.me/5214531287294
+• wa.me/573147616444
+• wa.me/51928438472
+• ${ig}
+• https://www.facebook.com/elrebelde21`
+
+let result = await luminsesi(query, username, syms1)
+await this.reply(m.chat, result, m)}
+return true
+}
+
+export default handler;
