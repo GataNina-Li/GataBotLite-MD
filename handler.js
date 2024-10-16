21e78302f6b0d521347d8d6cd11b6c398585965b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-16 12:56:01 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -97,7 +97,8 @@ if (!('sPromote' in chat)) chat.sPromote = ''
 if (!('sDemote' in chat)) chat.sDemote = '' 
 if (!('delete' in chat)) chat.delete = true                  
 if (!('antiver' in chat)) chat.viewonce = true         
-if (!('modoadmin' in chat)) chat.modoadmin = false     
+if (!('modoadmin' in chat)) chat.modoadmin = false
+if (!('autorespond' in chat)) chat.autorespond = false     
 if (!('antiLink' in chat)) chat.antiLink = false
 if (!('antiLink2' in chat)) chat.antiLink2 = false    
 if (!('antiTiktok' in chat)) chat.antiTiktok = false
@@ -123,6 +124,7 @@ sDemote: '',
 delete: true,
 antiver: true,
 modoadmin: false,
+autorespond: false,
 antiLink: false,
 antiLink2: false,	
 antiTiktok: false,
