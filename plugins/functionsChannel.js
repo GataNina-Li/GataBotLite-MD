adaae62f2154e0111b762b790725835e6b68fdbc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-19 16:31:56 -0500

    Update functionsChannel.js

--- plugins/functionsChannel.js
+++ plugins/functionsChannel.js
@@ -83,7 +83,7 @@ caption += `*Descripción cambiado por:*\n${res.descOwner ? `@${res.descOwner?.s
 
 // Parámetros comunes tanto para metadatos como para enlace de invitación
 caption += `*Miembros destacados:*\n` + (res.participants ? res.participants.map((user, i) => `${i + 1}. @${user.id?.split("@")[0]}${user.admin === "superadmin" ? " (superadmin)" : user.admin === "admin" ? " (admin)" : ""}`).join("\n") : "No hay") + `\n\n` +
-`*Participantes total:*\n${res.size || "Cantidad no encontrada"}\n\n`
+`*Participantes total:*\n${res.size || "Cantidad no encontrada"}\n\n` +
 `*Comunidad vinculada al grupo:*\n${res.linkedParent ? `(Id: ${res.linkedParent})` : "No hay"} ${nameCommunity}\n\n` +
 `*Restricciones:* ${res.restrict ? "✅ Si" : "❌ No"}\n` +
 `*Anuncios:* ${res.announce ? "✅ Si" : "❌ No"}\n` +
