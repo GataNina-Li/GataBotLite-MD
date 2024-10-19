99e4d3082b3a1d323515c3b586f866771051f2f6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-19 07:44:03 -0500

    Update functionsChannel.js

--- plugins/functionsChannel.js
+++ plugins/functionsChannel.js
@@ -27,14 +27,12 @@ if (isInviteInfo) {
 caption += `*Descripción:* ${res.desc || "*No hay descripción*"}\n` +
 `*ID de descripción:* ${res.descId || "N/A"}\n`
 if (res.linkedParent) {
-    try {
-        let linkedGroupMeta = await conn.groupMetadata(res.linkedParent)
-        caption += `*Grupo vinculado:* (Id: ${res.linkedParent || "N/A"}) ${linkedGroupMeta.subject || "N/A"}\n`
-    } catch (e) {
-        console.log(e)
-        caption += `*Grupo vinculado:* ${res.linkedParent || "N/A"}\n`
-    }
-}
+try {
+let linkedGroupMeta = await conn.groupMetadata(res.linkedParent)
+caption += `*Comunidad vinculada al grupo:* (Id: ${res.linkedParent || "N/A"}) ${linkedGroupMeta.subject || "N/A"}\n` +
+} catch {
+caption += `*Comunidad vinculada al grupo:* ${res.linkedParent || "N/A"}\n` +
+}}
 `*Restricciones:* ${res.restrict ? "✅ Si" : "❌ No"}\n` +
 `*Anuncios:* ${res.announce ? "✅ Si" : "❌ No"}\n` +
 `*¿Es comunidad?:* ${res.isCommunity ? "✅ Si" : "❌ No"}\n` +
