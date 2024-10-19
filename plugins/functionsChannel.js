e43fc3a469aab087541fc8641f590ce938274340
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-19 05:27:41 -0500

    Update functionsChannel.js

--- plugins/functionsChannel.js
+++ plugins/functionsChannel.js
@@ -5,7 +5,7 @@ import { getUrlFromDirectPath } from "@whiskeysockets/baileys"
 import _ from "lodash"
 
 let handler = async (m, { conn, command, usedPrefix, args, text }) => {
-const isCommand1 = /^(inspect2)$/i.test(command)
+const isCommand1 = /^(inspectfull)$/i.test(command)
     
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 async function reportError(e) {
@@ -121,7 +121,7 @@ reportError(e)
 break
         
 }}
-handler.command = /^(inspect2)$/i
+handler.command = /^(inspectfull)$/i
 handler.register = true
 export default handler 
 
