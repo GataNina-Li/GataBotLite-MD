d7f5559e7873f1856bc8502c51b3eb47e40ed10c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-20 12:37:27 -0500

    Update functionsChannel.js

--- plugins/functionsChannel.js
+++ plugins/functionsChannel.js
@@ -84,7 +84,7 @@ info = await groupInfo(res)
 console.log('Método de metadatos')
 } catch { // En caso de que no esté en el grupo, va a intentar con el enlace
 const inviteUrl = text?.match(/(?:https:\/\/)?(?:www\.)?(?:chat\.|wa\.)?whatsapp\.com\/(?:invite\/|joinchat\/)?([0-9A-Za-z]{22,24})/i)?.[1]
-if (!inviteUrl) return m.reply("Grupo no encontrado.")
+//if (!inviteUrl) return m.reply("Grupo no encontrado.")
 let inviteInfo = await conn.groupGetInviteInfo(inviteUrl)
 info = await groupInfo(inviteInfo, true)  
 console.log(info)
