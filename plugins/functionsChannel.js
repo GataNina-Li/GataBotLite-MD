fd6f3a2c2827cbd076f8ca0274e04d6765b3838b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-22 02:13:47 -0500

    Update functionsChannel.js

--- plugins/functionsChannel.js
+++ plugins/functionsChannel.js
@@ -120,14 +120,13 @@ renderLargerThumbnail: false
 } else {
 // Manejo de enlaces de canales
 const channelUrl = text?.match(/(?:https:\/\/)?(?:www\.)?(?:chat\.|wa\.)?whatsapp\.com\/(?:channel\/|joinchat\/)?([0-9A-Za-z]{22,24})/i)?.[1]
-if (!channelUrl) return await conn.reply(m.chat, "*Enlace incorrecto.* Verifique que sea un enlace de canal de WhatsApp.", m)
+if (!channelUrl) return await conn.reply(m.chat, "*Verifique que sea un enlace de canal de WhatsApp.*", m)
 if (channelUrl) {
 try {
 let newsletterInfo = await conn.newsletterMetadata("invite", channelUrl).catch(e => {  
 conn.reply(m.chat, "*No se encontró información del canal.* Verifique que el enlace sea correcto.", m)       
 return  
 })
-if (!newsletterInfo) return m.reply("Canal no encontrado.")
 let caption = "*Inspector de enlaces de Canales*\n\n" + processObject(newsletterInfo, "", newsletterInfo.preview)
 if (newsletterInfo.preview) {
 pp = getUrlFromDirectPath(newsletterInfo.preview)
