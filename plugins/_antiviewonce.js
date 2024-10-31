274baa2786adadaa326d0fa63de62eb4a363abdf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-10-31 00:26:09 -0500

    𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 ~ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -2,8 +2,6 @@ import { downloadContentFromMessage } from "@whiskeysockets/baileys"
 
 export async function before(m, { isAdmin, isBotAdmin }) {
 let chat = db.data.chats[m.chat]
-//if (viewonce && m.mtype && m.msg && m.msg.hasOwnProperty("viewOnce"))
-//if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return
 if (!chat.antiver || chat.isBanned) return
 if (m.mtype == 'viewOnceMessageV2' || m.mtype.hasOwnProperty("viewOnce")) {
 let msg = m.message.viewOnceMessageV2.message
