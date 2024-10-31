fad20a230ff4443afaffbce8c09d0c98ca8362e3
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-10-31 07:36:47 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,5 +1,18 @@
 export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+
+let setting = global.db.data.settings[this.user.jid]
+const settingsREAD = global.db.data.settings[this.user.jid] || {}
+
+if (m.text && prefixRegex.test(m.text)) {
+await this.sendPresenceUpdate('composing', m.chat)
+await this.readMessages([m.key])
+        
+let usedPrefix = m.text.match(prefixRegex)[0]
+let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
+}
+
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
