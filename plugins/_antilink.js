1509b251e5bbcba71b96fc36c70064a2b1abdf74
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-11-15 19:26:44 -0300

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,4 +1,5 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i 
+let linkRegex1 = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+let linkRegex2 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
 export async function before(m, { isAdmin, isBotAdmin }) {
 if (m.isBaileys && m.fromMe)
 return !0
@@ -8,8 +9,10 @@ let delet = m.key.participant
 let bang = m.key.id
 let toUser = `${m.sender.split("@")[0]}`
 let aa = toUser + '@s.whatsapp.net'
+const groupAdmins = participants.filter(p => p.admin);
+const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n');
 let bot = global.db.data.settings[this.user.jid] || {}
-const isGroupLink = linkRegex.exec(m.text)
+const isGroupLink = linkRegex1.exec(m.text) || linkRegex2.exec(m.text);
 const grupo = `https://chat.whatsapp.com`
 if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
 if (chat.antiLink && isGroupLink && !isAdmin) {
@@ -17,7 +20,7 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return m.reply(lenguajeGB['smsWaMismoEnlace']())  
 }    
-if (!isBotAdmin) return m.reply(`${lenguajeGB['smsAvisoFG']()} ${lenguajeGB['smsAllAdmin']()}`)
+if (!isBotAdmin) return conn.sendMessage(m.chat, { text: `${lenguajeGB['smsAvisoFG']()} ${lenguajeGB['smsAllAdmin']()}`, mentions: [...groupAdmins.map(v => v.id)] }, { quoted: m });
 if (isBotAdmin) {
 await conn.reply(m.chat, `${lenguajeGB['smsEnlaceWat']()} *@${toUser}*`, null, { mentions: [aa] })
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
