763fe0d816bc209511b535373531991b6e2a5b46
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-11 11:02:10 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,4 +1,38 @@
-import { downloadContentFromMessage } from "@whiskeysockets/baileys"
+let { downloadContentFromMessage } = (await import(global.baileys));
+
+let handler = m => m;
+handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
+let media, msg, type
+const { antiver, isBanned } = global.db.data.chats[m.chat]
+if (!antiver || isBanned || !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return
+if (m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension') {
+msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message
+type = Object.keys(msg)[0]
+let mediaType = type == 'imageMessage' ? 'image' :  type == 'videoMessage' ? 'video' : type == 'audioMessage' ? 'audio' : null
+
+if (!mediaType) return
+media = await downloadContentFromMessage(msg[type], mediaType)
+        
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])
+}
+
+const fileSize = formatFileSize(msg[type].fileLength)
+const description = mid.antiviewonce(type, fileSize, m, msg)
+
+if (/image|video/.test(type)) {
+return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
+} else if (/audio/.test(type)) {
+await conn.reply(m.chat, description, m, { mentions: [m.sender] })
+await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
+}}
+}
+
+export default handler
+
+
+/*import { downloadContentFromMessage } from "@whiskeysockets/baileys"
 
 export async function before(m, { isAdmin, isBotAdmin }) {
 let chat = db.data.chats[m.chat]
@@ -14,4 +48,4 @@ if (/video/.test(type)) {
 return this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption}` + lenguajeGB.smsAntiView1(), m)
 } else if (/image/.test(type)) {
 return this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption}` + lenguajeGB.smsAntiView2(), m)
-}}}
+}}}*/
