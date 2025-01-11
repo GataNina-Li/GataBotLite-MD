6333af1f1c6c2bf6c020f3749c2a1b5866d3db6d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-01-11 11:09:50 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,18 +1,28 @@
-let { downloadContentFromMessage } = (await import(global.baileys));
+import { downloadContentFromMessage } from "@whiskeysockets/baileys";
 
-let handler = m => m;
+let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
 let media, msg, type
+
 const { antiver, isBanned } = global.db.data.chats[m.chat]
-if (!antiver || isBanned || !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return
-if (m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension') {
-msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message
+if (!antiver || isBanned || !(m.mtype === 'viewOnceMessageV2' || m.mtype === 'viewOnceMessageV2Extension' || m.mtype === 'messageContextInfo')) return
+
+if (m.mtype === 'viewOnceMessageV2' || m.mtype === 'viewOnceMessageV2Extension') {
+msg = m.mtype === 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message
+} else if (m.mtype === 'messageContextInfo' && m.mediaMessage?.imageMessage) {
+msg = { imageMessage: m.mediaMessage.imageMessage }
+} else {
+return
+}
+
 type = Object.keys(msg)[0]
-let mediaType = type == 'imageMessage' ? 'image' :  type == 'videoMessage' ? 'video' : type == 'audioMessage' ? 'audio' : null
+let mediaType = type === 'imageMessage' ? 'image' : type === 'videoMessage' ? 'video' : type === 'audioMessage' ? 'audio' : null
 
 if (!mediaType) return
+
+try {
 media = await downloadContentFromMessage(msg[type], mediaType)
-        
+
 let buffer = Buffer.from([])
 for await (const chunk of media) {
 buffer = Buffer.concat([buffer, chunk])
@@ -22,12 +32,13 @@ const fileSize = formatFileSize(msg[type].fileLength)
 const description = mid.antiviewonce(type, fileSize, m, msg)
 
 if (/image|video/.test(type)) {
-return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
+return await conn.sendFile(m.chat, buffer, type === 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
 } else if (/audio/.test(type)) {
 await conn.reply(m.chat, description, m, { mentions: [m.sender] })
 await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
+}} catch (error) {
+console.error('Error al procesar el mensaje de visualización única: ', error)
 }}
-}
 
 export default handler
 
