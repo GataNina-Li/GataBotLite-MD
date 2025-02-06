b1b82d233c58d3d209f49ebdd88ba5c53a2d10ec
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-06 13:47:08 -0500

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -101,14 +101,14 @@ msgRetry,
 msgRetryCache,
 version: [2, 3000, 1015901307],
 syncFullHistory: true,
-browser: mcode ? ['Ubuntu', 'Chrome', '110.0.5585.95'] : ['GataBot-MD (Sub Bot)', 'Chrome','2.0.0'],
+browser: mcode ? ['Ubuntu', 'Chrome', '110.0.5585.95'] : ['GataBotLite-MD (Sub Bot)', 'Chrome','2.0.0'],
 defaultQueryTimeoutMs: undefined,
 getMessage: async (key) => {
 if (store) {
 //const msg = store.loadMessage(key.remoteJid, key.id)
 //return msg.message && undefined
 } return {
-conversation: 'GataBot-MD',
+conversation: 'GataBotLite-MD',
 }}} 
 
 let sock = makeWASocket(connectionOptions)
@@ -132,18 +132,14 @@ return
 if (qr && mcode) {
 let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
 secret = secret.match(/.{1,4}/g)?.join("-")
-const dispositivo = await getDevice(m.key.id);
-if (!m.isWABusiness) {
-if (/web|desktop|unknown/i.test(dispositivo)) {
+const dispositivo = await getDevice(m.key.id)
+if (!m.isWABusiness && /web|desktop|unknown/i.test(dispositivo)) {
 txtCode = await conn.sendMessage(m.chat, { image: { url: 'https://qu.ax/wyUjT.jpg' || gataMenu.getRandom() }, caption: rtx2.trim() + '\n' + drmer.toString("utf-8") }, { quoted: m })
-codeBot = await m.reply(secret);
+codeBot = await m.reply(secret)
 } else {
 txtCode = await conn.sendButton(m.chat, rtx2.trim() + '\n' + drmer.toString("utf-8"), wm + `\n*Código:* ${secret}`, 'https://qu.ax/wyUjT.jpg' || gataMenu.getRandom(), null,  [[`Copiar código`, secret]], null, null, m)
-}} else {
-txtCode = await conn.sendMessage(m.chat, { image: { url: 'https://qu.ax/wyUjT.jpg' || gataMenu.getRandom() }, caption: rtx2.trim() + '\n' + drmer.toString("utf-8") }, { quoted: m })
-codeBot = await m.reply(secret);
 }
-console.log(secret);
+console.log(secret)
 }
 if ((txtCode && txtCode.key) || (txtCode && txtCode.id)) {
 const messageId = txtCode.key || txtCode.id
@@ -342,20 +338,3 @@ async function joinChannels(conn) {
 for (const channelId of Object.values(global.ch)) {
 await conn.newsletterFollow(channelId).catch(() => {})
 }}
-
-/*async function checkSubBots() {
-  for (let sock of global.conns) {
-    if (!sock.ws || sock.ws.readyState !== ws.OPEN) {
-      console.log(`Sub bot ${sock.user.jid} está desconectado, intentando reiniciar...`);
-      try {
-        await creloadHandler(true).catch(console.error);
-        console.log(`Sub bot ${sock.user.jid} reiniciado exitosamente`);
-      } catch (error) {
-        console.error(`Error al reiniciar el sub bot ${sock.user.jid}:`, error);
-      }
-    }
-  }
-}
-
-setInterval(checkSubBots, 300000) //5 min
-*/
