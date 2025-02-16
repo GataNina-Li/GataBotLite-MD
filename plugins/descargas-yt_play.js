82842fd1b9eb1e9275ef90e17fb688c5b68a9a39
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-15 19:01:49 -0500

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -160,6 +160,19 @@ const search = await yts.search({query, hl: 'es', gl: 'ES', ...options})
 return search.videos
 }
 
+function secondString(seconds) {
+seconds = Number(seconds)
+const d = Math.floor(seconds / (3600 * 24))
+const h = Math.floor((seconds % (3600 * 24)) / 3600)
+const m = Math.floor((seconds % 3600) / 60)
+const s = Math.floor(seconds % 60)
+const dDisplay = d > 0 ? d + (d == 1 ? ' día, ' : ' días, ') : ''
+const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : ''
+const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : ''
+const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : ''
+return dDisplay + hDisplay + mDisplay + sDisplay
+}
+
 async function getFileSize(url) {
 try {
 const response = await fetch(url, { method: 'HEAD' })
@@ -191,5 +204,3 @@ return videoInfo
 } else {
 throw new Error("No se pudo obtener información del video desde Invidious")
 }}
-
-
