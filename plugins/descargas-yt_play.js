f17d549fc52130a503b80e467d288c3c0638dba0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-15 22:37:10 -0500

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -9,12 +9,12 @@ const youtubeRegexID = /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/))([a-z
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
 if (!text) return m.reply(lenguajeGB.smsMalused2() + `*${usedPrefix + command} Billie Eilish - Bellyache*`)
 
-let videoIdToFind = text.match(youtubeRegexID)
+let videoIdToFind = text.match(youtubeRegexID) || text
   
 const yt_play = await search(args.join(' '))
-let ytplay2 = await yts(videoIdToFind ? 'https://youtu.be/' + videoIdToFind[1] : text)
+let ytplay2 = await yts(videoIdToFind)
   
-if (videoIdToFind) {
+if (videoIdToFind[1]) {
 const videoId = videoIdToFind[1]  
 ytplay2 = ytplay2.all.find(item => item.videoId === videoId) || ytplay2.videos.find(item => item.videoId === videoId)
 } 
