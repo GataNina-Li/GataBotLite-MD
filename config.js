1bf4ac5e9117a44586d0ed39b4101c9cb95b4a0e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-16 06:11:49 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -49,50 +49,23 @@ global.openai_key = 'sk-0'
 // ES ➜ Consigue tu ID de organizacion en: https://platform.openai.com/account/org-settings
 global.openai_org_id = 'org-3'
 
-global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = 'GataDiosV3'
-global.itsrose = ['4b146102c4d500809da9d1ff']
+//global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
+//global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.baileys = '@whiskeysockets/baileys'
 
 global.APIs = { 
-lolhuman: { url: 'https://api.lolhuman.xyz/api/', key: lolkeysapi },
+lolhuman: { url: 'https://api.lolhuman.xyz/api/', key: 'GataDiosV3' },
 skizo: { url: 'https://skizo.tech/api/', key: 'GataDios' },
 alyachan: { url: 'https://api.alyachan.dev/api/', key: 'Gata-Dios' }, 
 exonity: { url: 'https://exonity.tech/api/', key: 'GataDios' },
 ryzendesu: { url: 'https://api.ryzendesu.vip/api/', key: null },
 neoxr: { url: 'https://api.neoxr.eu/api/', key: null },
-aemt: { url: 'https://widipe.com/', key: null }, 
 dorratz: { url: 'https://api.dorratz.com/', key: null },
 siputzx: { url: 'https://api.siputzx.my.id/api/', key: null },
 vreden: { url: 'https://api.vreden.web.id/api/', key: null },
 delirius: { url: 'https://delirius-apiofc.vercel.app/', key: null },
-zahwazein: { url: 'https://api.zahwazein.xyz', key: null },
-akuari: { url: 'https://apimu.my.id', key: null },
-apimu: { url: 'https://api.xteam.xyz', key: null },
-fgmods: { url: 'https://api-fgmods.ddns.net', key: null },
-botcahx: { url: 'https://api.botcahx.biz.id', key: null },
-ibeng: { url: 'https://api.ibeng.tech/docs', key: null },
-itsrose: { url: 'https://api.itsrose.site', key: null },
-popcat: { url: 'https://api.popcat.xyz', key: null },
-xcoders: { url: 'https://api-xcoders.site', key: 'Frieren' }
-}
-
-global.APIKeys = { 
-  'https://api.xteam.xyz': `${keysxteam}`,
-  'https://api.lolhuman.xyz': `${lolkeysapi}`,
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
-  'https://violetics.pw': 'beta',
-  'https://api.zahwazein.xyz': `${keysxxx}`,
-  'https://api-fgmods.ddns.net': 'fg-dylux',
-  'https://api.botcahx.biz.id': 'Admin',
-  'https://api.ibeng.tech/docs': 'tamvan',
-  'https://api.itsrose.site': 'Rs-Zeltoria',
-  'https://api-xcoders.site': 'Frieren'
+fgmods: { url: 'https://api.fgmods.xyz/api/', key: null },
+popcat: { url: 'https://api.popcat.xyz', key: null }
 }
 
 global.mods = [] 
