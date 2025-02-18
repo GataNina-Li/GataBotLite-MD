bdfb3d03bea023bf1114ace37cbc1caaa0a54b48
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-17 21:36:30 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,19 +1,11 @@
 import axios from 'axios'
-import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
 handler.all = async function (m, {conn}) {
-let user = global.db.data.users[m.sender]
-m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
-if (m.isBot) return 
+let user = global.db.data.users[m.sender] 
   
 let chat = global.db.data.chats[m.chat]
 let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-
-if (prefixRegex.test(m.text)) return true;
-if (m.isBot || m.sender.includes('bot') || m.sender.includes('Bot')) {
-return true; 
-}
   
 if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid) && !chat.isBanned) {
 if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
@@ -28,7 +20,8 @@ webSearchMode: true // true = resultado con url
 });
 return response.data.result
 } catch (error) {
-console.error(error)
+return
+//console.error(error)
 }}
 
 async function geminiProApi(q, logic) {
